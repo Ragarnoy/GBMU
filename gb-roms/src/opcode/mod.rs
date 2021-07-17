f3e357e764fcb011e4afd824a32108fbe6404957
firelight.flagboy <firelight.flagboy@gmail.com>
2021-07-17 12:04:27 +0200

    wip opcode generator

--- gb-roms/src/opcode/mod.rs
+++ gb-roms/src/opcode/mod.rs
@@ -1,3 +1,22 @@
+mod error;
 mod register;
 
 pub enum Opcode {}
+
+pub struct OpcodeGenerator<It>
+where
+	It: Iterator<Item = u8>,
+{
+	stream: It,
+}
+
+impl<It> Iterator for OpcodeGenerator<It>
+where
+	It: Iterator<Item = u8>,
+{
+	type Item = Opcode;
+
+	fn next(&mut self) -> Option<Self::Item> {
+		unimplemented!();
+	}
+}
