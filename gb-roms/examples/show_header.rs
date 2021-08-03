3ec3093d0d69db8520c5cbce37325757ddfafbcd
FirelightFlagboy <30697622+FirelightFlagboy@users.noreply.github.com>
2021-08-03 08:13:23 +0200

    Format code (#8)
    * format code
    
    * correct editorconfig

--- gb-roms/examples/show_header.rs
+++ gb-roms/examples/show_header.rs
@@ -6,19 +6,19 @@ use gb_roms::Header;
 #[derive(Clap, Debug)]
 #[clap(version = "1.0", author = "fbenneto")]
 struct CliOpts {
-	#[clap(required = true)]
-	files: Vec<String>,
+    #[clap(required = true)]
+    files: Vec<String>,
 }
 
 fn get_gb_header_from_file(name: &String) {
-	println!("current file: \"{}\"", name);
-	let file = File::open(name).expect("cannot open file");
+    println!("current file: \"{}\"", name);
+    let file = File::open(name).expect("cannot open file");
 
-	let header = Header::from_file(file).expect("failed to read header");
-	println!("header      : {:02x?}", header);
+    let header = Header::from_file(file).expect("failed to read header");
+    println!("header      : {:02x?}", header);
 }
 
 fn main() {
-	let opts: CliOpts = CliOpts::parse();
-	opts.files.iter().for_each(get_gb_header_from_file);
+    let opts: CliOpts = CliOpts::parse();
+    opts.files.iter().for_each(get_gb_header_from_file);
 }
