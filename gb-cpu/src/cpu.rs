cf993bd6b39688c740eeaa6e56ae417ef928d0c9
Cedric M'Passi <cedric.mpassi@pmu.fr>
2021-08-08 01:50:12 +0000

    Add cpu.rs

---
+++ gb-cpu/src/cpu.rs
@@ -0,0 +1,27 @@
+use super::registers::Registers;
+use super::memory::Memory;
+use super::error::Error;
+
+#[derive(Debug, Default)]
+pub struct Cpu {
+    pub registers: Registers,
+}
+
+impl Cpu {
+    fn next(&mut self, memory: &Memory) -> Result<u8, Error> {
+        match memory.read(self.registers.pc){
+            Ok(byte) => {
+                self.registers.next_pc();
+                Ok(byte)
+            }
+            Err(_) => Err(Error::InvalidPC(self.registers.pc))
+        }
+    }
+
+    pub fn step(&mut self, memory: &Memory) -> u32 {
+        let _opcode = self.next(memory);
+
+        //do opcode, return cycles taken
+        1
+    }
+}
