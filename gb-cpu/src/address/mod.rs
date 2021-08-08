6bf015c379449d7afeb00776ea9ecdf38376f455
Cedric M'Passi <cedric.mpassi@pmu.fr>
2021-08-08 04:14:35 +0000

    Correct relative address calculation

--- gb-cpu/src/address/mod.rs
+++ gb-cpu/src/address/mod.rs
@@ -7,13 +7,13 @@ use consts::*;
 pub fn relative(area: Area, address: u16) -> usize {
     let result = match area {
         Area::Rom => address,
-        Area::Vram => ROM_MIN - address,
-        Area::ExtRam => EXTERN_MIN - address,
-        Area::Wram => WRAM_MIN - address,
-        Area::EchoRam => ECHO_MIN - address,
-        Area::Oam => OAM_MIN - address,
-        Area::IOReg => IOREG_MIN - address,
-        Area::HighRam => HIGH_MIN - address,
+        Area::Vram => address - ROM_MIN,
+        Area::ExtRam => address - EXTERN_MIN,
+        Area::Wram => address - WRAM_MIN,
+        Area::EchoRam => address - ECHO_MIN,
+        Area::Oam => address - OAM_MIN,
+        Area::IOReg => address - IOREG_MIN,
+        Area::HighRam => address - HIGH_MIN,
         Area::IEReg => INTERUPT_ENABLE,
     };
     result as usize
