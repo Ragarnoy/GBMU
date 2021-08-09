e60715c3e11a72091a25e289a6a464f6120c1cd5
Cedric M'Passi <cedric.mpassi@pmu.fr>
2021-08-09 15:51:58 +0000

    Remove RelativeAddress Error

--- gb-cpu/src/error.rs
+++ gb-cpu/src/error.rs
@@ -2,7 +2,6 @@ use crate::processor::cpu::registers::area;
 
 #[derive(Debug)]
 pub enum Error {
-    InvalidRelativeAddress(usize),
     InvalidAbsoluteAddress(u16),
     InvalidRegister(area::Area),
     InvalidPC(u16),
