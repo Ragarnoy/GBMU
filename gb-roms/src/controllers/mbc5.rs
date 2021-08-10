77d99a96077714ba89e8d7337e33c30f6b2251ba
firelightflagboy <firelight.flagboy@gmail.com>
2021-08-10 10:29:08 +0200

    wip implement file_operation

--- gb-roms/src/controllers/mbc5.rs
+++ gb-roms/src/controllers/mbc5.rs
@@ -42,11 +42,19 @@ impl MBC5 {
 
 impl FileOperation for MBC5 {
     fn read(&self, addr: Address) -> Result<u8, Error> {
-        unimplemented!("no read for mbc5")
+        match addr.area {
+            Area::Rom => unimplemented!("no read for mbc5 on ROM"),
+            Area::ExtRam => unimplemented!("no read for mbc5 on RAM"),
+            _ => panic!("mbc5 should not be mapped to the area {:?}", addr.area)
+        }
     }
 
     fn write(&mut self, v: u8, addr: Address) -> Result<(), Error> {
-        unimplemented!("no write for mbc5")
+        match addr.area {
+            Area::Rom => unimplemented!("no write for mbc5 on ROM")
+            Area::ExtRam => unimplemented!("no write for mbc5 on RAM")
+            _ => panic!("mbc5 should not be mapped to the area {:?}", addr.area)
+        }
     }
 }
 
