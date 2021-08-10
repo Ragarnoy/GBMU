620763df3703126c5ec7bbd9c2ee5e0a56ff36b9
firelightflagboy <firelight.flagboy@gmail.com>
2021-08-10 09:28:58 +0200

    wip constructor for mbc5

--- gb-roms/src/controllers/mbc5.rs
+++ gb-roms/src/controllers/mbc5.rs
@@ -1,6 +1,26 @@
+use crate::header::size::{RamSize, RomSize};
 use gb_cpu::address_bus::{Address, Area, Error, FileOperation};
+use std::io::{self, Read};
 
 pub struct MBC5 {}
+
+impl MBC5 {
+    /// initialize the controller using a file as the rom
+    pub fn from_file(
+        mut file: impl Read,
+        ram_size: RamSize,
+        rom_size: RomSize,
+    ) -> Result<MBC5, io::Error> {
+        let ctl = MBC5::empty(ram_size, rom_size);
+
+        Ok(ctl)
+    }
+
+    /// empty return an empty initialized controller
+    pub fn empty(ram_size: RamSize, rom_size: RomSize) -> MBC5 {
+        unimplemented!("cannot create mbc5 controller")
+    }
+}
 
 impl FileOperation for MBC5 {
     fn read(&self, addr: Address) -> Result<u8, Error> {
