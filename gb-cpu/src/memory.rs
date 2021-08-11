4ab6e752f32288b39e15c5bc1035cf7c8d77ea73
Cedric M'Passi <cedric.mpassi@pmu.fr>
2021-08-11 21:45:43 +0000

    Convert Rom to trait

--- gb-cpu/src/memory.rs
+++ gb-cpu/src/memory.rs
@@ -4,26 +4,29 @@ mod consts;
 use crate::error::Error;
 use crate::getset::*;
 
-use area::rom::{mbc::Mbc, Rom};
-use area::wram::Wram;
-use area::Area;
+use area::{Area, Wram, Rom};
+use area::rom::{Mbc, NoMbc};
 
-#[derive(Debug)]
 pub struct Memory {
     pub wram: Wram,
-    pub rom: Rom,
+    pub rom: Box<dyn Rom<Item = u8, Result = Result<(), Error>>>,
 }
 
 impl Memory {
     pub fn new(mbc: Mbc, data: Vec<u8>) -> Self {
+        let rom: Box<dyn Rom<Item = u8, Result = Result<(), Error>>> = match mbc {
+            Mbc::NoMbc => Box::new(NoMbc::new(data)),
+        };
+
         Memory {
-            rom: Rom::new(mbc, data),
+            rom,
             wram: Wram::default(),
         }
     }
 
     pub fn read(&self, address: u16) -> Result<u8, Error> {
         match address {
+            consts::ROM_MIN..=consts::ROM_MAX => Ok(self.rom.get(Area::Rom.relative(address))),
             consts::WRAM_MIN..=consts::WRAM_MAX => Ok(self.wram.get(Area::Wram.relative(address))),
             _ => Err(Error::InvalidAbsoluteAddress(address)),
         }
@@ -39,47 +42,54 @@ impl Memory {
     }
 }
 
-//#[cfg(test)]
-// mod test_memory {
-//     use super::Memory;
-//
-//     #[test]
-//     fn test_invalid_read() {
-//         let memory = Memory::default();
-//
-//         assert!(memory.read(0xfea1).is_err())
-//     }
-//
-//     #[test]
-//     fn test_invalid_write() {
-//         let mut memory = Memory::default();
-//
-//         assert!(memory.write(0xfea1, 42).is_err())
-//     }
-//
-//     #[test]
-//     fn test_read_wram() {
-//         let memory = Memory::default();
-//
-//         assert!(memory.read(0xc010).is_ok());
-//     }
-//
-//     #[test]
-//     fn test_write_wram() {
-//         let mut memory = Memory::default();
-//
-//         assert!(memory.write(0xc010, 42).is_ok());
-//     }
-//
-//     #[test]
-//     fn test_write_read_wram() {
-//         let mut memory = Memory::default();
-//
-//         assert!(memory.write(0xc010, 42).is_ok());
-//
-//         let read = memory.read(0xc010);
-//
-//         assert!(read.is_ok());
-//         assert_eq!(read.unwrap(), 42);
-//     }
-// }
+impl Default for Memory {
+    fn default() -> Self {
+        Memory::new(Mbc::NoMbc, vec![5])
+    }
+}
+
+
+#[cfg(test)]
+mod test_memory {
+    use super::Memory;
+
+    #[test]
+    fn test_invalid_read() {
+        let memory = Memory::default();
+
+        assert!(memory.read(0xfea1).is_err())
+    }
+
+    #[test]
+    fn test_invalid_write() {
+        let mut memory = Memory::default();
+
+        assert!(memory.write(0xfea1, 42).is_err())
+    }
+
+    #[test]
+    fn test_read_wram() {
+        let memory = Memory::default();
+
+        assert!(memory.read(0xc010).is_ok());
+    }
+
+    #[test]
+    fn test_write_wram() {
+        let mut memory = Memory::default();
+
+        assert!(memory.write(0xc010, 42).is_ok());
+    }
+
+    #[test]
+    fn test_write_read_wram() {
+        let mut memory = Memory::default();
+
+        assert!(memory.write(0xc010, 42).is_ok());
+
+        let read = memory.read(0xc010);
+
+        assert!(read.is_ok());
+        assert_eq!(read.unwrap(), 42);
+    }
+}
--- gb-cpu/src/memory/area/rom/mbc.rs
+++ gb-cpu/src/memory/area/rom/mbc.rs
@@ -1,5 +1,3 @@
-pub mod nombc;
-
 #[derive(Debug)]
 pub enum Mbc {
     NoMbc,
