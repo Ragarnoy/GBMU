e32e5676e8a858c2f37a5fc3fb4ef5c5b3be36d0
Cedric M'Passi <cedric.mpassi@pmu.fr>
2021-08-12 11:05:00 +0000

    Name trait type variable position

--- gb-cpu/src/bus.rs
+++ gb-cpu/src/bus.rs
@@ -3,6 +3,6 @@ pub trait Bus<T> {
     type Result;
     type Data;
 
-    fn get(&self, _: T) -> Self::Item;
-    fn set(&mut self, _: T, data: Self::Data) -> Self::Result;
+    fn get(&self, position: T) -> Self::Item;
+    fn set(&mut self, position: T, data: Self::Data) -> Self::Result;
 }
