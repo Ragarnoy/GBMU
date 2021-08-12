e82af77dce005104a0c3f98360b57e9d811cae9c
Cedric M'Passi <cedric.mpassi@pmu.fr>
2021-08-12 15:10:03 +0000

    Add comment on bus.rs

--- gb-cpu/src/bus.rs
+++ gb-cpu/src/bus.rs
@@ -1,3 +1,18 @@
+// A bus is a communication system that transfers data between components inside a computer.
+//
+// The structs holding memory should implement this trait to allow storing and retrieving of data.
+//
+// Functions:
+//    Implemented:
+//      - get: return {Item} at location {T} in &self.
+//      - set: Set {Data} at location {T} in &self, return {Result}
+//
+// Types:
+//      - T: The type of data the bus is indexed by.
+//      - Item: The type of data returned by a get on the Bus.
+//      - Result: The type of data return by a set on the Bus.
+//      - Data: The type of Data set by the bus.
+//
 pub trait Bus<T> {
     type Item;
     type Result;
