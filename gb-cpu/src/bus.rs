bc9a2a6d281488e9e4a1ac9e07406c33fe9b439a
Cedric M'Passi <cedric.mpassi@pmu.fr>
2021-08-13 17:42:20 +0000

    Refrase bus comment

--- gb-cpu/src/bus.rs
+++ gb-cpu/src/bus.rs
@@ -1,17 +1,16 @@
 // A bus is a communication system that transfers data between components inside a computer.
 //
-// The structs holding memory should implement this trait to allow storing and retrieving of data.
+// The structs holding memory should implement this trait to manage the storage and retrieval of data.
 //
 // Functions:
-//    Implemented:
-//      - get: return {Item} at location {T} in &self.
-//      - set: Set {Data} at location {T} in &self, return {Result}
+//  - get: return {Item} at location {T} in &self.
+//  - set: Set {Data} at location {T} in &self, return {Result}
 //
 // Types:
-//      - T: The type of data the bus is indexed by.
-//      - Item: The type of data returned by a get on the Bus.
-//      - Result: The type of data return by a set on the Bus.
-//      - Data: The type of Data set by the bus.
+//  - T: The type of data the bus is indexed by.
+//  - Item: The type of data returned by a get on the Bus.
+//  - Result: The type of data return by a set on the Bus.
+//  - Data: The type of Data set by the bus.
 //
 pub trait Bus<T> {
     type Item;
