16ff27751d6e06ab0a4e3f40dbf65ac20004e533
Cedric M'Passi <cedric.mpassi@pmu.fr>
2021-08-15 22:21:14 +0000

    Add correct tests

--- gb-cpu/src/memory/area/rom/nombc.rs
+++ gb-cpu/src/memory/area/rom/nombc.rs
@@ -1,6 +1,8 @@
 use crate::bus::Bus;
 use crate::error::Error;
 
+pub const NOMBC_MAX_SIZE: usize = 32_768;
+
 pub struct NoMbc {
     data: Vec<u8>,
 }
@@ -22,5 +24,34 @@ impl Bus<usize> for NoMbc {
 impl NoMbc {
     pub fn new(data: Vec<u8>) -> Self {
         NoMbc { data }
+    }
+}
+
+impl Default for NoMbc {
+    fn default() -> Self {
+        NoMbc::new(vec![0 ; NOMBC_MAX_SIZE])
+    }
+}
+
+#[cfg(test)]
+mod test_nombc {
+    use super::NoMbc;
+    use crate::bus::Bus;
+
+    #[test]
+    fn test_read_nombc() {
+        let nombc = NoMbc::default();
+
+        assert_eq!(nombc.get(0x10), 0);
+    }
+
+    #[test]
+    fn test_write_read_nombc() {
+        let mut nombc = NoMbc::default();
+
+        match nombc.set(0x42, 42) {
+            Ok(_) => panic!(),
+            Err(_) => (),
+        }
     }
 }
