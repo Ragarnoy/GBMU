5879385afb74880879da5ca4bb00b87f5cf77d4b
Cedric M'Passi <cedric.mpassi@pmu.fr>
2021-08-15 22:34:11 +0000

    Remove unused Rom trait

--- gb-cpu/src/memory/area/rom.rs
+++ gb-cpu/src/memory/area/rom.rs
@@ -3,11 +3,3 @@ pub mod nombc;
 
 pub use mbc::Mbc;
 pub use nombc::NoMbc;
-
-pub trait Rom {
-    type Item;
-    type Result;
-
-    fn get(&self, address: usize) -> Self::Item;
-    fn set(&mut self, address: usize, data: Self::Item) -> Self::Result;
-}
