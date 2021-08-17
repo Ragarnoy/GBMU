c18ed5ce44ee81db1342f645c573241344c6ce7a
Ragarnoy <tiagolernould@hotmail.com>
2021-08-17 22:57:01 +0200

    formatting

--- gb-dbg/examples/debugger.rs
+++ gb-dbg/examples/debugger.rs
@@ -1,12 +1,12 @@
-use gb_dbg::app::Debugger;
-use eframe::epi::*;
 use eframe::egui::CtxRef;
-use gb_dbg::memory::{GBMemoryEditor, MemoryEditorBuilder};
-use gb_dbg::flow_control::FlowController;
+use eframe::epi::*;
 use egui::Vec2;
+use gb_dbg::app::Debugger;
+use gb_dbg::flow_control::FlowController;
+use gb_dbg::memory::{GBMemoryEditor, MemoryEditorBuilder};
 
 pub struct DebuggerApp {
-    pub debugger: Debugger<Vec<u8>>
+    pub debugger: Debugger<Vec<u8>>,
 }
 
 impl App for DebuggerApp {
@@ -20,17 +20,18 @@ impl App for DebuggerApp {
     }
 }
 
-
 fn main() {
     let mem = vec![0xFFu8; u16::MAX as usize];
-    let gbm_mem = MemoryEditorBuilder::new(|mem , address| *mem.get(address).unwrap(), mem)
-        .with_write_function(|mem, address, value| { mem[address] = value; println!("Write!") })
+    let gbm_mem = MemoryEditorBuilder::new(|mem, address| *mem.get(address).unwrap(), mem)
+        .with_write_function(|mem, address, value| {
+            mem[address] = value;
+            println!("Write!")
+        })
         .with_address_range("VRam", 0..0xFF + 1)
         .with_address_range("Ram", 0xFF..0xFFF)
         .build();
     let dgb_app = DebuggerApp {
-        debugger: Debugger::new(gbm_mem,
-        FlowController)
+        debugger: Debugger::new(gbm_mem, FlowController),
     };
     eframe::run_native(Box::new(dgb_app), eframe::NativeOptions::default())
 }
