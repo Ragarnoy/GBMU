e0c0fba256672293874051520f7417e2edc44bfc
firelight.flagboy <firelight.flagboy@gmail.com>
2021-08-22 17:49:46 +0200

    convert area to u16

--- gb-bus/src/area.rs
+++ gb-bus/src/area.rs
@@ -10,5 +10,26 @@ pub enum Area {
     IoReg,
     HighRam,
     IEReg,
-    Unbound,
+}
+
+impl std::convert::From<Area> for u16 {
+    fn from(area: Area) -> Self {
+        use crate::constant::{
+            BIOS_START, ERAM_START, EXT_RAM_START, HRAM_START, IE_REG_START, IO_REG_START,
+            OAM_START, RAM_START, ROM_START, VRAM_START,
+        };
+
+        match area {
+            Area::Bios => BIOS_START,
+            Area::Rom => ROM_START,
+            Area::Vram => VRAM_START,
+            Area::ExtRam => EXT_RAM_START,
+            Area::Ram => RAM_START,
+            Area::ERam => ERAM_START,
+            Area::Oam => OAM_START,
+            Area::IoReg => IO_REG_START,
+            Area::HighRam => HRAM_START,
+            Area::IEReg => IE_REG_START,
+        }
+    }
 }
