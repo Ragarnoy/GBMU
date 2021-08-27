5d5c7834d001064c5e98d32ce6d9a987a6ad635d
firelight.flagboy <firelight.flagboy@gmail.com>
2021-08-27 22:08:02 +0200

    implement file operation for Bios container

--- gb-roms/src/controllers/bios.rs
+++ gb-roms/src/controllers/bios.rs
@@ -1,14 +1,43 @@
-use gb_bus::FileOperation;
-pub const BIOS_SIZE: usize = 0x100;
+use gb_bus::{Address, Error, FileOperation};
+pub const BIOS_SIZE: usize = 256;
+
+pub enum BiosType {
+    Dmg,
+    Cgb,
+}
 
 pub struct Bios {
-    container: [u8; BIOS_SIZE],
+    container: Vec<u8>,
+    pub bios_type: BiosType,
 }
 
 impl Bios {
-    from_bytes(bytes: [u8; BIOS_SIZE]) -> Self {
+    pub fn from_bytes(bios: BiosType, bytes: &[u8]) -> Self {
+        Self {
+            bios_type: bios,
+            container: Vec::from(bytes),
+        }
     }
 }
 
+pub fn dmg_bios() -> Bios {
+    Bios::from_bytes(
+        BiosType::Dmg,
+        include_bytes!("../../../roms/bios/dmg_boot.bin"),
+    )
+}
+
+pub fn cgb_bios() -> Bios {
+    Bios::from_bytes(
+        BiosType::Cgb,
+        include_bytes!("../../../roms/bios/cgb_boot.bin"),
+    )
+}
+
 impl FileOperation for Bios {
+    fn read(&self, addr: Box<dyn Address>) -> Result<u8, Error> {
+        self.container
+            .get(addr.get_address())
+            .map_or_else(|| Err(Error::new_segfault(addr)), |v| Ok(*v))
+    }
 }
