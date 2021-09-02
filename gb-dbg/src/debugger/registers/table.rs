aa28f915b81294eca8747e3fd0aa8b4d7ec89015
Tiago Lernould <tiagolernould@gmail.com>
2021-09-02 22:05:41 +0200

    RegisterTable in table.rs

---
+++ gb-dbg/src/debugger/registers/table.rs
@@ -0,0 +1,28 @@
+use std::collections::HashMap;
+use crate::dbg_interfaces::DebugRegister;
+
+pub struct RegisterTable<T> {
+    holder: T,
+    pub registers: HashMap<String, u16>,
+}
+
+impl<T: DebugRegister> RegisterTable<T> {
+
+    pub fn new(table: T) -> Self {
+        let mut register_map: HashMap<String, u16> = HashMap::with_capacity(22);
+        for register in table.register_iter() {
+            register_map.insert(register.0, register.1.into());
+        }
+        Self {
+            holder: table,
+            registers: register_map,
+        }
+    }
+
+    pub fn update_table(&mut self) {
+        for (name, value) in self.registers.iter_mut() {
+            let new = self.holder.get(name).unwrap();
+            *value = new.into();
+        }
+    }
+}
