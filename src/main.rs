497ec496441eeb54e38e2832b3bef762549c60aa
firelightflagboy <firelight.flagboy@gmail.com>
2021-09-03 11:56:53 +0200

    add fallback to release logger setup

--- src/main.rs
+++ src/main.rs
@@ -166,15 +166,9 @@ fn main() {
 #[cfg(debug_assertions)]
 fn init_logger() {
     use log::LevelFilter;
-    use simplelog::{ColorChoice, Config, TermLogger, TerminalMode};
+    use simplelog::Config;
 
-    TermLogger::init(
-        LevelFilter::Debug,
-        Config::default(),
-        TerminalMode::Mixed,
-        ColorChoice::Auto,
-    )
-    .expect("cannot initialise logger");
+    setup_terminal_logger(LevelFilter::Debug, Config::default());
 }
 
 #[cfg(not(debug_assertions))]
@@ -183,7 +177,36 @@ fn init_logger() {
     use simplelog::{Config, WriteLogger};
     use std::fs::File;
 
-    let file = File::create("/tmp/gbmu.log").expect("cannot open log file");
-    WriteLogger::init(LevelFilter::Warn, Config::default(), file)
-        .expect("cannot initialise logger");
+    const LEVEL_FILTER: LevelFilter = LevelFilter::Warn;
+    const LOG_FILE: &'static str = "/tmp/gbmu.log";
+    let config: Config = Config::default();
+    let file_res = File::create(LOG_FILE);
+
+    if let Ok(file) = file_res {
+        let write_logger_res = WriteLogger::init(LEVEL_FILTER, config.clone(), file);
+        if write_logger_res.is_ok() {
+            return;
+        } else {
+            setup_terminal_logger(LEVEL_FILTER, config);
+            log::warn!(
+                "cannot setup write logger (because: {})",
+                write_logger_res.unwrap_err()
+            );
+        }
+    } else {
+        setup_terminal_logger(LEVEL_FILTER, config);
+        log::warn!(
+            "cannot setup logging to file {} (because: {})",
+            LOG_FILE,
+            file_res.unwrap_err()
+        );
+    }
+    log::warn!("fallback to terminal logger");
+}
+
+fn setup_terminal_logger(level: log::LevelFilter, config: simplelog::Config) {
+    use simplelog::{ColorChoice, TermLogger, TerminalMode};
+
+    TermLogger::init(level, config, TerminalMode::Mixed, ColorChoice::Auto)
+        .expect("cannot setup terminal logger")
 }
