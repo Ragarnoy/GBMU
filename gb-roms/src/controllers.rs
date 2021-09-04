d520e1ed23a1d33669e95e30f39e2ea87c69bd5b
FirelightFlagboy <30697622+FirelightFlagboy@users.noreply.github.com>
2021-09-04 10:49:23 +0200

    Feature/impl controller mbc5 (#75)
    * add Controller trait
    
    * remove default implementation
    
    * Controller::trait now mut current struct
    
    * [wip] implement controller on mbc5
    
    * wip use mbc5data for (de)serialization
    
    * refactor const & rename banks field
    
    * apply lint suggestion

--- gb-roms/src/controllers.rs
+++ gb-roms/src/controllers.rs
@@ -5,3 +5,15 @@ pub mod rom_only;
 pub use mbc1::MBC1;
 pub use mbc5::MBC5;
 pub use rom_only::RomOnlyController;
+
+pub trait Controller {
+    /// Save the current controller into a Serializer file
+    fn save<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer;
+
+    /// Load data from a Deserializer file
+    fn load<'de, D>(&mut self, deserializer: D) -> Result<(), D::Error>
+    where
+        D: serde::Deserializer<'de>;
+}
