ab9d15fb5b8d90e02c426b3703b631e8ab5a3c8d
firelight.flagboy <firelight.flagboy@gmail.com>
2021-09-08 20:50:46 +0200

    impl trait for playing with `f` flag

--- gb-cpu/src/registers.rs
+++ gb-cpu/src/registers.rs
@@ -1,3 +1,7 @@
+use crate::interfaces::{
+    Read8BitsReg, Read8BitsRegExt, ReadFlagReg, Write8BitsReg, Write8BitsRegExt, WriteFlagReg,
+};
+
 pub struct Registers {
     /// Accumulator & Flags
     pub af: u16,
@@ -8,42 +12,6 @@ pub struct Registers {
     pub sp: u16,
     /// Program Counter / Pointer
     pub pc: u16,
-}
-
-pub trait Read8BitsReg {
-    fn a(&self) -> u8;
-    fn b(&self) -> u8;
-    fn c(&self) -> u8;
-    fn d(&self) -> u8;
-    fn e(&self) -> u8;
-    fn h(&self) -> u8;
-    fn l(&self) -> u8;
-}
-
-pub trait Write8BitsReg {
-    fn set_a(&mut self, value: u8);
-    fn set_b(&mut self, value: u8);
-    fn set_c(&mut self, value: u8);
-    fn set_d(&mut self, value: u8);
-    fn set_e(&mut self, value: u8);
-    fn set_h(&mut self, value: u8);
-    fn set_l(&mut self, value: u8);
-}
-
-pub trait ReadFlagReg {
-    fn zero(&self) -> bool;
-    fn substraction(&self) -> bool;
-    fn half_carry(&self) -> bool;
-    fn carry(&self) -> bool;
-    fn raw(&self) -> u8;
-}
-
-pub trait WriteFlagReg {
-    fn set_zero(&mut self, value: bool);
-    fn set_subtraction(&mut self, value: bool);
-    fn set_half_carry(&mut self, value: bool);
-    fn set_carry(&mut self, value: bool);
-    fn raw(&mut self, value: u8);
 }
 
 impl Read8BitsReg for Registers {
@@ -76,9 +44,15 @@ impl Read8BitsReg for Registers {
     }
 }
 
+impl Read8BitsRegExt for Registers {
+    fn f(&self) -> u8 {
+        self.af.to_be_bytes()[1]
+    }
+}
+
 impl Write8BitsReg for Registers {
     fn set_a(&mut self, value: u8) {
-        self.af = u16::from_be_bytes([value, self.af.to_be_bytes()[1]]);
+        self.af = u16::from_be_bytes([value, self.f()]);
     }
 
     fn set_b(&mut self, value: u8) {
@@ -103,5 +77,71 @@ impl Write8BitsReg for Registers {
 
     fn set_l(&mut self, value: u8) {
         self.hl = u16::from_be_bytes([self.h(), value]);
+    }
+}
+
+impl Write8BitsRegExt for Registers {
+    fn set_f(&mut self, value: u8) {
+        self.af = u16::from_be_bytes([self.a(), value]);
+    }
+}
+
+impl ReadFlagReg for Registers {
+    fn zero(&self) -> bool {
+        self.f() >> 7 == 0b1
+    }
+
+    fn substraction(&self) -> bool {
+        (self.f() >> 6 & 1) == 1
+    }
+
+    fn half_carry(&self) -> bool {
+        (self.f() >> 5 & 1) == 1
+    }
+
+    fn carry(&self) -> bool {
+        (self.f() >> 4 & 1) == 1
+    }
+
+    fn raw(&self) -> u8 {
+        self.f()
+    }
+}
+
+impl WriteFlagReg for Registers {
+    fn set_zero(&mut self, value: bool) {
+        if value {
+            self.af |= 0b1000_0000;
+        } else {
+            self.af &= !0b1000_0000;
+        }
+    }
+
+    fn set_subtraction(&mut self, value: bool) {
+        if value {
+            self.af |= 0b100_0000;
+        } else {
+            self.af &= !0b100_0000;
+        }
+    }
+
+    fn set_half_carry(&mut self, value: bool) {
+        if value {
+            self.af |= 0b10_0000;
+        } else {
+            self.af &= !0b10_0000;
+        }
+    }
+
+    fn set_carry(&mut self, value: bool) {
+        if value {
+            self.af |= 0b1_0000;
+        } else {
+            self.af &= !0b1_0000;
+        }
+    }
+
+    fn raw(&mut self, value: u8) {
+        self.set_f(value)
     }
 }
