4dc1740766ceaa0ff409fe5434108efa9ce7ffb4
firelight.flagboy <firelight.flagboy@gmail.com>
2021-09-09 18:33:27 +0200

    add test write reg

--- gb-cpu/src/registers.rs
+++ gb-cpu/src/registers.rs
@@ -53,37 +53,37 @@ impl Read8BitsRegExt for Registers {
 
 impl Write8BitsReg for Registers {
     fn set_a(&mut self, value: u8) {
-        self.af = u16::from_be_bytes([value, self.f()]);
+        self.af = u16::from_be_bytes([self.f(), value]);
     }
 
     fn set_b(&mut self, value: u8) {
-        self.bc = u16::from_be_bytes([value, self.c()]);
+        self.bc = u16::from_be_bytes([self.c(), value]);
     }
 
     fn set_c(&mut self, value: u8) {
-        self.bc = u16::from_be_bytes([self.b(), value]);
+        self.bc = u16::from_be_bytes([value, self.b()]);
     }
 
     fn set_d(&mut self, value: u8) {
-        self.de = u16::from_be_bytes([value, self.e()]);
+        self.de = u16::from_be_bytes([self.e(), value]);
     }
 
     fn set_e(&mut self, value: u8) {
-        self.de = u16::from_be_bytes([self.d(), value]);
+        self.de = u16::from_be_bytes([value, self.d()]);
     }
 
     fn set_h(&mut self, value: u8) {
-        self.hl = u16::from_be_bytes([value, self.l()]);
+        self.hl = u16::from_be_bytes([self.l(), value]);
     }
 
     fn set_l(&mut self, value: u8) {
-        self.hl = u16::from_be_bytes([self.h(), value]);
+        self.hl = u16::from_be_bytes([value, self.h()]);
     }
 }
 
 impl Write8BitsRegExt for Registers {
     fn set_f(&mut self, value: u8) {
-        self.af = u16::from_be_bytes([self.a(), value]);
+        self.af = u16::from_be_bytes([value, self.a()]);
     }
 }
 
@@ -155,93 +155,123 @@ impl WriteFlagReg for Registers {
 #[cfg(test)]
 mod test_read {
     use super::Registers;
-    use crate::interfaces::{Read8BitsReg, Read8BitsRegExt, ReadFlagReg};
+    use crate::interfaces::{Read8BitsReg, Read8BitsRegExt};
 
     #[test]
-    fn a() {
+    fn af() {
         let regs = Registers {
             af: u16::to_be(0xaaff),
             ..Registers::default()
         };
 
         assert_eq!(regs.a(), 0xaa);
+        assert_eq!(regs.f(), 0xff);
     }
 
     #[test]
-    fn b() {
+    fn bc() {
         let regs = Registers {
             bc: u16::to_be(0xbbcc),
             ..Registers::default()
         };
 
         assert_eq!(regs.b(), 0xbb);
-    }
-
-    #[test]
-    fn c() {
-        let regs = Registers {
-            bc: u16::to_be(0xbbcc),
-            ..Registers::default()
-        };
-
         assert_eq!(regs.c(), 0xcc);
     }
 
     #[test]
-    fn d() {
+    fn de() {
         let regs = Registers {
             de: u16::to_be(0xddee),
             ..Registers::default()
         };
 
         assert_eq!(regs.d(), 0xdd);
-    }
-
-    #[test]
-    fn e() {
-        let regs = Registers {
-            de: u16::to_be(0xddee),
-            ..Registers::default()
-        };
-
         assert_eq!(regs.e(), 0xee);
     }
 
     #[test]
-    fn h() {
+    fn hl() {
         let regs = Registers {
             hl: u16::to_be(0x8833),
             ..Registers::default()
         };
 
         assert_eq!(regs.h(), 0x88);
-    }
-
-    #[test]
-    fn l() {
-        let regs = Registers {
-            hl: u16::to_be(0x8833),
-            ..Registers::default()
-        };
-
         assert_eq!(regs.l(), 0x33);
-    }
-
-    #[test]
-    fn f() {
-        let regs = Registers {
-            af: u16::to_be(0xaaff),
-            ..Registers::default()
-        };
-
-        assert_eq!(regs.f(), 0xff);
     }
 }
 
 #[cfg(test)]
 mod test_write {
     use super::Registers;
-    use crate::interfaces::{
-        Read8BitsReg, Read8BitsRegExt, ReadFlagReg, Write8BitsReg, Write8BitsRegExt, WriteFlagReg,
-    };
+    use crate::interfaces::{Read8BitsReg, Read8BitsRegExt, Write8BitsReg, Write8BitsRegExt};
+
+    #[test]
+    fn af() {
+        let mut regs = Registers::default();
+
+        assert_eq!(regs.a(), 0);
+        assert_eq!(regs.f(), 0);
+
+        regs.set_a(0xaa);
+        regs.set_f(0xff);
+
+        assert_eq!(regs.a(), 0xaa);
+        assert_eq!(regs.f(), 0xff);
+    }
+
+    #[test]
+    fn bc() {
+        let mut regs = Registers::default();
+
+        assert_eq!(regs.b(), 0);
+        assert_eq!(regs.c(), 0);
+
+        regs.set_b(0xbb);
+        regs.set_c(0xcc);
+
+        assert_eq!(regs.b(), 0xbb);
+        assert_eq!(regs.c(), 0xcc);
+    }
+
+    #[test]
+    fn de() {
+        let mut regs = Registers::default();
+
+        assert_eq!(regs.d(), 0);
+        assert_eq!(regs.e(), 0);
+
+        regs.set_d(0xdd);
+        regs.set_e(0xee);
+
+        assert_eq!(regs.d(), 0xdd);
+        assert_eq!(regs.e(), 0xee);
+    }
+
+    #[test]
+    fn hl() {
+        let mut regs = Registers::default();
+
+        assert_eq!(regs.h(), 0);
+        assert_eq!(regs.l(), 0);
+
+        regs.set_h(0x88);
+        regs.set_l(0x33);
+
+        assert_eq!(regs.h(), 0x88);
+        assert_eq!(regs.l(), 0x33);
+    }
+}
+
+#[cfg(test)]
+mod test_read_flag {
+    use super::Registers;
+    use crate::interfaces::ReadFlagReg;
+}
+
+#[cfg(test)]
+mod test_write_flag {
+    use super::Registers;
+    use crate::interfaces::{ReadFlagReg, WriteFlagReg};
 }
