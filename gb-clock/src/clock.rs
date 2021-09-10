7dde79f42c5d17786993e8bd3bad37e62bf21492
Guilhem Smith <gsmith@student.42.fr>
2021-09-10 16:10:10 +0200

    Add some doc.

--- gb-clock/src/clock.rs
+++ gb-clock/src/clock.rs
@@ -3,14 +3,17 @@ use gb_bus::Bus;
 use std::marker::PhantomData;
 
 #[derive(Default)]
+/// Ensure that the various process unit execute their instructions in the right order.
 pub struct Clock<B: Bus<u8> + Bus<u16>> {
     curr_frame_cycle: usize,
     phantom_bus: PhantomData<B>,
 }
 
 impl<B: Bus<u8> + Bus<u16>> Clock<B> {
-    const CYCLES_PER_FRAME: usize = 17556;
+    /// The amount of cycles to execute per frame.
+    pub const CYCLES_PER_FRAME: usize = 17556;
 
+    /// A single clock cycle, during which each [Ticker] will tick 1 or 4 times depending on their [Tick] type.
     pub fn cycle(&mut self, adr_bus: &mut B, process_units: &mut Vec<&mut dyn Ticker<B>>) {
         for ticker in process_units {
             for _ in 0..ticker.cycle_count().into() {
@@ -20,6 +23,13 @@ impl<B: Bus<u8> + Bus<u16>> Clock<B> {
         self.curr_frame_cycle += 1;
     }
 
+    /// Indicate if the current frame has been completed or not.
+    pub fn frame_ready(&mut self) -> bool {
+        self.curr_frame_cycle %= Self::CYCLES_PER_FRAME;
+        self.curr_frame_cycle == 0
+    }
+
+    /// Execute enough cycles to complete the current frame.
     pub fn frame(&mut self, adr_bus: &mut B, process_units: &mut Vec<&mut dyn Ticker<B>>) {
         self.curr_frame_cycle %= Self::CYCLES_PER_FRAME;
         while self.curr_frame_cycle < Self::CYCLES_PER_FRAME {
