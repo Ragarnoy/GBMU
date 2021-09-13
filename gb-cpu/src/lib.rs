b5ee3d95df5ffee72b14b4319f66f3168b9b68d4
FirelightFlagboy <30697622+FirelightFlagboy@users.noreply.github.com>
2021-09-13 18:39:26 +0200

    Feature/cpu register (#91)
    * wip cpu register
    
    * extract register trait to specific file + add Ext trait for `f` flag
    
    * impl trait for playing with `f` flag
    
    * rewrite `f` flag write interface with const
    
    * add test read 8 bits regs
    
    * add test write reg
    
    * correct typo on method read flag
    
    * finish testing registers
    
    * correct warning unused
    
    * extract test into spec file

--- gb-cpu/src/lib.rs
+++ gb-cpu/src/lib.rs
@@ -1,1 +1,6 @@
 pub mod bus;
+pub mod cpu;
+pub mod interfaces;
+pub mod registers;
+#[cfg(test)]
+mod registers_test;
---
+++ gb-cpu/src/registers.rs
@@ -0,0 +1,153 @@
+use crate::interfaces::{
+    Read8BitsReg, Read8BitsRegExt, ReadFlagReg, Write8BitsReg, Write8BitsRegExt, WriteFlagReg,
+};
+
+pub const ZERO_MASK: u16 = 0b1000_0000;
+pub const SUBTRACTION_MASK: u16 = 0b100_0000;
+pub const HALF_CARRY_MASK: u16 = 0b10_0000;
+pub const CARRY_MASK: u16 = 0b1_0000;
+
+#[derive(Default)]
+pub struct Registers {
+    /// Accumulator & Flags
+    pub af: u16,
+    pub bc: u16,
+    pub de: u16,
+    pub hl: u16,
+    /// Stack Pointer
+    pub sp: u16,
+    /// Program Counter / Pointer
+    pub pc: u16,
+}
+
+impl Read8BitsReg for Registers {
+    fn a(&self) -> u8 {
+        self.af.to_be_bytes()[1]
+    }
+
+    fn b(&self) -> u8 {
+        self.bc.to_be_bytes()[1]
+    }
+
+    fn c(&self) -> u8 {
+        self.bc.to_be_bytes()[0]
+    }
+
+    fn d(&self) -> u8 {
+        self.de.to_be_bytes()[1]
+    }
+
+    fn e(&self) -> u8 {
+        self.de.to_be_bytes()[0]
+    }
+
+    fn h(&self) -> u8 {
+        self.hl.to_be_bytes()[1]
+    }
+
+    fn l(&self) -> u8 {
+        self.hl.to_be_bytes()[0]
+    }
+}
+
+impl Read8BitsRegExt for Registers {
+    fn f(&self) -> u8 {
+        self.af.to_be_bytes()[0]
+    }
+}
+
+impl Write8BitsReg for Registers {
+    fn set_a(&mut self, value: u8) {
+        self.af = u16::from_be_bytes([self.f(), value]);
+    }
+
+    fn set_b(&mut self, value: u8) {
+        self.bc = u16::from_be_bytes([self.c(), value]);
+    }
+
+    fn set_c(&mut self, value: u8) {
+        self.bc = u16::from_be_bytes([value, self.b()]);
+    }
+
+    fn set_d(&mut self, value: u8) {
+        self.de = u16::from_be_bytes([self.e(), value]);
+    }
+
+    fn set_e(&mut self, value: u8) {
+        self.de = u16::from_be_bytes([value, self.d()]);
+    }
+
+    fn set_h(&mut self, value: u8) {
+        self.hl = u16::from_be_bytes([self.l(), value]);
+    }
+
+    fn set_l(&mut self, value: u8) {
+        self.hl = u16::from_be_bytes([value, self.h()]);
+    }
+}
+
+impl Write8BitsRegExt for Registers {
+    fn set_f(&mut self, value: u8) {
+        self.af = u16::from_be_bytes([value, self.a()]);
+    }
+}
+
+impl ReadFlagReg for Registers {
+    fn zero(&self) -> bool {
+        self.f() >> 7 == 0b1
+    }
+
+    fn subtraction(&self) -> bool {
+        (self.f() >> 6 & 1) == 1
+    }
+
+    fn half_carry(&self) -> bool {
+        (self.f() >> 5 & 1) == 1
+    }
+
+    fn carry(&self) -> bool {
+        (self.f() >> 4 & 1) == 1
+    }
+
+    fn raw(&self) -> u8 {
+        self.f()
+    }
+}
+
+impl WriteFlagReg for Registers {
+    fn set_zero(&mut self, value: bool) {
+        if value {
+            self.af |= ZERO_MASK << 8;
+        } else {
+            self.af &= !ZERO_MASK << 8;
+        }
+    }
+
+    fn set_subtraction(&mut self, value: bool) {
+        if value {
+            self.af |= SUBTRACTION_MASK << 8;
+        } else {
+            self.af &= !SUBTRACTION_MASK << 8;
+        }
+    }
+
+    fn set_half_carry(&mut self, value: bool) {
+        if value {
+            self.af |= HALF_CARRY_MASK << 8;
+        } else {
+            self.af &= !HALF_CARRY_MASK << 8;
+        }
+    }
+
+    fn set_carry(&mut self, value: bool) {
+        if value {
+            self.af |= CARRY_MASK << 8;
+        } else {
+            self.af &= !CARRY_MASK << 8;
+        }
+    }
+
+    fn raw(&mut self, value: u8) {
+        self.set_f(value)
+    }
+}
