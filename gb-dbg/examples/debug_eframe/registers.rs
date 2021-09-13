3dabc6fcb583fc73092d48b29cf4564d44d5e232
Ragarnoy <tiagolernould@gmail.com>
2021-09-13 18:58:38 +0200

    Feature/dbg registers (#86)
    * update IML file
    
    * Change architecture of dbg crate and use trait instead of function types.
    
    * (revert) accidentally removed a method
    
    * Rename DebugRW to RW, create new trait for registers.
    
    * Register editor implementation start
    
    * Make RegisterMap clearer (Name, 8bit value) instead of (16bit value, Name)
    
    * Rename to dbg_interfaces.rs for clarity
    
    * Update main to reflect on changes to dbg crate.
    
    * RustFMT
    
    * Register editor implementation start
    
    * use grid for register display, WIP
    
    * Double import fix
    
    * Bump egui and eframe to 0.14
    
    * Set crate version to 0
    
    * Move central panel to last otherwise stuff breaks
    
    * Mostly layout work, very WIP. Debug is on
    
    * Get & Set, Default Set and Write to Read-Only, bound to u16.
    
    * oops
    
    * Register editor implementation start
    
    * use grid for register display, WIP
    
    * Register editor implementation start
    
    * Bump egui and eframe to 0.14
    
    * Set crate version to 0
    
    * Move central panel to last otherwise stuff breaks
    
    * Mostly layout work, very WIP. Debug is on
    
    * layout resize
    
    * forgot self
    
    * Add anyhow to crate for testing purposes
    
    * Make get make sense, remove set as we're readonly for now
    
    * Split example into different files
    
    * Implement example for register implementation
    
    * RustFMT
    
    * Implement From<u8> and From<u16> for RegisterType
    
    * Implement From<RegisterType> for u16
    
    * RegisterEditor in Debugger
    
    * RegisterTable in table.rs
    
    * RegisterEditor wip
    
    * RegisterEditor in example wip
    
    * fuck this shit wtf
    
    * Rename RegisterType to RegisterValue
    
    * Stop asking for iterator and use a simple Vec instead because duh
    
    * RustFMT
    
    * Comment for now as placeholders would ruin the main.
    
    * Add method to update register tables inside RegisterEditor.

--- gb-dbg/examples/debug_eframe/registers.rs
+++ gb-dbg/examples/debug_eframe/registers.rs
@@ -1,5 +1,6 @@
+use crate::registers;
 use anyhow::anyhow;
-use gb_dbg::dbg_interfaces::{DebugRegister, RegisterMap, RegisterType};
+use gb_dbg::dbg_interfaces::{DebugRegister, RegisterMap, RegisterValue};
 
 pub struct Iter<'a> {
     count: u32,
@@ -13,18 +14,17 @@ impl<'a> Iterator for Iter<'a> {
         self.count += 1;
 
         match self.count {
-            1 => Some(("A".to_owned(), RegisterType::from(self.registers.a))),
-            2 => Some(("B".to_owned(), RegisterType::from(self.registers.b))),
-            3 => Some(("C".to_owned(), RegisterType::from(self.registers.c))),
-            4 => Some(("D".to_owned(), RegisterType::from(self.registers.d))),
-            5 => Some(("E".to_owned(), RegisterType::from(self.registers.e))),
-            6 => Some(("F".to_owned(), RegisterType::from(self.registers.f))),
+            1 => Some(("A".to_owned(), RegisterValue::from(self.registers.a))),
+            2 => Some(("B".to_owned(), RegisterValue::from(self.registers.b))),
+            3 => Some(("C".to_owned(), RegisterValue::from(self.registers.c))),
+            4 => Some(("D".to_owned(), RegisterValue::from(self.registers.d))),
+            5 => Some(("E".to_owned(), RegisterValue::from(self.registers.e))),
+            6 => Some(("F".to_owned(), RegisterValue::from(self.registers.f))),
             _ => None,
         }
     }
 }
 
-#[derive(Default)]
 pub struct Registers {
     a: u8,
     b: u8,
@@ -34,8 +34,21 @@ pub struct Registers {
     f: u8,
 }
 
+impl Default for Registers {
+    fn default() -> Self {
+        Self {
+            a: 0xFF,
+            b: 0x1F,
+            c: 0x22,
+            d: 0x3F,
+            e: 4,
+            f: 8,
+        }
+    }
+}
+
 impl Registers {
-    pub fn iter(&self) -> Iter {
+    pub fn iter(&self) -> Iter<'_> {
         Iter {
             count: 0,
             registers: self,
@@ -43,22 +56,33 @@ impl Registers {
     }
 }
 
-impl<'a> DebugRegister for &'a Registers {
-    type RegisterIter = Iter<'a>;
+impl From<&Registers> for Vec<RegisterMap> {
+    fn from(registers: &registers::Registers) -> Self {
+        vec![
+            ("A".to_owned(), RegisterValue::from(registers.a)),
+            ("B".to_owned(), RegisterValue::from(registers.b)),
+            ("C".to_owned(), RegisterValue::from(registers.c)),
+            ("D".to_owned(), RegisterValue::from(registers.d)),
+            ("E".to_owned(), RegisterValue::from(registers.e)),
+            ("F".to_owned(), RegisterValue::from(registers.f)),
+        ]
+    }
+}
 
-    fn get(&self, key: &str) -> anyhow::Result<RegisterType> {
+impl DebugRegister for Registers {
+    fn get(&self, key: &str) -> anyhow::Result<RegisterValue> {
         match key {
-            "A" => Ok(RegisterType::from(self.a)),
-            "B" => Ok(RegisterType::from(self.b)),
-            "C" => Ok(RegisterType::from(self.c)),
-            "D" => Ok(RegisterType::from(self.d)),
-            "E" => Ok(RegisterType::from(self.e)),
-            "F" => Ok(RegisterType::from(self.f)),
+            "A" => Ok(RegisterValue::from(self.a)),
+            "B" => Ok(RegisterValue::from(self.b)),
+            "C" => Ok(RegisterValue::from(self.c)),
+            "D" => Ok(RegisterValue::from(self.d)),
+            "E" => Ok(RegisterValue::from(self.e)),
+            "F" => Ok(RegisterValue::from(self.f)),
             _ => Err(anyhow!("Not a valid register!")),
         }
     }
 
-    fn register_iter(&self) -> Self::RegisterIter {
-        self.iter()
+    fn registers(&self) -> Vec<RegisterMap> {
+        self.into()
     }
 }
---
+++ gb-dbg/src/debugger/registers.rs
@@ -0,0 +1,114 @@
+mod table;
+
+use crate::dbg_interfaces::DebugRegister;
+use crate::debugger::registers::table::RegisterTable;
+use egui::Label;
+use egui::{CollapsingHeader, Color32, Ui, Vec2};
+
+pub struct RegisterEditor<T> {
+    cpu: RegisterTable<T>,
+    ppu: RegisterTable<T>,
+    io: RegisterTable<T>,
+}
+
+impl<T: DebugRegister> RegisterEditor<T> {
+    fn update_table(&mut self) {
+        self.cpu.update_table();
+        self.ppu.update_table();
+        self.io.update_table();
+    }
+
+    pub fn draw(&mut self, ui: &mut Ui) {
+        self.update_table();
+        ui.label(Label::new("Register Editors").text_color(Color32::WHITE));
+        CollapsingHeader::new("🛠 Options")
+            .id_source("Register_Options")
+            .default_open(false)
+            .show(ui, |ui| {
+                ui.label("Hello");
+            });
+        ui.separator();
+        ui.horizontal_top(|ui| {
+            ui.spacing_mut().item_spacing = Vec2::new(2.0, 2.0);
+            self.draw_register_table(&self.cpu, "CPU", ui);
+            self.draw_register_table(&self.ppu, "PPU", ui);
+            self.draw_register_table(&self.io, "IO", ui);
+        });
+        ui.add_space(58.0);
+        ui.separator();
+    }
+
+    fn draw_register_table(&self, registers: &RegisterTable<T>, name: &str, ui: &mut Ui) {
+        let layout = egui::Layout::top_down(egui::Align::LEFT);
+        ui.allocate_ui_with_layout(Vec2::new(80.0, 150.0), layout, |ui| {
+            ui.colored_label(Color32::WHITE, name);
+            ui.horizontal(|ui| {
+                ui.colored_label(Color32::WHITE, "Name");
+                ui.add_space(8.0);
+                ui.colored_label(Color32::WHITE, "Value");
+            });
+            egui::ScrollArea::from_max_height(400.0)
+                .id_source("ScrollArea_".to_owned() + name)
+                .show(ui, |ui| {
+                    egui::Grid::new("Grid_".to_owned() + name)
+                        .striped(true)
+                        .spacing(Vec2::new(2.5, 2.5))
+                        .show(ui, |ui| {
+                            for row in registers.registers.iter() {
+                                let format = if *row.1 > u8::MAX as u16 {
+                                    format!("0x{:04X}", row.1)
+                                } else {
+                                    format!("0x{:02X}", row.1)
+                                };
+
+                                ui.label(egui::Label::new(&row.0));
+                                ui.label(egui::Label::new(format));
+                                ui.end_row();
+                            }
+                            ui.end_row();
+                        });
+                });
+        });
+    }
+}
+
+pub struct RegisterEditorBuilder<T> {
+    cpu: Option<RegisterTable<T>>,
+    ppu: Option<RegisterTable<T>>,
+    io: Option<RegisterTable<T>>,
+}
+
+impl<T> Default for RegisterEditorBuilder<T> {
+    fn default() -> Self {
+        Self {
+            cpu: None,
+            ppu: None,
+            io: None,
+        }
+    }
+}
+
+impl<T: DebugRegister> RegisterEditorBuilder<T> {
+    pub fn with_cpu(mut self, cpu: T) -> Self {
+        self.cpu = Some(RegisterTable::new(cpu));
+        self
+    }
+
+    pub fn with_ppu(mut self, ppu: T) -> Self {
+        self.ppu = Some(RegisterTable::new(ppu));
+        self
+    }
+
+    pub fn with_io(mut self, io: T) -> Self {
+        self.io = Some(RegisterTable::new(io));
+        self
+    }
+
+    pub fn build(self) -> RegisterEditor<T> {
+        RegisterEditor {
+            cpu: self.cpu.unwrap(),
+            ppu: self.ppu.unwrap(),
+            io: self.io.unwrap(),
+        }
+    }
+}
