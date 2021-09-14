99491bf82a2452dd06dae98a0ac82b7fb128d599
firelight.flagboy <firelight.flagboy@gmail.com>
2021-09-14 19:52:45 +0200

    wip cpu base

--- gb-cpu/src/cpu.rs
+++ gb-cpu/src/cpu.rs
@@ -1,5 +1,50 @@
 use crate::registers::Registers;
+use gb_bus::AddressBus;
+use gb_clock::Ticker;
+use gb_roms::{Opcode, OpcodeGenerator};
 
 pub struct Cpu {
     _registers: Registers,
+}
+
+impl Cpu {
+    fn execute_opcode(&mut self, opc: Opcode, addr_bus: &mut AddressBus) {}
+}
+
+impl Ticker<AddressBus> for Cpu {
+    fn cycle_count(&self) -> gb_clock::Tick {
+        gb_clock::Tick::MCycle
+    }
+
+    fn tick(&mut self, addr_bus: &mut AddressBus) {
+        let it = CpuIterator::new(self, addr_bus);
+        let mut gen = OpcodeGenerator::new(it);
+        if let Some(next) = gen.next() {
+            next.map_or_else(
+                |e| log::warn!("OpcodeGenerator returned an error: {:?}", e),
+                |opc| self.execute_opcode(opc, addr_bus),
+            );
+        } else {
+            log::trace!("OpcodeGenerator returned nothing");
+        }
+    }
+}
+
+struct CpuIterator<'a> {
+    cpu: &'a Cpu,
+    addr_bus: &'a AddressBus,
+}
+
+impl<'a> CpuIterator<'a> {
+    fn new(cpu: &'a Cpu, addr_bus: &'a AddressBus) -> Self {
+        CpuIterator { cpu, addr_bus }
+    }
+}
+
+impl<'a> Iterator for CpuIterator<'a> {
+    type Item = u8;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        Some(42)
+    }
 }
