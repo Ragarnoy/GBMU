902f549533677feab0aee2cafe0c41bf3a963879
Guilhem Smith <gsmith@student.42.fr>
2021-09-17 18:00:59 +0200

    Add scrolling registers to PPUReg.

--- gb-ppu/src/registers.rs
+++ gb-ppu/src/registers.rs
@@ -1,11 +1,13 @@
 mod control;
 mod palette;
 mod ppu_reg;
+mod scrolling;
 mod stat;
 
 pub use control::Control;
 pub use palette::Palette;
 pub use ppu_reg::PPURegisters;
+pub use scrolling::Scrolling;
 pub use stat::Stat;
 
 trait Register: Copy + Into<u8> + From<u8> {
@@ -16,6 +18,30 @@ trait Register: Copy + Into<u8> + From<u8> {
     }
 
     fn write(&mut self, value: u8) {
-        *self = (value & Self::WRITE_BITS | (*self).into() & !Self::WRITE_BITS).into()
+        *self = (value & Self::WRITE_BITS | (*self).into() & !Self::WRITE_BITS).into();
+    }
+}
+
+impl Register for u8 {
+    fn read(&self) -> u8 {
+        *self
+    }
+
+    fn write(&mut self, value: u8) {
+        *self = value;
+    }
+}
+
+trait RegisterArray<R: Register, const SIZE: usize>: std::ops::IndexMut<usize, Output = R> {
+    const WRITE_POS: [bool; SIZE] = [true; SIZE];
+
+    fn read(&self, pos: usize) -> u8 {
+        self[pos].read()
+    }
+
+    fn write(&mut self, pos: usize, value: u8) {
+        if pos < SIZE && Self::WRITE_POS[pos] {
+            self[pos].write(value);
+        }
     }
 }
--- gb-ppu/src/registers/ppu_reg.rs
+++ gb-ppu/src/registers/ppu_reg.rs
@@ -1,5 +1,4 @@
-use super::{Control, Register, Stat};
-use crate::error::{PPUError, PPUResult};
+use super::{Control, Register, RegisterArray, Scrolling, Stat};
 use crate::UNDEFINED_VALUE;
 use gb_bus::{Address, Error, FileOperation, IORegArea};
 use std::cell::RefCell;
@@ -8,6 +7,7 @@ use std::rc::Rc;
 pub struct PPURegisters {
     control: Rc<RefCell<Control>>,
     stat: Rc<RefCell<Stat>>,
+    scrolling: Rc<RefCell<Scrolling>>,
 }
 
 fn read_register_value(register: &Rc<RefCell<impl Register>>) -> u8 {
@@ -15,6 +15,19 @@ fn read_register_value(register: &Rc<RefCell<impl Register>>) -> u8 {
         Ok(register) => register.read(),
         Err(_) => {
             log::error!("failed ppu register read");
+            UNDEFINED_VALUE
+        }
+    }
+}
+
+fn read_register_value_at<R: Register, const S: usize>(
+    register: &Rc<RefCell<impl RegisterArray<R, S>>>,
+    pos: usize,
+) -> u8 {
+    match register.try_borrow() {
+        Ok(register) => register.read(pos),
+        Err(_) => {
+            log::error!("failed ppu register array read");
             UNDEFINED_VALUE
         }
     }
@@ -29,13 +42,27 @@ fn write_register_value(register: &Rc<RefCell<impl Register>>, value: u8) {
     }
 }
 
+fn write_register_value_at<R: Register, const S: usize>(
+    register: &Rc<RefCell<impl RegisterArray<R, S>>>,
+    pos: usize,
+    value: u8,
+) {
+    match register.try_borrow_mut() {
+        Ok(mut register) => register.write(pos, value),
+        Err(_) => {
+            log::error!("failed ppu register array write");
+        }
+    }
+}
+
 impl FileOperation<IORegArea> for PPURegisters {
     fn read(&self, addr: Box<dyn Address<IORegArea>>) -> Result<u8, Error> {
-        log::warn!("Unimplemented ppu registers read");
+        log::warn!("missing ppu registers read");
         match addr.area_type() {
             IORegArea::Lcd => match addr.get_address() {
-                0 => Ok(read_register_value(&self.control)),
-                1 => Ok(read_register_value(&self.stat)),
+                0x00 => Ok(read_register_value(&self.control)),
+                0x01 => Ok(read_register_value(&self.stat)),
+                pos @ 0x02..=0x05 => Ok(read_register_value_at(&self.scrolling, pos - 0x02)),
                 _ => Ok(UNDEFINED_VALUE),
             },
             IORegArea::VRamBank => Ok(UNDEFINED_VALUE),
@@ -46,12 +73,13 @@ impl FileOperation<IORegArea> for PPURegisters {
     }
 
     fn write(&mut self, v: u8, addr: Box<dyn Address<IORegArea>>) -> Result<(), Error> {
-        log::warn!("Unimplemented ppu registers write");
+        log::warn!("missing ppu registers write");
         match addr.area_type() {
             IORegArea::Lcd => {
                 match addr.get_address() {
-                    0 => write_register_value(&self.control, v),
-                    1 => write_register_value(&self.stat, v),
+                    0x00 => write_register_value(&self.control, v),
+                    0x01 => write_register_value(&self.stat, v),
+                    pos @ 0x02..=0x05 => write_register_value_at(&self.scrolling, pos - 0x02, v),
                     _ => {}
                 };
                 Ok(())
