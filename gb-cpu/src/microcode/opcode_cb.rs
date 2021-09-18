c6257b07cb3c4d59e7517bcdbae48afe1def38f9
firelight.flagboy <firelight.flagboy@gmail.com>
2021-09-18 12:08:48 +0200

    add bit opcode

--- gb-cpu/src/microcode/opcode_cb.rs
+++ gb-cpu/src/microcode/opcode_cb.rs
@@ -130,4 +130,133 @@ pub enum OpcodeCB {
     SrlHL = 0x3e,
     /// `SRL A`: shift `A` right. Most Smallest Bit set to 0. 2m
     SrlA = 0x3f,
+
+    /// `BIT0 B`: test if bit 0 is set on `B`. 2m
+    Bit0B = 0x40,
+    /// `BIT0 C`: test if bit 0 is set on `C`. 2m
+    Bit0C = 0x41,
+    /// `BIT0 D`: test if bit 0 is set on `D`. 2m
+    Bit0D = 0x42,
+    /// `BIT0 E`: test if bit 0 is set on `E`. 2m
+    Bit0E = 0x43,
+    /// `BIT0 H`: test if bit 0 is set on `H`. 2m
+    Bit0H = 0x44,
+    /// `BIT0 L`: test if bit 0 is set on `L`. 2m
+    Bit0L = 0x45,
+    /// `BIT0 (HL)`: test if bit 0 is set on `(HL)`. 4m
+    Bit0HL = 0x46,
+    /// `BIT0 A`: test if bit 0 is set on `A`. 2m
+    Bit0A = 0x47,
+    /// `BIT1 B`: test if bit 1 is set on `B`. 2m
+    Bit1B = 0x48,
+    /// `BIT1 C`: test if bit 1 is set on `C`. 2m
+    Bit1C = 0x49,
+    /// `BIT1 D`: test if bit 1 is set on `D`. 2m
+    Bit1D = 0x4a,
+    /// `BIT1 E`: test if bit 1 is set on `E`. 2m
+    Bit1E = 0x4b,
+    /// `BIT1 H`: test if bit 1 is set on `H`. 2m
+    Bit1H = 0x4c,
+    /// `BIT1 L`: test if bit 1 is set on `L`. 2m
+    Bit1L = 0x4d,
+    /// `BIT1 (HL)`: test if bit 1 is set on `(HL)`. 4m
+    Bit1HL = 0x4e,
+    /// `BIT1 A`: test if bit 1 is set on `A`. 2m
+    Bit1A = 0x4f,
+    /// `BIT2 B`: test if bit 2 is set on `B`. 2m
+    Bit2B = 0x50,
+    /// `BIT2 C`: test if bit 2 is set on `C`. 2m
+    Bit2C = 0x51,
+    /// `BIT2 D`: test if bit 2 is set on `D`. 2m
+    Bit2D = 0x52,
+    /// `BIT2 E`: test if bit 2 is set on `E`. 2m
+    Bit2E = 0x53,
+    /// `BIT2 H`: test if bit 2 is set on `H`. 2m
+    Bit2H = 0x54,
+    /// `BIT2 L`: test if bit 2 is set on `L`. 2m
+    Bit2L = 0x55,
+    /// `BIT2 (HL)`: test if bit 2 is set on `(HL)`. 4m
+    Bit2HL = 0x56,
+    /// `BIT2 A`: test if bit 2 is set on `A`. 2m
+    Bit2A = 0x57,
+    /// `BIT3 B`: test if bit 3 is set on `B`. 2m
+    Bit3B = 0x58,
+    /// `BIT3 C`: test if bit 3 is set on `C`. 2m
+    Bit3C = 0x59,
+    /// `BIT3 D`: test if bit 3 is set on `D`. 2m
+    Bit3D = 0x5a,
+    /// `BIT3 E`: test if bit 3 is set on `E`. 2m
+    Bit3E = 0x5b,
+    /// `BIT3 H`: test if bit 3 is set on `H`. 2m
+    Bit3H = 0x5c,
+    /// `BIT3 L`: test if bit 3 is set on `L`. 2m
+    Bit3L = 0x5d,
+    /// `BIT3 (HL)`: test if bit 3 is set on `(HL)`. 4m
+    Bit3HL = 0x5e,
+    /// `BIT3 A`: test if bit 3 is set on `A`. 2m
+    Bit3A = 0x5f,
+    /// `BIT4 B`: test if bit 4 is set on `B`. 2m
+    Bit4B = 0x60,
+    /// `BIT4 C`: test if bit 4 is set on `C`. 2m
+    Bit4C = 0x61,
+    /// `BIT4 D`: test if bit 4 is set on `D`. 2m
+    Bit4D = 0x62,
+    /// `BIT4 E`: test if bit 4 is set on `E`. 2m
+    Bit4E = 0x63,
+    /// `BIT4 H`: test if bit 4 is set on `H`. 2m
+    Bit4H = 0x64,
+    /// `BIT4 L`: test if bit 4 is set on `L`. 2m
+    Bit4L = 0x65,
+    /// `BIT4 (HL)`: test if bit 4 is set on `(HL)`. 4m
+    Bit4HL = 0x66,
+    /// `BIT4 A`: test if bit 4 is set on `A`. 2m
+    Bit4A = 0x67,
+    /// `BIT5 B`: test if bit 5 is set on `B`. 2m
+    Bit5B = 0x68,
+    /// `BIT5 C`: test if bit 5 is set on `C`. 2m
+    Bit5C = 0x69,
+    /// `BIT5 D`: test if bit 5 is set on `D`. 2m
+    Bit5D = 0x6a,
+    /// `BIT5 E`: test if bit 5 is set on `E`. 2m
+    Bit5E = 0x6b,
+    /// `BIT5 H`: test if bit 5 is set on `H`. 2m
+    Bit5H = 0x6c,
+    /// `BIT5 L`: test if bit 5 is set on `L`. 2m
+    Bit5L = 0x6d,
+    /// `BIT5 (HL)`: test if bit 5 is set on `(HL)`. 4m
+    Bit5HL = 0x6e,
+    /// `BIT5 A`: test if bit 5 is set on `A`. 2m
+    Bit5A = 0x6f,
+    /// `BIT6 B`: test if bit 6 is set on `B`. 2m
+    Bit6B = 0x70,
+    /// `BIT6 C`: test if bit 6 is set on `C`. 2m
+    Bit6C = 0x71,
+    /// `BIT6 D`: test if bit 6 is set on `D`. 2m
+    Bit6D = 0x72,
+    /// `BIT6 E`: test if bit 6 is set on `E`. 2m
+    Bit6E = 0x73,
+    /// `BIT6 H`: test if bit 6 is set on `H`. 2m
+    Bit6H = 0x74,
+    /// `BIT6 L`: test if bit 6 is set on `L`. 2m
+    Bit6L = 0x75,
+    /// `BIT6 (HL)`: test if bit 6 is set on `(HL)`. 4m
+    Bit6HL = 0x76,
+    /// `BIT6 A`: test if bit 6 is set on `A`. 2m
+    Bit6A = 0x77,
+    /// `BIT7 B`: test if bit 7 is set on `B`. 2m
+    Bit7B = 0x78,
+    /// `BIT7 C`: test if bit 7 is set on `C`. 2m
+    Bit7C = 0x79,
+    /// `BIT7 D`: test if bit 7 is set on `D`. 2m
+    Bit7D = 0x7a,
+    /// `BIT7 E`: test if bit 7 is set on `E`. 2m
+    Bit7E = 0x7b,
+    /// `BIT7 H`: test if bit 7 is set on `H`. 2m
+    Bit7H = 0x7c,
+    /// `BIT7 L`: test if bit 7 is set on `L`. 2m
+    Bit7L = 0x7d,
+    /// `BIT7 (HL)`: test if bit 7 is set on `(HL)`. 4m
+    Bit7HL = 0x7e,
+    /// `BIT7 A`: test if bit 7 is set on `A`. 2m
+    Bit7A = 0x7f,
 }
