34aeeb432e9be4a83aff14d1c6ff156d85eafb5e
firelight.flagboy <firelight.flagboy@gmail.com>
2021-09-20 09:18:14 +0200

    read now push byte into cache

--- gb-cpu/src/microcode/read.rs
+++ gb-cpu/src/microcode/read.rs
@@ -1,9 +1,8 @@
-use super::{
-    controller::{MicrocodeController, State},
-    Continuum,
-};
+use super::{Continuum, MicrocodeController, State};
 use gb_bus::Bus;
 
 pub fn read<B: Bus<u8>>(ctl: &mut MicrocodeController<B>, state: &mut State<B>) -> Continuum {
-    state.bus.
+    let byte = state.read();
+    ctl.push(byte);
+    Continuum::Ok
 }
