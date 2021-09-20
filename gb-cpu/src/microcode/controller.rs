91cb673edd5e5a96fa21c1e83834f261fd73d814
firelight.flagboy <firelight.flagboy@gmail.com>
2021-09-20 09:37:51 +0200

    add comment + implement push_actions

--- gb-cpu/src/microcode/controller.rs
+++ gb-cpu/src/microcode/controller.rs
@@ -24,6 +24,7 @@ pub struct MicrocodeController<B: Bus<u8>> {
     pub opcode: Option<OpcodeType>,
     /// Microcode actions, their role is to execute one step of an Opcode
     /// Each Actions take at most 1 `M-Cycle`
+    /// Used like a LOFI queue
     actions: Vec<ActionFn<B>>,
     /// Cache use for microcode action
     cache: Vec<u8>,
@@ -57,14 +58,28 @@ impl<B: Bus<u8>> MicrocodeController<B> {
         }
     }
 
+    /// Push the action a the back of the queue.
+    /// The last pushed action will be the first to be executed
     pub fn push_action(&mut self, action: ActionFn<B>) {
         self.actions.push(action);
     }
 
+    /// Push the actions in the queue.
+    /// The actions while be push in the queue in a way that allow the first action of the slice
+    /// to be executed in first.
+    pub fn push_actions(&mut self, actions: &[ActionFn<B>]) {
+        for action in actions.iter().rev() {
+            self.push_action(*action)
+        }
+    }
+
+    /// Push the `byte` to the cache.
+    /// The last `byte` pushed will be the first accessed by `MicrocodeController::pop`.
     pub fn push(&mut self, byte: u8) {
         self.cache.push(byte)
     }
 
+    /// Pop the last push `byte` from the cache.
     pub fn pop(&mut self) -> u8 {
         self.cache.pop().expect("not enough value stored in cache")
     }
