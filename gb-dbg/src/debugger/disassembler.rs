639f1ccba6ff942d3afc7cfaa54f1b6b46b7460f
Tiago Lernould <tiagolernould@gmail.com>
2021-09-24 14:42:09 +0200

    Implement InstructionIterator with Iterator Trait

--- gb-dbg/src/debugger/disassembler.rs
+++ gb-dbg/src/debugger/disassembler.rs
@@ -1,9 +1,10 @@
 use egui::{Color32, Label, Ui, Vec2};
+use crate::dbg_interfaces::MemoryDebugOperations;
 
 pub struct DisassemblyViewer;
 
 impl DisassemblyViewer {
-    pub fn draw(&self, ui: &mut Ui, pc: u16) {
+    pub fn draw<MEM: MemoryDebugOperations>(&self, ui: &mut Ui, pc: u16, memory: &MEM) {
         ui.label(Label::new("Disassembler").text_color(Color32::WHITE));
         egui::CollapsingHeader::new("🛠 Options")
             .id_source(55)
@@ -19,6 +20,7 @@ impl DisassemblyViewer {
                     ui.label(egui::Label::new("Instruction").text_color(Color32::WHITE));
                     ui.label(egui::Label::new("Data").text_color(Color32::WHITE));
                     ui.end_row();
+                    let it = (0..8).iter();
                     for row in 0..8 {
                         ui.label(egui::Label::new(format!("0x{:04X}", pc + row)));
                         ui.label(egui::Label::new("add x and y"));
@@ -28,5 +30,21 @@ impl DisassemblyViewer {
                     ui.end_row();
                 });
         });
+    }
+}
+
+struct InstructionIterator<'a, MEM: MemoryDebugOperations> {
+    start_address: u16,
+    address_offset: u16,
+    memory: &'a MEM,
+}
+
+impl<'a, MEM: MemoryDebugOperations> Iterator for InstructionIterator<'a, MEM> {
+    type Item = u8;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let address: u16 = self.start_address + self.address_offset;
+        self.address_offset += 1;
+        Some(self.memory.read(address))
     }
 }
