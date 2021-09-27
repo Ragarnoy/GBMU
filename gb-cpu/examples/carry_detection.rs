e9708f07d75b675afc6ccd3916f445e3c17b1bba
firelight.flagboy <firelight.flagboy@gmail.com>
2021-09-27 20:43:20 +0200

    wip add example to detect carry

---
+++ gb-cpu/examples/carry_detection.rs
@@ -0,0 +1,47 @@
+fn borrow() {
+    println!("borrow:");
+    println!(" x  y  r ba bb bc  b");
+    for x in 0..4 {
+        for y in 0..4 {
+            let r = (x - y) & 0b11;
+            let x2 = x >> 1;
+            let y2 = y >> 1;
+            let r2 = r >> 1;
+            let ba = !x2 & y2;
+            let bb = y2 & r2;
+            let bc = r2 & !x2;
+            let b = ba | bb | bc;
+
+            println!(
+                "{:02b} {:02b} {:02b}  {:b}  {:b}  {:b}  {:b}",
+                x, y, r, ba, bb, bc, b
+            );
+        }
+    }
+}
+
+fn carry() {
+    println!("carry:");
+    println!(" x  y  r ba bb bc  b");
+    for x in 0..4 {
+        for y in 0..4 {
+            let r = (x + y) & 0b11;
+            let x2 = x >> 1;
+            let y2 = y >> 1;
+            let r2 = r >> 1;
+            let ba = !x2 & y2;
+            let bb = y2 & r2;
+            let bc = r2 & !x2;
+            let b = ba | bb | bc;
+
+            println!(
+                "{:02b} {:02b} {:02b}  {:b}  {:b}  {:b}  {:b}",
+                x, y, r, ba, bb, bc, b
+            );
+        }
+    }
+}
+fn main() {
+    borrow();
+    carry();
+}
