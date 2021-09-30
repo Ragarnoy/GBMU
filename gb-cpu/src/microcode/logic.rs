547bd936659ba2bdffe295e97e8564db6fc85984
firelight.flagboy <firelight.flagboy@gmail.com>
2021-09-30 17:47:30 +0200

    extract logic to get u8 from ident

--- gb-cpu/src/microcode/logic.rs
+++ gb-cpu/src/microcode/logic.rs
@@ -4,24 +4,7 @@ use super::{
 use crate::interfaces::{Read8BitsReg, WriteFlagReg};
 
 pub fn cp(ctl: &mut MicrocodeController, state: &mut State) -> MicrocodeFlow {
-    let value = {
-        use ident::{Ident, Reg8};
-
-        let src = ctl.get_src();
-        match src {
-            Ident::Reg8(r8) => match r8 {
-                Reg8::A => state.regs.a(),
-                Reg8::B => state.regs.b(),
-                Reg8::C => state.regs.c(),
-                Reg8::D => state.regs.d(),
-                Reg8::E => state.regs.e(),
-                Reg8::H => state.regs.h(),
-                Reg8::L => state.regs.l(),
-            },
-            Ident::Raw8 | Ident::IndirectHL8 => ctl.pop(),
-            _ => panic!("CP don't handle source of type {:?}", src),
-        }
-    };
+    let value = ident::get_u8_from_ident(*ctl.get_src(), state, ctl);
     let (_, flag) = sub_components(state.regs.a(), value);
     state.regs.set_subtraction(true);
     state.regs.set_half_carry(flag.half_carry);
