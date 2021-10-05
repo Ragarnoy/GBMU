e0f98046a521267aa7b785a7d2fdde544597e99f
Guilhem Smith <gsmith@student.42.fr>
2021-10-05 14:00:48 +0200

    Add oam fetch to state update.

--- gb-ppu/src/drawing/state.rs
+++ gb-ppu/src/drawing/state.rs
@@ -10,10 +10,9 @@ impl State {
     const LINE_COUNT: u32 = 154;
     const STEP_COUNT: u32 = 456;
 
-    const DRAW_END: u32 = 143;
     const HBLANK_MIN_START: u32 = 252;
-
     const VBLANK_START: u32 = 144;
+    const PIXEL_DRAWING_START: u32 = 80;
 
     pub fn new() -> Self {
         State {
@@ -27,6 +26,7 @@ impl State {
         match self {
             Mode::HBlank => self.update_hblank(),
             Mode::VBlank => self.update_vblank(),
+            Mode::OAMFecth => self.update_oam_fetch(),
             _ => log::error!("unimplemented"),
         }
         self.step = (self.step + 1) % STEP_COUNT;
@@ -55,25 +55,21 @@ impl State {
     fn update_vblank(&mut self) {
         match (self.line, self.step) {
             (line, _) if line < Self::VBLANK_START => log::error!("VBlank reached on draw line"),
-            (Self::LINE_COUNT - 1, STEP_COUNT - 1) => {
+            (Self::LINE_COUNT - 1, Self::STEP_COUNT - 1) => {
                 self::mode = Mode::OAMFetch
             },
             _ => {}
         }
     }
-}
 
-fn leave_hblank(line: u32) -> Mode {
-    match line {
-        0..=Mode::DRAW_END => Mode::OAMFetch,
-        Mode::VBLANK_START => Mode::VBlank,
-        l => {
-            log::error!("HBlank ran on VBlank period");
-            if l >= Mode::LINE_COUNT {
-                Mode::OAMFetch
-            } else {
-                Mode::VBlank
-            }
+    fn update_oam_fetch(&mut self) {
+        match (self.line, self.step) {
+            (line, _) if line >= Self::VBLANK_START => log::error!("OAMFetch reached on VBlank period"),
+            (_, step) if step >= Self::PIXEL_DRAWING_START => log::error!("OAMFetch reached on PixelDrawing period"),
+            (_, Self::PIXEL_DRAWING_START - 1) => {
+                self::mode = Mode::PixelDrawing
+            },
+            _ => {}
         }
     }
 }
