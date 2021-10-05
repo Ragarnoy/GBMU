5f2bd34f26722883936a205935680f3082daa9e7
firelight.flagboy <firelight.flagboy@gmail.com>
2021-10-05 10:55:11 +0200

    remove unused from ident

--- gb-cpu/src/microcode/ident.rs
+++ gb-cpu/src/microcode/ident.rs
@@ -1,5 +1,3 @@
-use super::{MicrocodeController, State};
-use crate::interfaces::Read8BitsReg;
 use std::convert::From;
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
@@ -39,21 +37,5 @@ impl From<Reg8> for Ident {
 impl From<Reg16> for Ident {
     fn from(r16: Reg16) -> Self {
         Self::Reg16(r16)
-    }
-}
-
-pub fn get_u8_from_ident(ident: Ident, state: &mut State, ctl: &mut MicrocodeController) -> u8 {
-    match ident {
-        Ident::Reg8(r8) => match r8 {
-            Reg8::A => state.regs.a(),
-            Reg8::B => state.regs.b(),
-            Reg8::C => state.regs.c(),
-            Reg8::D => state.regs.d(),
-            Reg8::E => state.regs.e(),
-            Reg8::H => state.regs.h(),
-            Reg8::L => state.regs.l(),
-        },
-        Ident::Raw8 | Ident::IndirectHL8 => ctl.pop(),
-        _ => panic!("cannot get an u8 with type {:?}", ident),
     }
 }
