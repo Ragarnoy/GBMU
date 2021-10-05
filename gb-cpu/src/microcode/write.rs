050aab03f6a60b16ef74427c263cf5274e06e6d0
firelight.flagboy <firelight.flagboy@gmail.com>
2021-10-05 12:12:46 +0200

    remove comment

--- gb-cpu/src/microcode/write.rs
+++ gb-cpu/src/microcode/write.rs
@@ -1,14 +1,6 @@
 use super::{MicrocodeController, MicrocodeFlow, State, OK_CONSUME_CYCLE, OK_PLAY_NEXT_ACTION};
 use crate::interfaces::Write8BitsReg;
 
-// /// Write the byte at the top of the stack to the address `HL`
-// pub fn write_hl(ctl: &mut MicrocodeController, state: &mut State) -> MicrocodeFlow {
-//     let byte = ctl.pop();
-//     log::trace!("[microcode] write {:02x} at (HL)", byte);
-//     state.write_hl(byte);
-//     OK_CONSUME_CYCLE
-// }
-
 pub fn a(ctl: &mut MicrocodeController, state: &mut State) -> MicrocodeFlow {
     state.regs.set_a(ctl.pop());
     OK_PLAY_NEXT_ACTION
