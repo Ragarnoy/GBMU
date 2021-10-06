3b7e01612dc1bc90b5771111f4bbbce2585b18ee
FirelightFlagboy <30697622+FirelightFlagboy@users.noreply.github.com>
2021-10-06 15:35:22 +0200

    Feature/impl opcode ldi (#159)
    * add action to increase `HL`
    
    * implement opcode ldi

--- gb-cpu/src/microcode/fetch.rs
+++ gb-cpu/src/microcode/fetch.rs
@@ -120,6 +120,9 @@ pub fn fetch(ctl: &mut MicrocodeController, state: &mut State) -> MicrocodeFlow 
                 Opcode::LdHL16 => ctl.push_actions(&[read::byte, read::byte, write::hl]),
                 Opcode::LdSP16 => ctl.push_actions(&[read::byte, read::byte, write::sp]),
 
+                Opcode::LdiHLA => ctl.push_actions(&[read::a, read::hl, write::ind, inc::hl]),
+                Opcode::LdiAHL => ctl.push_actions(&[read::hl, read::ind, write::a, inc::hl]),
+
                 Opcode::LddHLA => ctl.push_actions(&[read::a, read::hl, write::ind, dec::hl]),
                 Opcode::LddAHL => ctl.push_actions(&[read::hl, read::ind, write::a, dec::hl]),
 
