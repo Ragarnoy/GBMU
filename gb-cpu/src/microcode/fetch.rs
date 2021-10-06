cb4e701b7affc0afad85dc0695e1c896429234a4
firelight.flagboy <firelight.flagboy@gmail.com>
2021-10-06 15:22:27 +0200

    impl opcode `ld (u16), r8`

--- gb-cpu/src/microcode/fetch.rs
+++ gb-cpu/src/microcode/fetch.rs
@@ -120,6 +120,8 @@ pub fn fetch(ctl: &mut MicrocodeController, state: &mut State) -> MicrocodeFlow 
                 Opcode::LdHL16 => ctl.push_actions(&[read::byte, read::byte, write::hl]),
                 Opcode::LdSP16 => ctl.push_actions(&[read::byte, read::byte, write::sp]),
 
+                Opcode::Ld16A => ctl.push_actions(&[read::a, read::byte, read::byte, write::ind]),
+
                 Opcode::Nop => &mut ctl,
                 Opcode::PrefixCb => ctl.push_action(fetch_cb),
                 _ => todo!("unimplemented opcode {:?}", opcode),
