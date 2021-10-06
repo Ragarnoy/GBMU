76b4c296a3a64a68ed101c4e7307a48337b423a7
Pierre Lamusse <plamusse42@gmail.com>
2021-10-06 13:53:32 +0200

    Bit => [fix] set flag values

--- gb-cpu/src/microcode/bitwise.rs
+++ gb-cpu/src/microcode/bitwise.rs
@@ -1,5 +1,5 @@
 use super::{MicrocodeController, MicrocodeFlow, State, OK_PLAY_NEXT_ACTION};
-use crate::interfaces::{ReadFlagReg, WriteFlagReg};
+use crate::interfaces::WriteFlagReg;
 
 pub fn bit_0(ctl: &mut MicrocodeController, state: &mut State) -> MicrocodeFlow {
     let value = ctl.pop();
@@ -7,7 +7,6 @@ pub fn bit_0(ctl: &mut MicrocodeController, state: &mut State) -> MicrocodeFlow 
 
     state.regs.set_zero(operation == 0);
     state.regs.set_subtraction(false);
-    state.regs.set_half_carry(true || false);
-    state.regs.set_carry(state.regs.carry());
+    state.regs.set_half_carry(true);
     OK_PLAY_NEXT_ACTION
 }
