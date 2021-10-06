222a61e9303999aab28ce3f526bb4ae5e2f0776a
Pierre Lamusse <plamusse42@gmail.com>
2021-10-06 14:04:36 +0200

    Bit => [add] generic method to read any bit of the given register

--- gb-cpu/src/microcode/bitwise.rs
+++ gb-cpu/src/microcode/bitwise.rs
@@ -1,12 +1,16 @@
 use super::{MicrocodeController, MicrocodeFlow, State, OK_PLAY_NEXT_ACTION};
 use crate::interfaces::WriteFlagReg;
 
-pub fn bit_0(ctl: &mut MicrocodeController, state: &mut State) -> MicrocodeFlow {
+fn read_bit(ctl: &mut MicrocodeController, state: &mut State, bit: u8) {
     let value = ctl.pop();
-    let operation = value & 1;
+    let operation = (value >> bit) & 1;
 
     state.regs.set_zero(operation == 0);
     state.regs.set_subtraction(false);
     state.regs.set_half_carry(true);
+}
+
+pub fn bit_0(ctl: &mut MicrocodeController, state: &mut State) -> MicrocodeFlow {
+    read_bit(ctl, state, 0);
     OK_PLAY_NEXT_ACTION
 }
