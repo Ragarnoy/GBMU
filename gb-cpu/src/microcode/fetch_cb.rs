ed878ec1d24e6a041d5fb8843bce3f7e6608a60f
Pierre Lamusse <plamusse42@gmail.com>
2021-10-06 14:51:01 +0200

    Bit => [add] indirect read cycle

--- gb-cpu/src/microcode/fetch_cb.rs
+++ gb-cpu/src/microcode/fetch_cb.rs
@@ -21,7 +21,7 @@ pub fn fetch_cb(ctl: &mut MicrocodeController, state: &mut State) -> MicrocodeFl
                 OpcodeCB::Bit0E => ctl.push_actions(&[read::e, bitwise::bit_0]),
                 OpcodeCB::Bit0H => ctl.push_actions(&[read::h, bitwise::bit_0]),
                 OpcodeCB::Bit0L => ctl.push_actions(&[read::l, bitwise::bit_0]),
-                OpcodeCB::Bit0HL => ctl.push_actions(&[read::hl, bitwise::bit_0]),
+                OpcodeCB::Bit0HL => ctl.push_actions(&[read::hl, read::ind, bitwise::bit_0]),
                 OpcodeCB::Bit0A => ctl.push_actions(&[read::a, bitwise::bit_0]),
 
                 OpcodeCB::Bit1B => ctl.push_actions(&[read::b, bitwise::bit_1]),
@@ -30,7 +30,7 @@ pub fn fetch_cb(ctl: &mut MicrocodeController, state: &mut State) -> MicrocodeFl
                 OpcodeCB::Bit1E => ctl.push_actions(&[read::e, bitwise::bit_1]),
                 OpcodeCB::Bit1H => ctl.push_actions(&[read::h, bitwise::bit_1]),
                 OpcodeCB::Bit1L => ctl.push_actions(&[read::l, bitwise::bit_1]),
-                OpcodeCB::Bit1HL => ctl.push_actions(&[read::hl, bitwise::bit_1]),
+                OpcodeCB::Bit1HL => ctl.push_actions(&[read::hl, read::ind, bitwise::bit_1]),
                 OpcodeCB::Bit1A => ctl.push_actions(&[read::a, bitwise::bit_1]),
 
                 OpcodeCB::Bit2B => ctl.push_actions(&[read::b, bitwise::bit_2]),
@@ -39,7 +39,7 @@ pub fn fetch_cb(ctl: &mut MicrocodeController, state: &mut State) -> MicrocodeFl
                 OpcodeCB::Bit2E => ctl.push_actions(&[read::e, bitwise::bit_2]),
                 OpcodeCB::Bit2H => ctl.push_actions(&[read::h, bitwise::bit_2]),
                 OpcodeCB::Bit2L => ctl.push_actions(&[read::l, bitwise::bit_2]),
-                OpcodeCB::Bit2HL => ctl.push_actions(&[read::hl, bitwise::bit_2]),
+                OpcodeCB::Bit2HL => ctl.push_actions(&[read::hl, read::ind, bitwise::bit_2]),
                 OpcodeCB::Bit2A => ctl.push_actions(&[read::a, bitwise::bit_2]),
 
                 OpcodeCB::Bit3B => ctl.push_actions(&[read::b, bitwise::bit_3]),
@@ -48,7 +48,7 @@ pub fn fetch_cb(ctl: &mut MicrocodeController, state: &mut State) -> MicrocodeFl
                 OpcodeCB::Bit3E => ctl.push_actions(&[read::e, bitwise::bit_3]),
                 OpcodeCB::Bit3H => ctl.push_actions(&[read::h, bitwise::bit_3]),
                 OpcodeCB::Bit3L => ctl.push_actions(&[read::l, bitwise::bit_3]),
-                OpcodeCB::Bit3HL => ctl.push_actions(&[read::hl, bitwise::bit_3]),
+                OpcodeCB::Bit3HL => ctl.push_actions(&[read::hl, read::ind, bitwise::bit_3]),
                 OpcodeCB::Bit3A => ctl.push_actions(&[read::a, bitwise::bit_3]),
 
                 OpcodeCB::Bit4B => ctl.push_actions(&[read::b, bitwise::bit_4]),
@@ -57,7 +57,7 @@ pub fn fetch_cb(ctl: &mut MicrocodeController, state: &mut State) -> MicrocodeFl
                 OpcodeCB::Bit4E => ctl.push_actions(&[read::e, bitwise::bit_4]),
                 OpcodeCB::Bit4H => ctl.push_actions(&[read::h, bitwise::bit_4]),
                 OpcodeCB::Bit4L => ctl.push_actions(&[read::l, bitwise::bit_4]),
-                OpcodeCB::Bit4HL => ctl.push_actions(&[read::hl, bitwise::bit_4]),
+                OpcodeCB::Bit4HL => ctl.push_actions(&[read::hl, read::ind, bitwise::bit_4]),
                 OpcodeCB::Bit4A => ctl.push_actions(&[read::a, bitwise::bit_4]),
 
                 OpcodeCB::Bit5B => ctl.push_actions(&[read::b, bitwise::bit_5]),
@@ -66,7 +66,7 @@ pub fn fetch_cb(ctl: &mut MicrocodeController, state: &mut State) -> MicrocodeFl
                 OpcodeCB::Bit5E => ctl.push_actions(&[read::e, bitwise::bit_5]),
                 OpcodeCB::Bit5H => ctl.push_actions(&[read::h, bitwise::bit_5]),
                 OpcodeCB::Bit5L => ctl.push_actions(&[read::l, bitwise::bit_5]),
-                OpcodeCB::Bit5HL => ctl.push_actions(&[read::hl, bitwise::bit_5]),
+                OpcodeCB::Bit5HL => ctl.push_actions(&[read::hl, read::ind, bitwise::bit_5]),
                 OpcodeCB::Bit5A => ctl.push_actions(&[read::a, bitwise::bit_5]),
 
                 OpcodeCB::Bit6B => ctl.push_actions(&[read::b, bitwise::bit_6]),
@@ -75,7 +75,7 @@ pub fn fetch_cb(ctl: &mut MicrocodeController, state: &mut State) -> MicrocodeFl
                 OpcodeCB::Bit6E => ctl.push_actions(&[read::e, bitwise::bit_6]),
                 OpcodeCB::Bit6H => ctl.push_actions(&[read::h, bitwise::bit_6]),
                 OpcodeCB::Bit6L => ctl.push_actions(&[read::l, bitwise::bit_6]),
-                OpcodeCB::Bit6HL => ctl.push_actions(&[read::hl, bitwise::bit_6]),
+                OpcodeCB::Bit6HL => ctl.push_actions(&[read::hl, read::ind, bitwise::bit_6]),
                 OpcodeCB::Bit6A => ctl.push_actions(&[read::a, bitwise::bit_6]),
 
                 OpcodeCB::Bit7B => ctl.push_actions(&[read::b, bitwise::bit_7]),
@@ -84,7 +84,7 @@ pub fn fetch_cb(ctl: &mut MicrocodeController, state: &mut State) -> MicrocodeFl
                 OpcodeCB::Bit7E => ctl.push_actions(&[read::e, bitwise::bit_7]),
                 OpcodeCB::Bit7H => ctl.push_actions(&[read::h, bitwise::bit_7]),
                 OpcodeCB::Bit7L => ctl.push_actions(&[read::l, bitwise::bit_7]),
-                OpcodeCB::Bit7HL => ctl.push_actions(&[read::hl, bitwise::bit_7]),
+                OpcodeCB::Bit7HL => ctl.push_actions(&[read::hl, read::ind, bitwise::bit_7]),
                 OpcodeCB::Bit7A => ctl.push_actions(&[read::a, bitwise::bit_7]),
                 _ => todo!("unimplemented opcode {:?}", opcode),
             };
