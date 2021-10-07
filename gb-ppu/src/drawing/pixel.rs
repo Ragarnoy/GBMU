84263d6677503e4dbbcaa3bf40f2c9fcbdaf61be
guilhem smith <gsmith@student.42.fr>
2021-10-07 11:58:01 +0200

    Allow mutability of palette.

--- gb-ppu/src/drawing/pixel.rs
+++ gb-ppu/src/drawing/pixel.rs
@@ -1,15 +1,32 @@
 use crate::registers::Palette;
 use crate::Color;
+use std::cell::Cell;
+use std::rc::Rc;
 
-pub struct Pixel<'p> {
-    color: u8,
-    palette: &'p Palette,
+pub struct Pixel {
+    pub color: u8,
+    pub palette: Rc<Cell<Palette>>,
     _sprite_priority: Option<u8>,
     _background_priority: bool,
 }
 
-impl<'p> From<Pixel<'p>> for Color {
+impl Pixel {
+    pub fn new(color: u8, palette: Rc<Cell<Palette>>, _background_priority: bool) -> Self {
+        Pixel {
+            color,
+            palette,
+            _sprite_priority: None,
+            _background_priority,
+        }
+    }
+}
+
+impl From<Pixel> for Color {
     fn from(pixel: Pixel) -> Color {
-        pixel.palette.get_color(pixel.color).unwrap_or_default()
+        pixel
+            .palette
+            .get()
+            .get_color(pixel.color)
+            .unwrap_or_default()
     }
 }
