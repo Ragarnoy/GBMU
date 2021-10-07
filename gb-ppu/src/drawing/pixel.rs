bc5f682c1667680a3696cf63e802d518e6be659c
guilhem smith <gsmith@student.42.fr>
2021-10-07 13:45:05 +0200

    Add pixel mixing function.

--- gb-ppu/src/drawing/pixel.rs
+++ gb-ppu/src/drawing/pixel.rs
@@ -6,17 +6,30 @@ use std::rc::Rc;
 pub struct Pixel {
     pub color: u8,
     pub palette: Rc<Cell<Palette>>,
+    is_sprite: bool,
     _sprite_priority: Option<u8>,
     _background_priority: bool,
 }
 
 impl Pixel {
-    pub fn new(color: u8, palette: Rc<Cell<Palette>>, _background_priority: bool) -> Self {
+    pub fn new(
+        color: u8,
+        palette: Rc<Cell<Palette>>,
+        is_sprite: bool,
+        _background_priority: bool,
+    ) -> Self {
         Pixel {
             color,
             palette,
+            is_sprite,
             _sprite_priority: None,
             _background_priority,
+        }
+    }
+
+    pub fn mix(&mut self, other: Pixel) {
+        if !self.is_sprite && other.is_sprite && other.color != 0 {
+            *self = other;
         }
     }
 }
