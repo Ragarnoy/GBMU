a2e29ee351f86c99f166d95cdba3543d3eefbce8
guilhem smith <gsmith@student.42.fr>
2021-10-08 19:18:49 +0200

    Store palette sprite/background type in palette.

--- gb-ppu/src/drawing/pixel.rs
+++ gb-ppu/src/drawing/pixel.rs
@@ -6,30 +6,24 @@ use std::rc::Rc;
 pub struct Pixel {
     pub color: u8,
     pub palette: Rc<Cell<Palette>>,
-    is_sprite: bool,
     _sprite_priority: Option<u8>,
     background_priority: bool,
 }
 
 impl Pixel {
-    pub fn new(
-        color: u8,
-        palette: Rc<Cell<Palette>>,
-        is_sprite: bool,
-        background_priority: bool,
-    ) -> Self {
+    pub fn new(color: u8, palette: Rc<Cell<Palette>>, background_priority: bool) -> Self {
         Pixel {
             color,
             palette,
-            is_sprite,
             _sprite_priority: None,
             background_priority,
         }
     }
 
     pub fn mix(&mut self, other: Pixel) {
-        if !self.is_sprite && (!other.background_priority && other.is_sprite && other.color != 0)
-            || (other.background_priority && other.is_sprite && self.color != 0)
+        if !self.palette.get().is_sprite()
+            && (!other.background_priority && other.palette.get().is_sprite() && other.color != 0)
+            || (other.background_priority && other.palette.get().is_sprite() && self.color != 0)
         {
             *self = other;
         }
