3139e8241168fbb7ae1927a658883bea3ddc9a8a
Pierre Lamusse <plamusse42@gmail.com>
2021-10-08 13:43:25 +0200

    implement rlca

--- gb-cpu/src/microcode/fetch.rs
+++ gb-cpu/src/microcode/fetch.rs
@@ -433,6 +433,7 @@ pub fn fetch(ctl: &mut MicrocodeController, state: &mut State) -> MicrocodeFlow 
 
                 Opcode::Daa => ctl.push_actions(&[read::a, arithmetic::daa, write::a]),
                 Opcode::Rla => ctl.push_actions(&[read::a, bitwise::rl, write::a]),
+                Opcode::RlcA => ctl.push_actions(&[read::a, bitwise::rlc, write::a]),
 
                 Opcode::Nop => &mut ctl,
                 Opcode::PrefixCb => ctl.push_action(fetch_cb),
