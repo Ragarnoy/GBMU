d15c887ce5939b8a5a60edf1527c2c3e2f272437
firelight.flagboy <firelight.flagboy@gmail.com>
2021-10-09 11:52:02 +0200

    correct type on cgb

--- gb-bus/src/working_ram.rs
+++ gb-bus/src/working_ram.rs
@@ -7,19 +7,19 @@ pub const DMG_MAX_BANKS: usize = 2;
 pub struct WorkingRam {
     banks: Vec<[u8; RAM_BANK_SIZE]>,
     selected_bank: u8,
-    enable_cbg_feature: bool,
+    enable_cgb_feature: bool,
 }
 
 impl WorkingRam {
-    pub fn new(enable_cbg_feature: bool) -> Self {
-        let banks = if enable_cbg_feature {
+    pub fn new(enable_cgb_feature: bool) -> Self {
+        let banks = if enable_cgb_feature {
             Vec::with_capacity(CGB_MAX_BANKS)
         } else {
             Vec::with_capacity(DMG_MAX_BANKS)
         };
         Self {
             banks,
-            enable_cbg_feature,
+            enable_cgb_feature,
             selected_bank: 1,
         }
     }
@@ -32,7 +32,7 @@ impl FileOperation<Area> for WorkingRam {
             0..=0xfff => self.banks[0][address] = value,
             0x1000..=0x1fff => {
                 let address = address - 0x1000;
-                let index = if self.enable_cbg_feature {
+                let index = if self.enable_cgb_feature {
                     self.selected_bank as usize
                 } else {
                     1
@@ -50,7 +50,7 @@ impl FileOperation<Area> for WorkingRam {
             0..=0xfff => Ok(self.banks[0][address]),
             0x1000..=0x1fff => {
                 let address = address - 0x1000;
-                let index = if self.enable_cbg_feature {
+                let index = if self.enable_cgb_feature {
                     self.selected_bank as usize
                 } else {
                     1
@@ -64,7 +64,7 @@ impl FileOperation<Area> for WorkingRam {
 
 impl FileOperation<IORegArea> for WorkingRam {
     fn write(&mut self, value: u8, addr: Box<dyn Address<IORegArea>>) -> Result<(), Error> {
-        if self.enable_cbg_feature {
+        if self.enable_cgb_feature {
             self.selected_bank = value.clamp(1, 7);
             Ok(())
         } else {
@@ -73,7 +73,7 @@ impl FileOperation<IORegArea> for WorkingRam {
     }
 
     fn read(&self, addr: Box<dyn Address<IORegArea>>) -> Result<u8, Error> {
-        if self.enable_cbg_feature {
+        if self.enable_cgb_feature {
             Ok(self.selected_bank)
         } else {
             Err(Error::new_segfault(addr))
