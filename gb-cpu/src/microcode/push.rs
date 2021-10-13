85c51a0d34e6702b2564a3967d609c0b961dba9c
Pierre Lamusse <plamusse42@gmail.com>
2021-10-13 06:48:25 +0200

    [fix] useless returned value in `push::addr_00XX` functions

--- gb-cpu/src/microcode/push.rs
+++ gb-cpu/src/microcode/push.rs
@@ -6,36 +6,28 @@ fn push_addr(ctl: &mut MicrocodeController, addr: u8) -> MicrocodeFlow {
 }
 
 pub fn addr_0000(ctl: &mut MicrocodeController, _state: &mut State) -> MicrocodeFlow {
-    push_addr(ctl, 0x00);
-    OK_PLAY_NEXT_ACTION
+    push_addr(ctl, 0x00)
 }
 
 pub fn addr_0008(ctl: &mut MicrocodeController, _state: &mut State) -> MicrocodeFlow {
-    push_addr(ctl, 0x08);
-    OK_PLAY_NEXT_ACTION
+    push_addr(ctl, 0x08)
 }
 
 pub fn addr_0010(ctl: &mut MicrocodeController, _state: &mut State) -> MicrocodeFlow {
-    push_addr(ctl, 0x10);
-    OK_PLAY_NEXT_ACTION
+    push_addr(ctl, 0x10)
 }
 pub fn addr_0018(ctl: &mut MicrocodeController, _state: &mut State) -> MicrocodeFlow {
-    push_addr(ctl, 0x18);
-    OK_PLAY_NEXT_ACTION
+    push_addr(ctl, 0x18)
 }
 pub fn addr_0020(ctl: &mut MicrocodeController, _state: &mut State) -> MicrocodeFlow {
-    push_addr(ctl, 0x20);
-    OK_PLAY_NEXT_ACTION
+    push_addr(ctl, 0x20)
 }
 pub fn addr_0028(ctl: &mut MicrocodeController, _state: &mut State) -> MicrocodeFlow {
-    push_addr(ctl, 0x28);
-    OK_PLAY_NEXT_ACTION
+    push_addr(ctl, 0x28)
 }
 pub fn addr_0030(ctl: &mut MicrocodeController, _state: &mut State) -> MicrocodeFlow {
-    push_addr(ctl, 0x30);
-    OK_PLAY_NEXT_ACTION
+    push_addr(ctl, 0x30)
 }
 pub fn addr_0038(ctl: &mut MicrocodeController, _state: &mut State) -> MicrocodeFlow {
-    push_addr(ctl, 0x38);
-    OK_PLAY_NEXT_ACTION
+    push_addr(ctl, 0x38)
 }
