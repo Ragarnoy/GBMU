5e231202d81595f7b00a883078733c1d2069e493
firelight.flagboy <firelight.flagboy@gmail.com>
2021-10-21 10:11:03 +0200

    impl file operation + correct increment error for system_clock

--- gb-timer/src/lib.rs
+++ gb-timer/src/lib.rs
@@ -1,4 +1,4 @@
-use gb_bus::Bus;
+use gb_bus::{Address, Bus, Error, FileOperation, IORegArea};
 use gb_clock::Ticker;
 
 pub struct Timer {
@@ -16,10 +16,27 @@ impl Ticker for Timer {
         gb_clock::Tick::MCycle
     }
 
-    fn tick<B>(&mut self, addr_bus: &mut B)
+    fn tick<B>(&mut self, _addr_bus: &mut B)
     where
         B: Bus<u8> + Bus<u16>,
     {
-        self.system_clock += self.cycle_count().into();
+        self.system_clock += u16::from(self.cycle_count() as u8);
+    }
+}
+
+impl FileOperation<IORegArea> for Timer {
+    fn read(&self, addr: Box<dyn Address<IORegArea>>) -> Result<u8, Error> {
+        match addr.area_type() {
+            IORegArea::DivTimer => Ok(self.div()),
+            _ => Err(Error::new_bus_error(addr)),
+        }
+    }
+
+    fn write(&mut self, _v: u8, addr: Box<dyn Address<IORegArea>>) -> Result<(), Error> {
+        match addr.area_type() {
+            IORegArea::DivTimer => self.system_clock = 0,
+            _ => return Err(Error::new_bus_error(addr)),
+        }
+        Ok(())
     }
 }
