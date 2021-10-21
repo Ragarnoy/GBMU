15399aa07903fd59d149e2ba71450d4f1ae0c5be
firelight.flagboy <firelight.flagboy@gmail.com>
2021-10-21 13:58:49 +0200

    rework initialisation of gbmu

--- src/main.rs
+++ src/main.rs
@@ -59,56 +59,8 @@ fn main() {
     let opts: Opts = Opts::parse();
     init_logger(opts.log_level);
 
-    let (sdl_context, video_subsystem, mut event_pump) =
-        gb_lcd::init().expect("Error while initializing LCD");
+    let (mut context, mut ppu, mut event_pump) = init_gbmu();
 
-    let bar_pixels_size = GBWindow::dots_to_pixels(&video_subsystem, render::MENU_BAR_SIZE)
-        .expect("Error while computing bar size");
-    let mut gb_window = GBWindow::new(
-        "GBMU",
-        (
-            render::SCREEN_WIDTH as u32,
-            render::SCREEN_HEIGHT as u32 + bar_pixels_size,
-        ),
-        true,
-        &video_subsystem,
-    )
-    .expect("Error while building main window");
-    let (width, height) = gb_window.sdl_window().size();
-
-    gb_window
-        .sdl_window_mut()
-        .set_minimum_size(width, height)
-        .expect("Failed to configure main window");
-
-    let display = render::RenderImage::with_bar_size(bar_pixels_size as f32);
-    let mut ppu = PPU::new();
-
-    let joypad = match settings::load() {
-        Some(conf) => gb_joypad::Joypad::from_config(gb_window.sdl_window().id(), conf),
-        None => {
-            log::warn!("No settings found, using default input configuration");
-            let tmp = gb_joypad::Joypad::new(gb_window.sdl_window().id());
-            settings::save(tmp.get_config());
-            tmp
-        }
-    };
-
-    #[cfg(feature = "debug_render")]
-    let mut debug = false;
-
-    let windows = Windows {
-        main: gb_window,
-        debug: None,
-        input: None,
-    };
-    let mut context = Context {
-        sdl: sdl_context,
-        video: video_subsystem,
-        display,
-        joypad,
-        windows,
-    };
     'running: loop {
         context
             .windows
@@ -162,4 +114,61 @@ fn main() {
         // std::thread::sleep(::std::time::Duration::new(0, 1_000_000_000u32 / 60));
     }
     log::info!("quitting");
+}
+
+fn init_gbmu<const WIDTH: usize, const HEIGHT: usize>(
+) -> (Context<WIDTH, HEIGHT>, PPU, sdl2::EventPump) {
+    let (sdl_context, video_subsystem, event_pump) =
+        gb_lcd::init().expect("Error while initializing LCD");
+
+    let bar_pixels_size = GBWindow::dots_to_pixels(&video_subsystem, render::MENU_BAR_SIZE)
+        .expect("Error while computing bar size");
+    let mut gb_window = GBWindow::new(
+        "GBMU",
+        (
+            render::SCREEN_WIDTH as u32,
+            render::SCREEN_HEIGHT as u32 + bar_pixels_size,
+        ),
+        true,
+        &video_subsystem,
+    )
+    .expect("Error while building main window");
+    let (width, height) = gb_window.sdl_window().size();
+
+    gb_window
+        .sdl_window_mut()
+        .set_minimum_size(width, height)
+        .expect("Failed to configure main window");
+
+    let display = render::RenderImage::with_bar_size(bar_pixels_size as f32);
+
+    let joypad = match settings::load() {
+        Some(conf) => gb_joypad::Joypad::from_config(gb_window.sdl_window().id(), conf),
+        None => {
+            log::warn!("No settings found, using default input configuration");
+            let tmp = gb_joypad::Joypad::new(gb_window.sdl_window().id());
+            settings::save(tmp.get_config());
+            tmp
+        }
+    };
+
+    #[cfg(feature = "debug_render")]
+    let mut debug = false;
+
+    let windows = Windows {
+        main: gb_window,
+        debug: None,
+        input: None,
+    };
+    (
+        Context {
+            sdl: sdl_context,
+            video: video_subsystem,
+            display,
+            joypad,
+            windows,
+        },
+        PPU::new(),
+        event_pump,
+    )
 }
