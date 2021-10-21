af40adbb5de0ee13d697b7415e81308b933cea48
firelight.flagboy <firelight.flagboy@gmail.com>
2021-10-21 11:35:24 +0200

    map timer registers

--- gb-timer/src/lib.rs
+++ gb-timer/src/lib.rs
@@ -1,8 +1,12 @@
 use gb_bus::{Address, Bus, Error, FileOperation, IORegArea};
 use gb_clock::Ticker;
 
+#[derive(Default)]
 pub struct Timer {
     system_clock: u16,
+    tima: u8,
+    tma: u8,
+    tac: u8,
 }
 
 impl Timer {
@@ -28,13 +32,19 @@ impl FileOperation<IORegArea> for Timer {
     fn read(&self, addr: Box<dyn Address<IORegArea>>) -> Result<u8, Error> {
         match addr.area_type() {
             IORegArea::DivTimer => Ok(self.div()),
+            IORegArea::TimerCounter => Ok(self.tima),
+            IORegArea::TimerModulo => Ok(self.tma),
+            IORegArea::TimerControl => Ok(self.tac),
             _ => Err(Error::new_bus_error(addr)),
         }
     }
 
-    fn write(&mut self, _v: u8, addr: Box<dyn Address<IORegArea>>) -> Result<(), Error> {
+    fn write(&mut self, v: u8, addr: Box<dyn Address<IORegArea>>) -> Result<(), Error> {
         match addr.area_type() {
             IORegArea::DivTimer => self.system_clock = 0,
+            IORegArea::TimerCounter => self.tima = v,
+            IORegArea::TimerModulo => self.tma = v,
+            IORegArea::TimerControl => self.tac = v,
             _ => return Err(Error::new_bus_error(addr)),
         }
         Ok(())
