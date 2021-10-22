f693b71bd2f26386b4544510ad3fe0352a49722e
firelight.flagboy <firelight.flagboy@gmail.com>
2021-10-22 11:26:59 +0200

    decode and store mbc

--- src/context.rs
+++ src/context.rs
@@ -1,7 +1,11 @@
 use gb_bus::{Area, FileOperation};
 use gb_joypad::Joypad;
 use gb_lcd::{render::RenderImage, window::GBWindow};
-use gb_roms::{header::AutoSave, Header};
+use gb_roms::{
+    controllers::{generate_rom_controller, MbcController},
+    header::AutoSave,
+    Header,
+};
 
 pub struct Context<const WIDTH: usize, const HEIGHT: usize> {
     pub sdl: sdl2::Sdl,
@@ -21,18 +25,21 @@ pub struct GameContext {
     romname: String,
     header: Header,
     auto_save: Option<AutoSave>,
+    mbc: MbcController,
 }
 
 impl GameContext {
     pub fn new(romname: String) -> Result<GameContext, anyhow::Error> {
-        use std::fs::File;
+        use std::{fs::File, io::Seek};
 
-        let file = File::open(romname)?;
-        let header = Header::from_file(file)?;
+        let mut file = File::open(romname)?;
+        let header = Header::from_file(&mut file)?;
 
         log::debug!("header: {:?}", header);
 
-        todo!("store mbc controller");
+        file.rewind()?;
+        let mbc = generate_rom_controller(file, header)?;
+
         todo!("store cpu");
         todo!("store clock");
         todo!("store timer");
@@ -41,6 +48,7 @@ impl GameContext {
             romname,
             header,
             auto_save: header.cartridge_type.auto_save_type(),
+            mbc,
         })
     }
 }
