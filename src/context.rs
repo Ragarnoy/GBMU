c4c217e3b90591e54eb09702106a3c46abf64895
firelight.flagboy <firelight.flagboy@gmail.com>
2021-10-22 14:52:35 +0200

    rework io bus creation using refcell

--- src/context.rs
+++ src/context.rs
@@ -12,6 +12,7 @@ use gb_roms::{
     header::AutoSave,
     Header,
 };
+use std::{cell::RefCell, rc::Rc};
 
 pub struct Context<const WIDTH: usize, const HEIGHT: usize> {
     pub sdl: sdl2::Sdl,
@@ -32,10 +33,10 @@ pub struct GameContext {
     header: Header,
     auto_save: Option<AutoSave>,
     mbc: MbcController,
-    cpu: Cpu,
+    cpu: Rc<RefCell<Cpu>>,
     clock: Clock<AddressBus>,
-    ppu: PPU,
-    io_bus: IORegBus,
+    ppu: Rc<RefCell<PPU>>,
+    io_bus: Rc<RefCell<IORegBus>>,
 }
 
 impl GameContext {
@@ -51,24 +52,25 @@ impl GameContext {
         let mbc = generate_rom_controller(file, header)?;
 
         let ppu = PPU::new();
-        let ppu_mem = ppu.memory();
-        let ppu_reg = ppu.registers();
-        let cpu = Cpu::default();
-        let wram = WorkingRam::new(false);
+        let ppu_mem = Rc::new(RefCell::new(ppu.memory()));
+        let ppu_reg = Rc::new(RefCell::new(ppu.registers()));
+        let ppu = Rc::new(RefCell::new(ppu));
+        let cpu = Rc::new(RefCell::new(Cpu::default()));
+        let wram = Rc::new(RefCell::new(WorkingRam::new(false)));
 
-        let io_bus = IORegBus {
-            controller: Box::new(CharDevice::default()),
-            communication: Box::new(SimpleRW::<2>::default()),
-            div_timer: Box::new(SimpleRW::<3>::default()),
-            sound: Box::new(SimpleRW::<0x16>::default()),
-            waveform_ram: Box::new(SimpleRW::<0xF>::default()),
-            lcd: Box::new(ppu_reg),
-            vram_bank: Box::new(ppu_reg),
-            boot_rom: Box::new(CharDevice::default()),
-            vram_dma: Box::new(SimpleRW::<4>::default()),
-            bg_obj_palettes: Box::new(ppu_reg),
-            wram_bank: Box::new(wram),
-        };
+        let io_bus = Rc::new(RefCell::new(IORegBus {
+            controller: Rc::new(RefCell::new(CharDevice::default())),
+            communication: Rc::new(RefCell::new(SimpleRW::<2>::default())), // We don't handle communication
+            div_timer: Rc::new(RefCell::new(SimpleRW::<3>::default())), // TODO: use timer to handle timer register
+            sound: Rc::new(RefCell::new(SimpleRW::<0x16>::default())),
+            waveform_ram: Rc::new(RefCell::new(SimpleRW::<0xF>::default())),
+            lcd: ppu_reg.clone(),
+            vram_bank: ppu_reg.clone(),
+            boot_rom: Rc::new(RefCell::new(CharDevice::default())),
+            vram_dma: Rc::new(RefCell::new(SimpleRW::<4>::default())), // TODO: link the part that handle the DMA
+            bg_obj_palettes: ppu_reg,
+            wram_bank: wram,
+        }));
 
         // TODO: store timer
         todo!("store address bus");
