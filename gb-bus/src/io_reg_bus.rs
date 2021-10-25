5ffa4ba92186f3039dbf277967f352772beb4027
FirelightFlagboy <30697622+FirelightFlagboy@users.noreply.github.com>
2021-10-25 19:06:52 +0200

    replace `Box` by `Rc + RefCell` (#271)

--- gb-bus/src/io_reg_bus.rs
+++ gb-bus/src/io_reg_bus.rs
@@ -8,79 +8,102 @@ use crate::{
     },
     Address as PseudoAddress, Area, Error, FileOperation, IORegArea,
 };
+use std::{cell::RefCell, rc::Rc};
 
 pub struct IORegBus {
-    pub controller: Box<dyn FileOperation<IORegArea>>,
-    pub communication: Box<dyn FileOperation<IORegArea>>,
-    pub div_timer: Box<dyn FileOperation<IORegArea>>,
-    pub sound: Box<dyn FileOperation<IORegArea>>,
-    pub waveform_ram: Box<dyn FileOperation<IORegArea>>,
-    pub lcd: Box<dyn FileOperation<IORegArea>>,
-    pub vram_bank: Box<dyn FileOperation<IORegArea>>,
-    pub boot_rom: Box<dyn FileOperation<IORegArea>>,
-    pub vram_dma: Box<dyn FileOperation<IORegArea>>,
-    pub bg_obj_palettes: Box<dyn FileOperation<IORegArea>>,
-    pub wram_bank: Box<dyn FileOperation<IORegArea>>,
+    pub controller: Rc<RefCell<dyn FileOperation<IORegArea>>>,
+    pub communication: Rc<RefCell<dyn FileOperation<IORegArea>>>,
+    pub div_timer: Rc<RefCell<dyn FileOperation<IORegArea>>>,
+    pub sound: Rc<RefCell<dyn FileOperation<IORegArea>>>,
+    pub waveform_ram: Rc<RefCell<dyn FileOperation<IORegArea>>>,
+    pub lcd: Rc<RefCell<dyn FileOperation<IORegArea>>>,
+    pub vram_bank: Rc<RefCell<dyn FileOperation<IORegArea>>>,
+    pub boot_rom: Rc<RefCell<dyn FileOperation<IORegArea>>>,
+    pub vram_dma: Rc<RefCell<dyn FileOperation<IORegArea>>>,
+    pub bg_obj_palettes: Rc<RefCell<dyn FileOperation<IORegArea>>>,
+    pub wram_bank: Rc<RefCell<dyn FileOperation<IORegArea>>>,
 }
 
 impl FileOperation<Area> for IORegBus {
     fn read(&self, address: Box<dyn PseudoAddress<Area>>) -> Result<u8, Error> {
         let addr: u16 = address.into();
         match addr {
-            CONTROLLER_START => self.controller.read(Box::new(Address::from_offset(
+            CONTROLLER_START => self.controller.borrow().read(Box::new(Address::from_offset(
                 IORegArea::Controller,
                 addr,
                 COMMUNICATION_START,
             ))),
-            COMMUNICATION_START..=COMMUNICATION_END => self.communication.read(Box::new(
-                Address::from_offset(IORegArea::Communication, addr, COMMUNICATION_START),
+            COMMUNICATION_START..=COMMUNICATION_END => {
+                self.communication
+                    .borrow()
+                    .read(Box::new(Address::from_offset(
+                        IORegArea::Communication,
+                        addr,
+                        COMMUNICATION_START,
+                    )))
+            }
+            DIV_TIMER_START..=DIV_TIMER_END => self.div_timer.borrow().read(Box::new(
+                Address::from_offset(IORegArea::DivTimer, addr, DIV_TIMER_START),
             )),
-            DIV_TIMER_START => self.div_timer.read(Box::new(Address::from_offset(
+            DIV_TIMER_START => self.div_timer.borrow().read(Box::new(Address::from_offset(
                 IORegArea::DivTimer,
                 addr,
                 DIV_TIMER_START,
             ))),
             TIMER_COUNTER_START => self
                 .tima
+                .borrow()
                 .read(Box::new(Address::byte_reg(IORegArea::TimerCounter, addr))),
             TIMER_MODULO_START => self
                 .tma
+                .borrow()
                 .read(Box::new(Address::byte_reg(IORegArea::TimerModulo, addr))),
             TIMER_CONTROL_START => self
                 .tac
+                .borrow()
                 .read(Box::new(Address::byte_reg(IORegArea::TimerControl, addr))),
-            SOUND_START..=SOUND_END => self.sound.read(Box::new(Address::from_offset(
+            SOUND_START..=SOUND_END => self.sound.borrow().read(Box::new(Address::from_offset(
                 IORegArea::Sound,
                 addr,
                 SOUND_START,
             ))),
-            WAVEFORM_RAM_START..=WAVEFORM_RAM_END => self.waveform_ram.read(Box::new(
-                Address::from_offset(IORegArea::WaveformRam, addr, WAVEFORM_RAM_START),
-            )),
-            LCD_START..=LCD_END => self.lcd.read(Box::new(Address::from_offset(
+            WAVEFORM_RAM_START..=WAVEFORM_RAM_END => {
+                self.waveform_ram
+                    .borrow()
+                    .read(Box::new(Address::from_offset(
+                        IORegArea::WaveformRam,
+                        addr,
+                        WAVEFORM_RAM_START,
+                    )))
+            }
+            LCD_START..=LCD_END => self.lcd.borrow().read(Box::new(Address::from_offset(
                 IORegArea::Lcd,
                 addr,
                 LCD_START,
             ))),
-            VRAM_BANK_START => self.vram_bank.read(Box::new(Address::from_offset(
+            VRAM_BANK_START => self.vram_bank.borrow().read(Box::new(Address::from_offset(
                 IORegArea::VRamBank,
                 addr,
                 VRAM_BANK_START,
             ))),
-            BOOT_ROM_START => self.boot_rom.read(Box::new(Address::from_offset(
+            BOOT_ROM_START => self.boot_rom.borrow().read(Box::new(Address::from_offset(
                 IORegArea::BootRom,
                 addr,
                 BOOT_ROM_START,
             ))),
-            VRAM_DMA_START..=VRAM_DMA_END => self.vram_dma.read(Box::new(Address::from_offset(
-                IORegArea::VramDma,
-                addr,
-                VRAM_DMA_START,
-            ))),
-            BG_OBJ_PALETTES_START..=BG_OBJ_PALETTES_END => self.bg_obj_palettes.read(Box::new(
-                Address::from_offset(IORegArea::BgObjPalettes, addr, BG_OBJ_PALETTES_START),
+            VRAM_DMA_START..=VRAM_DMA_END => self.vram_dma.borrow().read(Box::new(
+                Address::from_offset(IORegArea::VramDma, addr, VRAM_DMA_START),
             )),
-            WRAM_BANK_START => self.wram_bank.read(Box::new(Address::from_offset(
+            BG_OBJ_PALETTES_START..=BG_OBJ_PALETTES_END => {
+                self.bg_obj_palettes
+                    .borrow()
+                    .read(Box::new(Address::from_offset(
+                        IORegArea::BgObjPalettes,
+                        addr,
+                        BG_OBJ_PALETTES_START,
+                    )))
+            }
+            WRAM_BANK_START => self.wram_bank.borrow().read(Box::new(Address::from_offset(
                 IORegArea::WRamBank,
                 addr,
                 WRAM_BANK_START,
@@ -92,7 +115,7 @@ impl FileOperation<Area> for IORegBus {
     fn write(&mut self, v: u8, address: Box<dyn PseudoAddress<Area>>) -> Result<(), Error> {
         let addr: u16 = address.into();
         match addr {
-            CONTROLLER_START => self.controller.write(
+            CONTROLLER_START => self.controller.borrow_mut().write(
                 v,
                 Box::new(Address::from_offset(
                     IORegArea::Controller,
@@ -100,7 +123,7 @@ impl FileOperation<Area> for IORegBus {
                     CONTROLLER_START,
                 )),
             ),
-            COMMUNICATION_START..=COMMUNICATION_END => self.communication.write(
+            COMMUNICATION_START..=COMMUNICATION_END => self.communication.borrow_mut().write(
                 v,
                 Box::new(Address::from_offset(
                     IORegArea::Communication,
@@ -110,23 +133,25 @@ impl FileOperation<Area> for IORegBus {
             ),
             DIV_TIMER_START => self
                 .div_timer
+                .borrow_mut()
                 .write(v, Box::new(Address::byte_reg(IORegArea::DivTimer, addr))),
-            TIMER_COUNTER_START => self.tima.write(
+            TIMER_COUNTER_START => self.tima.borrow_mut().write(
                 v,
                 Box::new(Address::byte_reg(IORegArea::TimerCounter, addr)),
             ),
             TIMER_MODULO_START => self
                 .tma
+                .borrow_mut()
                 .write(v, Box::new(Address::byte_reg(IORegArea::TimerModulo, addr))),
-            TIMER_CONTROL_START => self.tac.write(
+            TIMER_CONTROL_START => self.tac.borrow_mut().write(
                 v,
                 Box::new(Address::byte_reg(IORegArea::TimerControl, addr)),
             ),
-            SOUND_START..=SOUND_END => self.sound.write(
+            SOUND_START..=SOUND_END => self.sound.borrow_mut().write(
                 v,
                 Box::new(Address::from_offset(IORegArea::Sound, addr, SOUND_START)),
             ),
-            WAVEFORM_RAM_START..=WAVEFORM_RAM_END => self.waveform_ram.write(
+            WAVEFORM_RAM_START..=WAVEFORM_RAM_END => self.waveform_ram.borrow_mut().write(
                 v,
                 Box::new(Address::from_offset(
                     IORegArea::WaveformRam,
@@ -134,11 +159,11 @@ impl FileOperation<Area> for IORegBus {
                     WAVEFORM_RAM_START,
                 )),
             ),
-            LCD_START..=LCD_END => self.lcd.write(
+            LCD_START..=LCD_END => self.lcd.borrow_mut().write(
                 v,
                 Box::new(Address::from_offset(IORegArea::Lcd, addr, LCD_START)),
             ),
-            VRAM_BANK_START => self.vram_bank.write(
+            VRAM_BANK_START => self.vram_bank.borrow_mut().write(
                 v,
                 Box::new(Address::from_offset(
                     IORegArea::VRamBank,
@@ -146,7 +171,7 @@ impl FileOperation<Area> for IORegBus {
                     VRAM_BANK_START,
                 )),
             ),
-            BOOT_ROM_START => self.boot_rom.write(
+            BOOT_ROM_START => self.boot_rom.borrow_mut().write(
                 v,
                 Box::new(Address::from_offset(
                     IORegArea::BootRom,
@@ -154,7 +179,7 @@ impl FileOperation<Area> for IORegBus {
                     BOOT_ROM_START,
                 )),
             ),
-            VRAM_DMA_START..=VRAM_DMA_END => self.vram_dma.write(
+            VRAM_DMA_START..=VRAM_DMA_END => self.vram_dma.borrow_mut().write(
                 v,
                 Box::new(Address::from_offset(
                     IORegArea::VramDma,
@@ -162,7 +187,7 @@ impl FileOperation<Area> for IORegBus {
                     VRAM_BANK_START,
                 )),
             ),
-            BG_OBJ_PALETTES_START..=BG_OBJ_PALETTES_END => self.bg_obj_palettes.write(
+            BG_OBJ_PALETTES_START..=BG_OBJ_PALETTES_END => self.bg_obj_palettes.borrow_mut().write(
                 v,
                 Box::new(Address::from_offset(
                     IORegArea::BgObjPalettes,
@@ -170,7 +195,7 @@ impl FileOperation<Area> for IORegBus {
                     BG_OBJ_PALETTES_START,
                 )),
             ),
-            WRAM_BANK_START => self.wram_bank.write(
+            WRAM_BANK_START => self.wram_bank.borrow_mut().write(
                 v,
                 Box::new(Address::from_offset(
                     IORegArea::WRamBank,
