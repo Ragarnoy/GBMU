d0835e3f62f4480adb305a743160a218029efae7
firelight.flagboy <firelight.flagboy@gmail.com>
2021-10-31 17:38:58 +0100

    add log to `FlowControl::draw`

--- gb-dbg/src/debugger/flow_control.rs
+++ gb-dbg/src/debugger/flow_control.rs
@@ -9,12 +9,15 @@ impl FlowController {
         let mut ret: Option<ControlFlow<(), RunDuration>> = None;
         ui.horizontal(|ui| {
             if ui.button("Step").clicked() {
+                log::error!("clicked on step");
                 ret = Some(ControlFlow::Continue(RunDuration::Step));
             }
             if ui.button("Run one frame").clicked() {
+                log::error!("clicked on frame");
                 ret = Some(ControlFlow::Continue(RunDuration::RunFrame));
             }
             if ui.button("Run one second").clicked() {
+                log::error!("clicked on one second");
                 ret = Some(ControlFlow::Continue(RunDuration::RunSecond));
             }
         });
