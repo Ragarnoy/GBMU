b927b1a5e0243e06b6eabcf7931db8cbfb57b3e2
firelight.flagboy <firelight.flagboy@gmail.com>
2021-11-01 10:30:52 +0100

    add enum that will be use for  Regs Debug Ops

--- gb-dbg/src/dbg_interfaces.rs
+++ gb-dbg/src/dbg_interfaces.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use std::fmt::{self, Display};
 
 pub type RegisterMap = (String, RegisterValue);
 
@@ -47,4 +48,146 @@ pub trait RegisterDebugOperations {
     fn ppu_registers(&self) -> Vec<RegisterMap>;
 
     fn io_registers(&self) -> Vec<RegisterMap>;
+}
+
+pub enum CpuRegs {
+    AF,
+    BC,
+    DE,
+    HL,
+    SP,
+    PC,
+}
+
+impl Display for CpuRegs {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let name = match self {
+            CpuRegs::AF => "AF",
+            CpuRegs::BC => "BC",
+            CpuRegs::DE => "DE",
+            CpuRegs::HL => "HL",
+            CpuRegs::SP => "SP",
+            CpuRegs::PC => "PC",
+        };
+        write!(f, "{}", name)
+    }
+}
+
+pub enum PpuRegs {
+    Control,
+    Status,
+    Scy,
+    Scx,
+    Ly,
+    Lyc,
+    Dma,
+    Bgp,
+    Obp0,
+    Obp1,
+    Wy,
+    Wx,
+}
+
+impl Display for PpuRegs {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let name = match self {
+            PpuRegs::Control => "Control",
+            PpuRegs::Status => "Status",
+            PpuRegs::Scy => "Scy",
+            PpuRegs::Scx => "Scx",
+            PpuRegs::Ly => "Ly",
+            PpuRegs::Lyc => "Lyc",
+            PpuRegs::Dma => "Dma",
+            PpuRegs::Bgp => "Bg p",
+            PpuRegs::Obp0 => "Obj p0",
+            PpuRegs::Obp1 => "Obj p1",
+            PpuRegs::Wy => "Wy",
+            PpuRegs::Wx => "Wx",
+        };
+        write!(f, "{}", name)
+    }
+}
+
+pub enum IORegs {
+    Joy,
+
+    SerialByte,
+    SerialControl,
+
+    Div,
+    Tima,
+    Tma,
+    TAc,
+
+    If,
+    Ie,
+
+    BootRom,
+
+    Fs1,
+    Pwm1,
+    Env1,
+    Af1,
+    Ctl1,
+
+    Pwm2,
+    Env2,
+    Af2,
+    Ctl2,
+
+    A3Toggle,
+    Pwm3,
+    Vol3,
+    Freq3,
+    Ctl3,
+
+    Pwm4,
+    Vol4,
+    Af4,
+    Ctl4,
+
+    AudOutMap,
+    AudMap,
+    AudChanCtl,
+    AudWave,
+}
+
+impl Display for IORegs {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let name = match self {
+            IORegs::Joy => "Joypad",
+            IORegs::SerialByte => "Serial Byte",
+            IORegs::SerialControl => "Serial Control",
+            IORegs::Div => "Div",
+            IORegs::Tima => "Tima",
+            IORegs::Tma => "Tma",
+            IORegs::TAc => "TAc",
+            IORegs::If => "Interrupt Flag",
+            IORegs::Ie => "Interrupt Enable",
+            IORegs::BootRom => "BootRom",
+            IORegs::Fs1 => "Aud 1 Sweep",
+            IORegs::Pwm1 => "Aud 1 Wave Duty",
+            IORegs::Env1 => "Aud 1 Envelope",
+            IORegs::Af1 => "Aud 1 Freq",
+            IORegs::Ctl1 => "Aud 1 Ctl",
+            IORegs::Pwm2 => "Aud 2 Wave Duty",
+            IORegs::Env2 => "Aud 2 Envelope",
+            IORegs::Af2 => "Aud 2 Freq",
+            IORegs::Ctl2 => "Aud 2 Ctl",
+            IORegs::A3Toggle => "Audio Channel 3 Toggle",
+            IORegs::Pwm3 => "Aud 3 Wave Duty",
+            IORegs::Vol3 => "Aud 3 Vol",
+            IORegs::Freq3 => "Aud 3 Freq",
+            IORegs::Ctl3 => "Aud 3 Ctl",
+            IORegs::Pwm4 => "Aud 4 Wave Duty",
+            IORegs::Vol4 => "Aud 4 Vol",
+            IORegs::Af4 => "Aud 4 Freq",
+            IORegs::Ctl4 => "Aud 4 Ctl",
+            IORegs::AudOutMap => "Aud Output Mapping",
+            IORegs::AudMap => "Aud Mapping",
+            IORegs::AudChanCtl => "Aud Channel Ctl",
+            IORegs::AudWave => "Aud Wave",
+        };
+        write!(f, "{}", name)
+    }
 }
