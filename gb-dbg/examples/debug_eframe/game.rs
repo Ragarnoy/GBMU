70414743ec7d55e3f50f21ce2cbfa8fb3a28a788
Tiago Lernould <tiagolernould@gmail.com>
2021-11-11 21:23:18 +0100

    Update example for Audio Regs and add fake registers to test padding

--- gb-dbg/examples/debug_eframe/game.rs
+++ gb-dbg/examples/debug_eframe/game.rs
@@ -1,6 +1,6 @@
 use gb_dbg::dbg_interfaces::{
-    CpuRegs, DebugOperations, IORegs, MemoryDebugOperations, PpuRegs, RegisterDebugOperations,
-    RegisterMap, RegisterValue,
+    AudioRegs, CpuRegs, DebugOperations, IORegs, MemoryDebugOperations, PpuRegs,
+    RegisterDebugOperations, RegisterMap, RegisterValue,
 };
 
 pub struct Iter<'a> {
@@ -121,15 +121,70 @@ impl RegisterDebugOperations for Game {
         Ok(0xffu8.into())
     }
 
+    fn audio_get(&self, _key: AudioRegs) -> anyhow::Result<RegisterValue> {
+        Ok(0xffu8.into())
+    }
+
     fn cpu_registers(&self) -> Vec<RegisterMap<CpuRegs>> {
         self.into()
     }
 
     fn ppu_registers(&self) -> Vec<RegisterMap<PpuRegs>> {
-        Vec::new()
+        vec![
+            RegisterMap(PpuRegs::Control, RegisterValue::from(2u8)),
+            RegisterMap(PpuRegs::Status, RegisterValue::from(14u8)),
+            RegisterMap(PpuRegs::Scy, RegisterValue::from(34u8)),
+            RegisterMap(PpuRegs::Scx, RegisterValue::from(125u16)),
+            RegisterMap(PpuRegs::Ly, RegisterValue::from(28u8)),
+            RegisterMap(PpuRegs::Lyc, RegisterValue::from(556u16)),
+            RegisterMap(PpuRegs::Dma, RegisterValue::from(444u16)),
+            RegisterMap(PpuRegs::Bgp, RegisterValue::from(215u8)),
+            RegisterMap(PpuRegs::Obp0, RegisterValue::from(33u8)),
+            RegisterMap(PpuRegs::Obp1, RegisterValue::from(8u8)),
+            RegisterMap(PpuRegs::Wy, RegisterValue::from(6u8)),
+            RegisterMap(PpuRegs::Wx, RegisterValue::from(88u8)),
+        ]
     }
 
     fn io_registers(&self) -> Vec<RegisterMap<IORegs>> {
-        Vec::new()
+        vec![
+            RegisterMap(IORegs::Joy, RegisterValue::from(2u8)),
+            RegisterMap(IORegs::SerialByte, RegisterValue::from(14u8)),
+            RegisterMap(IORegs::SerialCtl, RegisterValue::from(34u8)),
+            RegisterMap(IORegs::Div, RegisterValue::from(125u16)),
+            RegisterMap(IORegs::Tima, RegisterValue::from(28u8)),
+            RegisterMap(IORegs::Tma, RegisterValue::from(556u16)),
+            RegisterMap(IORegs::Tac, RegisterValue::from(444u16)),
+            RegisterMap(IORegs::If, RegisterValue::from(215u8)),
+            RegisterMap(IORegs::Ie, RegisterValue::from(33u8)),
+            RegisterMap(IORegs::BootRom, RegisterValue::from(81u16)),
+        ]
+    }
+
+    fn audio_registers(&self) -> Vec<RegisterMap<AudioRegs>> {
+        vec![
+            RegisterMap(AudioRegs::Fs1, RegisterValue::from(22u8)),
+            RegisterMap(AudioRegs::Pwm1, RegisterValue::from(2u8)),
+            RegisterMap(AudioRegs::Env1, RegisterValue::from(14u8)),
+            RegisterMap(AudioRegs::Af1, RegisterValue::from(34u8)),
+            RegisterMap(AudioRegs::Ctl1, RegisterValue::from(125u16)),
+            RegisterMap(AudioRegs::Pwm2, RegisterValue::from(288u16)),
+            RegisterMap(AudioRegs::Env2, RegisterValue::from(555u16)),
+            RegisterMap(AudioRegs::Af2, RegisterValue::from(444u16)),
+            RegisterMap(AudioRegs::Ctl2, RegisterValue::from(215u8)),
+            RegisterMap(AudioRegs::A3Toggle, RegisterValue::from(33u8)),
+            RegisterMap(AudioRegs::Pwm3, RegisterValue::from(81u16)),
+            RegisterMap(AudioRegs::Vol3, RegisterValue::from(48u16)),
+            RegisterMap(AudioRegs::Af3, RegisterValue::from(8u16)),
+            RegisterMap(AudioRegs::Ctl3, RegisterValue::from(99u16)),
+            RegisterMap(AudioRegs::Pwm4, RegisterValue::from(269u16)),
+            RegisterMap(AudioRegs::Vol4, RegisterValue::from(8654u16)),
+            RegisterMap(AudioRegs::Af4, RegisterValue::from(69u16)),
+            RegisterMap(AudioRegs::Ctl4, RegisterValue::from(6u16)),
+            RegisterMap(AudioRegs::AudOutMap, RegisterValue::from(15042u16)),
+            RegisterMap(AudioRegs::AudMap, RegisterValue::from(5555u16)),
+            RegisterMap(AudioRegs::AudChanCtl, RegisterValue::from(251u8)),
+            RegisterMap(AudioRegs::AudWave, RegisterValue::from(12u8)),
+        ]
     }
 }
