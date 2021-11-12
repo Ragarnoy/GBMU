cb2d501312989cf87a552fbfc42a225a1616177b
firelight.flagboy <firelight.flagboy@gmail.com>
2021-11-12 14:21:15 +0100

    rework range option to use constant + u16 value

--- gb-dbg/src/debugger/options.rs
+++ gb-dbg/src/debugger/options.rs
@@ -1,18 +1,22 @@
+use gb_bus::constant::{
+    ERAM_START, ERAM_STOP, HRAM_START, HRAM_STOP, RAM_START, RAM_STOP, ROM_START, ROM_STOP,
+    VRAM_START, VRAM_STOP,
+};
 use std::ops::Range;
 
 pub struct DebuggerOptions {
-    pub address_ranges: Vec<(&'static str, Range<usize>)>,
+    pub address_ranges: Vec<(&'static str, Range<u16>)>,
 }
 
 impl Default for DebuggerOptions {
     fn default() -> Self {
         Self {
             address_ranges: vec![
-                ("ROM", 0x0000..0x7FFF),
-                ("VRAM", 0x8000..0x9FFF),
-                ("ERAM", 0xA000..0xBFFF),
-                ("RAM", 0xC000..0xDFFF),
-                ("HRAM", 0xFF80..0xFFFE),
+                ("ROM", ROM_START..ROM_STOP),
+                ("VRAM", VRAM_START..VRAM_STOP),
+                ("ERAM", ERAM_START..ERAM_STOP),
+                ("RAM", RAM_START..RAM_STOP),
+                ("HRAM", HRAM_START..HRAM_STOP),
             ],
         }
     }
