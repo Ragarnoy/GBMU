d9079f2d09c41d296389df53b94a2af94c682f9e
firelight.flagboy <firelight.flagboy@gmail.com>
2021-11-16 11:17:58 +0100

    impl step for composite register

--- gb-cpu/tests/bdd.rs
+++ gb-cpu/tests/bdd.rs
@@ -93,6 +93,20 @@ async fn check_reg16_value(world: &mut CpuWorld, reg: Reg16, value: String) {
     assert_eq!(reg_value, value, "got {:x}, wanted {:x}", reg_value, value);
 }
 
+#[then(regex = r"the composite register ((?:\w) (?:\w)) set to the value ((?:[A-F0-9]{1,2}) (?:[A-F0-9]{1,2}))")]
+async fn check_composite_reg_value(world: &mut CpuWorld, reg: String, value: String) {
+    use std::str::FromStr;
+
+    let reg = reg.split(" ").collect::<Vec<&str>>().join("");
+    let reg = Reg16::from_str(&reg).expect("cannot decode composite register");
+
+    let value = value.split(" ").collect::<Vec<&str>>().join("");
+    let value = u16::from_str_radix(&value, 16).expect("valid hexa value");
+
+    let reg_value = reg.read_corresponding_regs(&world.cpu.registers);
+    assert_eq!(reg_value, value, "got {:x}, wanted {:x}", reg_value, value);
+}
+
 #[then(regex = r"the u8 register (\w) is set to ([A-F0-9]{1,2})")]
 async fn check_reg8_value(world: &mut CpuWorld, reg: Reg8, value: String) {
     let value = u8::from_str_radix(&value, 16).expect("valid hexa value");
