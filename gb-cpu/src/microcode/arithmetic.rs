6ae6ad8a418835eda4cdebe351503588595f3fa5
firelight.flagboy <firelight.flagboy@gmail.com>
2021-11-16 16:07:07 +0100

    invert left and right

--- gb-cpu/src/microcode/arithmetic.rs
+++ gb-cpu/src/microcode/arithmetic.rs
@@ -4,8 +4,7 @@ use super::{
 use crate::interfaces::{ReadFlagReg, WriteFlagReg};
 
 pub fn sub(ctl: &mut MicrocodeController, state: &mut State) -> MicrocodeFlow {
-    let value = ctl.pop();
-    let (value, flag) = math::sub_components(ctl.pop(), value);
+    let (value, flag) = math::sub_components(ctl.pop(), ctl.pop());
     state.regs.set_substraction(true);
     state.regs.set_zero(flag.zero);
     state.regs.set_half_carry(flag.half_carry);
@@ -15,8 +14,7 @@ pub fn sub(ctl: &mut MicrocodeController, state: &mut State) -> MicrocodeFlow {
 }
 
 pub fn add(ctl: &mut MicrocodeController, state: &mut State) -> MicrocodeFlow {
-    let value = ctl.pop();
-    let (value, flag) = math::add_components(ctl.pop(), value);
+    let (value, flag) = math::add_components(ctl.pop(), ctl.pop());
     state.regs.set_substraction(false);
     state.regs.set_zero(flag.zero);
     state.regs.set_half_carry(flag.half_carry);
@@ -132,8 +130,9 @@ fn test_slice_byte() {
 }
 
 pub fn adc(ctl: &mut MicrocodeController, state: &mut State) -> MicrocodeFlow {
-    let value = ctl.pop() + (state.regs.carry() as u8);
-    let (value, flag) = math::add_components(ctl.pop(), value);
+    let left = ctl.pop();
+    let right = ctl.pop() + (state.regs.carry() as u8);
+    let (value, flag) = math::add_components(left, right);
 
     state.regs.set_substraction(false);
     state.regs.set_zero(flag.zero);
@@ -144,8 +143,9 @@ pub fn adc(ctl: &mut MicrocodeController, state: &mut State) -> MicrocodeFlow {
 }
 
 pub fn sbc(ctl: &mut MicrocodeController, state: &mut State) -> MicrocodeFlow {
-    let value = ctl.pop() + (state.regs.carry() as u8);
-    let (value, flag) = math::sub_components(ctl.pop(), value);
+    let left = ctl.pop();
+    let right = ctl.pop() + (state.regs.carry() as u8);
+    let (value, flag) = math::sub_components(left, right);
 
     state.regs.set_substraction(true);
     state.regs.set_zero(flag.zero);
