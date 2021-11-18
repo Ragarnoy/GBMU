3245986da4fd77c8560bad108c05d4d04ca2f1e5
firelight.flagboy <firelight.flagboy@gmail.com>
2021-11-18 14:00:42 +0100

    add struct that handle/store interrupt registers

---
+++ gb-cpu/src/interrupt_flags.rs
@@ -0,0 +1,41 @@
+use gb_bus::{Area, FileOperation, IORegArea};
+
+#[derive(Default, Debug, Clone, Copy)]
+pub struct InterruptFlags {
+    pub master_enabled: bool,
+    pub flag: u8,
+    pub enable_mask: u8,
+}
+
+impl InterruptFlags {
+    pub fn is_interrupt_ready(&self) -> bool {
+        if !self.master_enabled {
+            return false;
+        }
+        self.flag & self.enable_mask != 0
+    }
+}
+
+impl FileOperation<Area> for InterruptFlags {
+    fn read(&self, _addr: Box<dyn gb_bus::Address<Area>>) -> Result<u8, gb_bus::Error> {
+        Ok(self.enable_mask)
+    }
+    fn write(&mut self, v: u8, _addr: Box<dyn gb_bus::Address<Area>>) -> Result<(), gb_bus::Error> {
+        self.enable_mask = v;
+        Ok(())
+    }
+}
+
+impl FileOperation<IORegArea> for InterruptFlags {
+    fn read(&self, _addr: Box<dyn gb_bus::Address<IORegArea>>) -> Result<u8, gb_bus::Error> {
+        Ok(self.flag)
+    }
+    fn write(
+        &mut self,
+        v: u8,
+        _addr: Box<dyn gb_bus::Address<IORegArea>>,
+    ) -> Result<(), gb_bus::Error> {
+        self.flag = v;
+        Ok(())
+    }
+}
