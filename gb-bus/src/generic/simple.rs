f9216c42e353c9421d4bf46e0fd0b9c046f4944f
firelight.flagboy <firelight.flagboy@gmail.com>
2021-11-22 21:42:57 +0100

    add logging in simple_rw

--- gb-bus/src/generic/simple.rs
+++ gb-bus/src/generic/simple.rs
@@ -12,12 +12,21 @@ pub struct SimpleRW<const SIZE: usize> {
 impl<const SIZE: usize> FileOperation<Area> for SimpleRW<SIZE> {
     fn write(&mut self, v: u8, addr: Box<dyn Address<Area>>) -> Result<(), Error> {
         let address = addr.get_address();
+        #[cfg(feature = "trace_simple_rw_write")]
+        log::trace!(
+            "writing value v={:x}, abs={:x}, rel={:x}",
+            v,
+            u16::from(addr),
+            address
+        );
         self.store[address] = v;
         Ok(())
     }
 
     fn read(&self, addr: Box<dyn Address<Area>>) -> Result<u8, Error> {
         let address = addr.get_address();
+        #[cfg(feature = "trace_simple_rw_read")]
+        log::trace!("reading value abs={:x}, rel={:x}", u16::from(addr), address);
         Ok(self.store[address])
     }
 }
@@ -25,12 +34,21 @@ impl<const SIZE: usize> FileOperation<Area> for SimpleRW<SIZE> {
 impl<const SIZE: usize> FileOperation<IORegArea> for SimpleRW<SIZE> {
     fn write(&mut self, v: u8, addr: Box<dyn Address<IORegArea>>) -> Result<(), Error> {
         let address = addr.get_address();
+        #[cfg(feature = "trace_simple_rw_write")]
+        log::trace!(
+            "writing value v={:x}, abs={:x}, rel={:x}",
+            v,
+            u16::from(addr),
+            address
+        );
         self.store[address] = v;
         Ok(())
     }
 
     fn read(&self, addr: Box<dyn Address<IORegArea>>) -> Result<u8, Error> {
         let address = addr.get_address();
+        #[cfg(feature = "trace_simple_rw_read")]
+        log::trace!("reading value abs={:x}, rel={:x}", u16::from(addr), address);
         Ok(self.store[address])
     }
 }
