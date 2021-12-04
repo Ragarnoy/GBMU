fa9a04df1bb0f2bb2b9ebf682ad60673022f7b5f
firelight.flagboy <firelight.flagboy@gmail.com>
2021-12-04 20:42:26 +0100

    implement missing parser

--- gb-dbg/src/debugger/breakpoints/breakpoint_expression.rs
+++ gb-dbg/src/debugger/breakpoints/breakpoint_expression.rs
@@ -1,14 +1,14 @@
 use crate::dbg_interfaces::{CpuRegs, DebugOperations};
 use anyhow::anyhow;
-use nom::{
-    branch::alt,
-    bytes::complete::{tag, take_while_m_n},
-    combinator::map,
-    sequence::tuple,
-    IResult,
-};
+use nom::{bytes::streaming::tag, branch::alt, combinator::map, sequence::{tuple, delimited}, IResult};
 use std::fmt::{Display, Formatter};
 use std::str::FromStr;
+
+macro_rules! boxed {
+    ($any:expr) => {
+        Box::new($any)
+    };
+};
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 enum UnaryOperator {
@@ -131,27 +131,64 @@ impl FromStr for Node {
 
 fn expr(input: &str) -> IResult<&str, Node> {
     alt((
-        map(tuple(expr, comb_op, expr), |(lhs, op, rhs)| {
-            Node::BinaryExpr { op, lhs, rhs }
+        map(tuple((expr, comb_op, expr)), |(lhs, op, rhs)| {
+            Node::BinaryExpr { op, lhs: boxed!(lhs), rhs: boxed!(rhs) }
         }),
-        map(tuple(any_value, bin_op, any_value), |(lhs, op, rhs)| {
-            Node::BinaryExpr { op, lhs, rhs }
+        map(tuple((any_value, bin_op, any_value)), |(lhs, op, rhs)| {
+            Node::BinaryExpr { op, lhs: boxed!(lhs), rhs: boxed!(rhs) }
         }),
     ))(input)
 }
 
-fn operand_right(input: &str) -> IResult<&str, Operand> {
-    alt((map(register, Operand::Register), map(value, Operand::Value)))(input)
-}
-
-fn operand_left(input: &str) -> IResult<&str, Operand> {
+fn comb_op(input: &str) -> IResult<&str, Operator> {
     alt((
-        map(register, Operand::Register),
-        map(value, Operand::Address),
+        map(tag("&&"), |_| Operator::And),
+        map(tag("||"), |_| Operator::Or),
     ))(input)
 }
 
-fn operator(input: &str) -> IResult<&str, Operator> {
+fn any_value(input: &str) -> IResult<&str, Node> {
+    alt((
+        unary_expr,
+        value,
+    ))(input)
+}
+
+fn unary_expr(input: &str) -> IResult<&str, Node> {
+    let (input, unary_op) = alt((tag("L"), tag("U")))(input)?;
+    let unary_op = match unary_op {
+        "L" => UnaryOperator::Lower,
+        "H" => UnaryOperator::Upper,
+        _ => panic!("unexpected valid unary op `{}'", unary_op)
+    };
+
+    map(delimited(tag("("), register, tag(")")), |reg| Node::UnaryExpr{op: unary_op, child: boxed!(reg)})(input)
+}
+
+fn register(input: &str) -> IResult<&str, Node> {
+    map(raw_register, Node::Register)(input)
+}
+
+fn raw_register(input: &str) -> IResult<&str, CpuRegs> {
+    alt((
+        map(tag("AF"), |_| CpuRegs::AF),
+        map(tag("BC"), |_| CpuRegs::BC),
+        map(tag("DE"), |_| CpuRegs::DE),
+        map(tag("HL"), |_| CpuRegs::HL),
+        map(tag("PC"), |_| CpuRegs::PC),
+        map(tag("SP"), |_| CpuRegs::SP),
+    ))(input)
+}
+
+fn value(input: &str) -> IResult<&str, Node> {
+    alt((
+        register,
+        map(raw_value, Node::Value),
+        address,
+    ))(input)
+}
+
+fn raw_operator(input: &str) -> IResult<&str, Operator> {
     alt((
         map(tag("=="), |_| Operator::Eq),
         map(tag("!="), |_| Operator::NotEq),
@@ -162,18 +199,9 @@ fn operator(input: &str) -> IResult<&str, Operator> {
     ))(input.trim())
 }
 
-fn register(input: &str) -> IResult<&str, CpuRegs> {
-    alt((
-        map(tag("AF"), |_| CpuRegs::AF),
-        map(tag("BC"), |_| CpuRegs::BC),
-        map(tag("DE"), |_| CpuRegs::DE),
-        map(tag("HL"), |_| CpuRegs::HL),
-        map(tag("PC"), |_| CpuRegs::PC),
-        map(tag("SP"), |_| CpuRegs::SP),
-    ))(input.trim())
-}
 
-fn value(input: &str) -> IResult<&str, u16> {
+
+fn raw_value(input: &str) -> IResult<&str, u16> {
     map(take_while_m_n(1, 4, |c: char| c.is_ascii_hexdigit()), |s| {
         u16::from_str_radix(s, 16).unwrap()
     })(input.trim())
