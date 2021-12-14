eb13956ce1c9be2431eb88662a5dafcd73252927
firelight.flagboy <firelight.flagboy@gmail.com>
2021-12-14 11:56:18 +0100

    add method to know if cartridge is cgb compatible

--- gb-roms/src/header.rs
+++ gb-roms/src/header.rs
@@ -221,6 +221,19 @@ impl Default for Title {
     }
 }
 
+impl Title {
+    pub fn is_cgb_cartridge(&self) -> bool {
+        match self {
+            Title::Advanced {
+                title: _,
+                manufacturer: _,
+                cgb_flag: _,
+            } => true,
+            _ => false,
+        }
+    }
+}
+
 impl TryFrom<[u8; 16]> for Title {
     type Error = Error;
 
