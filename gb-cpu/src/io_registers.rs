5cb19ada7e21360280b37aa372433d64cc44269e
firelight.flagboy <firelight.flagboy@gmail.com>
2021-12-16 13:59:40 +0100

    impl + test double register

--- gb-cpu/src/io_registers.rs
+++ gb-cpu/src/io_registers.rs
@@ -6,8 +6,10 @@ pub struct IORegisters {
     pub flag: u8,
     pub enable_mask: u8,
 
-    pub double_speed: bool,
-    pub desire_double_speed: bool,
+    #[cfg(feature = "cgb")]
+    pub current_speed: bool,
+    #[cfg(feature = "cgb")]
+    pub desired_speed: bool,
 }
 
 impl IORegisters {
@@ -24,8 +26,9 @@ impl IORegisters {
     }
 
     /// Indicate when we need to switch between `normal speed <=> double speed`
+    #[cfg(feature = "cgb")]
     pub fn need_to_change_speed(&self) -> bool {
-        self.double_speed != self.desire_double_speed
+        self.current_speed != self.desired_speed
     }
 }
 
@@ -44,6 +47,11 @@ impl FileOperation<IORegArea> for IORegisters {
     fn read(&self, addr: Box<dyn gb_bus::Address<IORegArea>>) -> Result<u8, gb_bus::Error> {
         match addr.area_type() {
             IORegArea::InterruptFlag => Ok(self.flag),
+            #[cfg(feature = "cgb")]
+            IORegArea::DoubleSpeed => Ok(double_speed_register(
+                self.current_speed,
+                self.desired_speed,
+            )),
             _ => Err(gb_bus::Error::bus_error(addr)),
         }
     }
@@ -56,4 +64,28 @@ impl FileOperation<IORegArea> for IORegisters {
         self.flag = v;
         Ok(())
     }
+}
+
+/// generate the key1 register from to current & desired speed mode
+#[cfg(feature = "cgb")]
+fn double_speed_register(current_speed: bool, desired_speed: bool) -> u8 {
+    let mut v = 0;
+
+    if current_speed {
+        v |= 0x80;
+    }
+    if desired_speed {
+        v |= 0x1;
+    }
+
+    v
+}
+
+#[cfg(feature = "cgb")]
+#[test]
+fn test_double_speed_regs() {
+    assert_eq!(double_speed_register(false, false), 0x00);
+    assert_eq!(double_speed_register(true, false), 0x80);
+    assert_eq!(double_speed_register(true, true), 0x81);
+    assert_eq!(double_speed_register(false, true), 0x01);
 }
