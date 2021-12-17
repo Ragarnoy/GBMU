386d51db058bed81d8b47acdaea01e122aac0b7a
FirelightFlagboy <30697622+FirelightFlagboy@users.noreply.github.com>
2021-12-17 10:57:15 +0100

    Feature/bankable storage (#407)
    * add generic bankable byte storage
    
    * expose current bank index for bankable storage
    
    * rework working_ram to use bankable storage

--- gb-bus/src/working_ram.rs
+++ gb-bus/src/working_ram.rs
@@ -1,26 +1,25 @@
-use crate::{Address, Area, Error, FileOperation, IORegArea};
+use crate::{generic::DynBankableStorage, Address, Area, Error, FileOperation, IORegArea};
 
 pub const RAM_BANK_SIZE: usize = 0x1000;
 pub const CGB_MAX_BANKS: usize = 8;
 pub const DMG_MAX_BANKS: usize = 2;
 
 pub struct WorkingRam {
-    banks: Vec<[u8; RAM_BANK_SIZE]>,
-    selected_bank: u8,
+    storage: DynBankableStorage<RAM_BANK_SIZE>,
     enable_cgb_feature: bool,
 }
 
 impl WorkingRam {
     pub fn new(enable_cgb_feature: bool) -> Self {
-        let banks = if enable_cgb_feature {
-            vec![[0; RAM_BANK_SIZE]; CGB_MAX_BANKS]
+        let mut storage = if enable_cgb_feature {
+            DynBankableStorage::with_bank_amount(CGB_MAX_BANKS)
         } else {
-            vec![[0; RAM_BANK_SIZE]; DMG_MAX_BANKS]
+            DynBankableStorage::with_bank_amount(DMG_MAX_BANKS)
         };
+        storage.set_bank_index(1);
         Self {
-            banks,
+            storage,
             enable_cgb_feature,
-            selected_bank: 1,
         }
     }
 }
@@ -29,15 +28,10 @@ impl FileOperation<Area> for WorkingRam {
     fn write(&mut self, value: u8, addr: Box<dyn Address<Area>>) -> Result<(), Error> {
         let address = addr.get_address();
         match address {
-            0..=0xfff => self.banks[0][address] = value,
+            0..=0xfff => self.storage.root_bank_mut()[address] = value,
             0x1000..=0x1fff => {
                 let address = address - 0x1000;
-                let index = if self.enable_cgb_feature {
-                    self.selected_bank as usize
-                } else {
-                    1
-                };
-                self.banks[index][address] = value;
+                self.storage[address] = value;
             }
             _ => return Err(Error::bus_error(addr)),
         }
@@ -47,15 +41,10 @@ impl FileOperation<Area> for WorkingRam {
     fn read(&self, addr: Box<dyn Address<Area>>) -> Result<u8, Error> {
         let address = addr.get_address();
         match address {
-            0..=0xfff => Ok(self.banks[0][address]),
+            0..=0xfff => Ok(self.storage.root_bank()[address]),
             0x1000..=0x1fff => {
                 let address = address - 0x1000;
-                let index = if self.enable_cgb_feature {
-                    self.selected_bank as usize
-                } else {
-                    1
-                };
-                Ok(self.banks[index][address])
+                Ok(self.storage[address])
             }
             _ => Err(Error::bus_error(addr)),
         }
@@ -65,7 +54,7 @@ impl FileOperation<Area> for WorkingRam {
 impl FileOperation<IORegArea> for WorkingRam {
     fn write(&mut self, value: u8, addr: Box<dyn Address<IORegArea>>) -> Result<(), Error> {
         if self.enable_cgb_feature {
-            self.selected_bank = value.clamp(1, 7);
+            self.storage.set_bank_index((value & 0x7).min(1) as usize);
             Ok(())
         } else {
             Err(Error::new_segfault(addr))
@@ -74,7 +63,7 @@ impl FileOperation<IORegArea> for WorkingRam {
 
     fn read(&self, addr: Box<dyn Address<IORegArea>>) -> Result<u8, Error> {
         if self.enable_cgb_feature {
-            Ok(self.selected_bank)
+            Ok(self.storage.current_bank_index as u8)
         } else {
             Err(Error::new_segfault(addr))
         }
