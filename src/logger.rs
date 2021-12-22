83283ed3f10ea81b011fa26d1839432ca350f693
firelight.flagboy <firelight.flagboy@gmail.com>
2021-12-22 19:10:07 +0100

    rework logger on release mode to buffer log with a level greater than Info

--- src/logger.rs
+++ src/logger.rs
@@ -1,56 +1,71 @@
-use simplelog::{Config, WriteLogger};
+use log::{set_boxed_logger, LevelFilter};
+use simplelog::{Config, TermLogger, WriteLogger};
 use std::{fs::File, io::BufWriter};
 
 #[cfg(debug_assertions)]
 pub fn init_logger(level: log::LevelFilter) {
-    setup_terminal_logger(level, Config::default());
+    let logger = terminal_logger(level, Config::default());
+    set_boxed_logger(logger).expect("cannot set logger");
 }
 
 #[cfg(not(debug_assertions))]
 pub fn init_logger(level: log::LevelFilter) {
-    use simplelog::WriteLogger;
-    use std::fs::File;
-
-    const LOG_FILE: &'static str = "/tmp/gbmu.log";
     let config: Config = Config::default();
-    let file_res = File::create(LOG_FILE);
 
-    if let Ok(file) = file_res {
-        let write_logger_res = WriteLogger::init(level, config.clone(), file);
-        if write_logger_res.is_ok() {
-            return;
-        } else {
-            setup_terminal_logger(level, config);
-            log::warn!(
-                "cannot setup write logger (because: {})",
-                write_logger_res.unwrap_err()
-            );
-        }
+    if let Err(e) = init_file_logger(level, config.clone()) {
+        set_boxed_logger(terminal_logger(level, config)).expect("cannot set any logger");
+        log::error!("failed to set file logger: {}", e);
+    };
+}
+
+#[cfg(not(debug_assertions))]
+fn init_file_logger(level: log::LevelFilter, config: Config) -> anyhow::Result<()> {
+    const LOG_FILE: &'static str = "/tmp/gbmu.log";
+    const TRACE_LOG_FILE: &'static str = "/tmp/gbmu-trace.log";
+    const LEVEL_BEFORE_BUFFERING: LevelFilter = LevelFilter::Info;
+
+    if level > LEVEL_BEFORE_BUFFERING {
+        use simplelog::CombinedLogger;
+
+        CombinedLogger::init(vec![
+            file_logger(LOG_FILE, LEVEL_BEFORE_BUFFERING, config.clone())?,
+            buffered_file_logger(TRACE_LOG_FILE, level, config)?,
+        ])?
     } else {
-        setup_terminal_logger(level, config);
-        log::warn!(
-            "cannot setup logging to file {} (because: {})",
-            LOG_FILE,
-            file_res.unwrap_err()
-        );
+        set_boxed_logger(file_logger(LOG_FILE, level, config)?)?
     }
-    log::warn!("fallback to terminal logger");
+    Ok(())
 }
 
-fn setup_terminal_logger(level: log::LevelFilter, config: simplelog::Config) {
-    use simplelog::{ColorChoice, TermLogger, TerminalMode};
-
-    TermLogger::init(level, config, TerminalMode::Mixed, ColorChoice::Auto)
-        .expect("cannot setup terminal logger")
-}
-
-fn trace_buffered_logger(config: Config) -> anyhow::Result<Box<WriteLogger<BufWriter<File>>>> {
-    const TRACE_FILE: &'static str = "/tmp/gbmu-trace.log";
-    let file = File::create(TRACE_FILE)?;
+#[cfg(not(debug_assertions))]
+fn buffered_file_logger(
+    filename: &str,
+    level: LevelFilter,
+    config: Config,
+) -> anyhow::Result<Box<WriteLogger<BufWriter<File>>>> {
+    const BUFFER_CAPS: usize = 32_768;
+    let file = File::create(filename)?;
 
     Ok(WriteLogger::new(
-        log::LevelFilter::Trace,
+        level,
         config,
-        BufWriter::new(file),
+        BufWriter::with_capacity(BUFFER_CAPS, file),
     ))
+}
+
+fn terminal_logger(level: LevelFilter, config: Config) -> Box<TermLogger> {
+    use simplelog::{ColorChoice, TerminalMode};
+
+    TermLogger::new(level, config, TerminalMode::Mixed, ColorChoice::Auto)
+}
+
+#[cfg(not(debug_assertions))]
+fn file_logger(
+    filename: &str,
+    level: LevelFilter,
+    config: Config,
+) -> anyhow::Result<Box<WriteLogger<File>>> {
+    let file = File::create(filename)?;
+
+    Ok(WriteLogger::new(level, config, file))
 }
