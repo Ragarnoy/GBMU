5e3c594a7c0ce84eb9f9de6fee38a2d9faaabb35
FirelightFlagboy <30697622+FirelightFlagboy@users.noreply.github.com>
2021-12-30 18:04:52 +0100

    Feature/cgb color io reg (#475)
    * rename interrupt_flags to io_registers
    
    * rename struct IORegisters
    
    * add cgb feature for cpu + add cpu cgb feature as requirement for main +
    update README
    
    * add missing variant for `io_reg_area => u16`
    
    * update import
    
    * impl + test double register
    
    * add double_speed reg to io bus
    
    * init double speed reg
    
    * add cgb feature to dbg
    
    * add io regs to dbg list
    
    * add step for io_get
    
    * auto format

--- gb-cpu/src/builder.rs
+++ gb-cpu/src/builder.rs
@@ -1,7 +1,7 @@
-use crate::{cpu::Cpu, interrupt_flags::InterruptFlags};
+use crate::{cpu::Cpu, io_registers::IORegisters};
 use std::{cell::RefCell, rc::Rc};
 
-pub fn new_cpu() -> (Cpu, Rc<RefCell<InterruptFlags>>) {
+pub fn new_cpu() -> (Cpu, Rc<RefCell<IORegisters>>) {
     let cpu = Cpu::default();
     let flags = cpu.interrupt_flags();
 
