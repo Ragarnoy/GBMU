658183cb53573bd36b4fecb240374c220106e698
firelight.flagboy <firelight.flagboy@gmail.com>
2021-12-31 10:20:38 +0100

    rewrite build script to reduce build time

--- gb-roms/build.rs
+++ gb-roms/build.rs
@@ -1,45 +1,80 @@
 use std::env;
-use std::fs;
+use std::fs::{self, File};
 use std::io;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
-use zip::ZipArchive;
+use zip::read::{ZipArchive, ZipFile};
 
 fn main() {
-    let out_dir = env::var_os("CARGO_MANIFEST_DIR").unwrap();
-    let dst_path = Path::new(&out_dir).join("..").join("assets").join("bios");
-    let src_path = dst_path.with_extension("zip");
+    let dst_path = get_destination_path();
+    let archive = get_archive_path();
 
-    if let Ok(file) = fs::File::open(&src_path) {
-        if let Ok(mut archive) = ZipArchive::new(file) {
-            fs::create_dir_all(&dst_path).unwrap();
-            for i in 0..archive.len() {
-                let mut file = archive.by_index(i).unwrap();
-                let outpath = match file.enclosed_name() {
-                    Some(path) => dst_path.clone().join(path),
-                    None => continue,
-                };
-                if (&*file.name()).ends_with('/') {
-                    fs::create_dir_all(&outpath).unwrap();
-                } else {
-                    if let Some(p) = outpath.parent() {
-                        if !p.exists() {
-                            fs::create_dir_all(&p).unwrap();
-                        }
-                    }
-                    let mut outfile = fs::File::create(&outpath).unwrap();
-                    io::copy(&mut file, &mut outfile).unwrap();
-                }
-                // Get and Set permissions
-                #[cfg(unix)]
-                {
-                    use std::os::unix::fs::PermissionsExt;
-                    if let Some(mode) = file.unix_mode() {
-                        fs::set_permissions(&outpath, fs::Permissions::from_mode(mode)).unwrap();
-                    }
-                }
-            }
+    fs::create_dir_all(&dst_path).unwrap();
+    if let Ok(file) = fs::File::open(&archive) {
+        unzip_file(dst_path, file);
+    }
+    println!(
+        "cargo:rerun-if-changed={}",
+        archive.to_str().expect("cannot create a string from path")
+    );
+}
+
+fn get_destination_path() -> PathBuf {
+    let out_dir = env::var_os("CARGO_MANIFEST_DIR").unwrap();
+    Path::new(&out_dir).join("..").join("assets").join("bios")
+}
+
+fn get_archive_path() -> PathBuf {
+    let dst_path = get_destination_path();
+    dst_path.with_extension("zip")
+}
+
+fn unzip_file(dst_path: PathBuf, file: fs::File) {
+    if let Ok(mut archive) = ZipArchive::new(file) {
+        for i in 0..archive.len() {
+            let file = archive.by_index(i).unwrap();
+            extract_file(dst_path.clone(), file)
         }
     }
-    println!("cargo:rerun-if-changed=assets/bios.zip");
+}
+
+/// Extract a file that is contain is an archive
+fn extract_file(output_dir: PathBuf, mut file: ZipFile) {
+    let dest_path = archive_file_output_path(&output_dir, &file).unwrap();
+    if file.is_dir() {
+        fs::create_dir_all(&dest_path).unwrap();
+    } else {
+        may_create_parent_subdir(&dest_path);
+        let mut dest_file = create_destination_file(&dest_path, file.unix_mode());
+        io::copy(&mut file, &mut dest_file).unwrap();
+    }
+}
+
+/// Create the output path for the file that is contain is the archive
+fn archive_file_output_path(output_dir: &PathBuf, file: &ZipFile) -> Result<PathBuf, String> {
+    file.enclosed_name()
+        .ok_or_else(|| format!("cannot get enclosed for {}", file.name()))
+        .map(|name| output_dir.clone().join(name))
+}
+
+fn may_create_parent_subdir(path: &PathBuf) {
+    if let Some(p) = path.parent() {
+        if !p.exists() {
+            fs::create_dir_all(&p).unwrap();
+        }
+    }
+}
+
+fn create_destination_file(path: &PathBuf, mode: Option<u32>) -> File {
+    let file = File::create(path).unwrap();
+    #[cfg(unix)]
+    {
+        use fs::Permissions;
+        use std::os::unix::fs::PermissionsExt;
+        if let Some(mode) = mode {
+            file.set_permissions(Permissions::from_mode(mode))
+                .expect("cannot set file Permissions");
+        }
+    }
+    file
 }
