14d06f7cbaae0ef29b5662b517aafff40bf01c7d
firelight.flagboy <firelight.flagboy@gmail.com>
2022-01-03 14:07:46 +0100

    add new constants

--- src/constant.rs
+++ src/constant.rs
@@ -1,2 +1,12 @@
+/// Name of the application
 pub const APP_NAME: &str = "gbmu";
+/// Name of the organization
 pub const ORG_NAME: &str = "";
+/// File extension for a file that should contain a `save state` save
+pub const SAVE_STATE_EXT: &str = ".save.msgpack";
+/// File extension for a file that should contain a `game save` save
+pub const GAME_SAVE_EXT: &str = ".game.msgpack";
+/// List of prefered extensions for ROM file
+pub const PREFERED_ROM_EXTS: [&str; 3] = [".rom", ".gb", ".gbc"];
+/// List of prefered extensions for `save state` file
+pub const PREFERED_SAVE_STATE_EXT: [&str; 1] = [SAVE_STATE_EXT];
