62d521824444c460b440437ca2e99c8ea5755ab1
firelight.flagboy <firelight.flagboy@gmail.com>
2022-01-03 14:22:34 +0100

    rename extension for save files

--- src/constant.rs
+++ src/constant.rs
@@ -3,10 +3,10 @@ pub const APP_NAME: &str = "gbmu";
 /// Name of the organization
 pub const ORG_NAME: &str = "";
 /// File extension for a file that should contain a `save state` save
-pub const SAVE_STATE_EXT: &str = ".save.msgpack";
+pub const SAVE_STATE_EXT: &str = "savepack";
 /// File extension for a file that should contain a `game save` save
-pub const GAME_SAVE_EXT: &str = ".game.msgpack";
+pub const GAME_SAVE_EXT: &str = "gamepack";
 /// List of prefered extensions for ROM file
-pub const PREFERED_ROM_EXTS: [&str; 3] = [".rom", ".gb", ".gbc"];
+pub const PREFERED_ROM_EXTS: [&str; 3] = ["rom", "gb", "gbc"];
 /// List of prefered extensions for `save state` file
 pub const PREFERED_SAVE_STATE_EXT: [&str; 1] = [SAVE_STATE_EXT];
