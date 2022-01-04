dec3867119651d4e7f665fdfa4d4b0bdab95ecbb
firelight.flagboy <firelight.flagboy@gmail.com>
2022-01-04 18:25:30 +0100

    use default serial zone

--- src/context.rs
+++ src/context.rs
@@ -131,6 +131,7 @@ impl Game {
                 .with_area(IORegArea::Wy, ppu_reg.clone())
                 .with_area(IORegArea::Wx, ppu_reg)
                 .with_area(IORegArea::BootRom, bios_wrapper.clone())
+                .with_default_serial()
                 .with_default_sound()
                 .with_default_waveform_ram();
 
