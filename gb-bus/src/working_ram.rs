2eebdc799107f83009fa7d9c1e99bbade63a0881
firelight.flagboy <firelight.flagboy@gmail.com>
2022-01-15 10:50:59 +0100

    rework `working_ram`
    - allow `working_ram` to be serialized
    - remove the use of `DynBankableStorage`

--- gb-bus/src/working_ram.rs
+++ gb-bus/src/working_ram.rs
@@ -1,25 +1,44 @@
-use crate::{generic::DynBankableStorage, Address, Area, Error, FileOperation, IORegArea};
+use crate::{Address, Area, Error, FileOperation, IORegArea};
 
 pub const RAM_BANK_SIZE: usize = 0x1000;
 pub const CGB_MAX_BANKS: usize = 8;
 pub const DMG_MAX_BANKS: usize = 2;
 
+#[cfg_attr(
+    feature = "serialization",
+    derive(serde::Deserialize, serde::Serialize)
+)]
+#[derive(Debug, Clone)]
 pub struct WorkingRam {
-    storage: DynBankableStorage<RAM_BANK_SIZE>,
+    storage: Vec<u8>,
+    bank: u8,
     enable_cgb_feature: bool,
 }
 
 impl WorkingRam {
     pub fn new(enable_cgb_feature: bool) -> Self {
-        let mut storage = if enable_cgb_feature {
-            DynBankableStorage::with_bank_amount(CGB_MAX_BANKS)
+        let storage = if enable_cgb_feature {
+            Vec::with_capacity(CGB_MAX_BANKS * RAM_BANK_SIZE)
         } else {
-            DynBankableStorage::with_bank_amount(DMG_MAX_BANKS)
+            Vec::with_capacity(DMG_MAX_BANKS * RAM_BANK_SIZE)
         };
-        storage.set_bank_index(1);
         Self {
             storage,
+            bank: 1,
             enable_cgb_feature,
+        }
+    }
+
+    fn offset_addr(&self, addr: usize) -> usize {
+        let bank = self.get_bank_for_addr(addr);
+        bank * RAM_BANK_SIZE | (addr & 0xfff) as usize
+    }
+
+    fn get_bank_for_addr(&self, addr: usize) -> usize {
+        if addr <= 0xfff {
+            0
+        } else {
+            self.bank as usize
         }
     }
 }
@@ -31,27 +50,15 @@ where
 {
     fn write(&mut self, value: u8, addr: A) -> Result<(), Error> {
         let address = addr.get_address();
-        match address {
-            0..=0xfff => self.storage.root_bank_mut()[address] = value,
-            0x1000..=0x1fff => {
-                let address = address - 0x1000;
-                self.storage[address] = value;
-            }
-            _ => return Err(Error::bus_error(addr.into())),
-        }
+        let offset_addr = self.offset_addr(address);
+        self.storage[offset_addr] = value;
         Ok(())
     }
 
     fn read(&self, addr: A) -> Result<u8, Error> {
         let address = addr.get_address();
-        match address {
-            0..=0xfff => Ok(self.storage.root_bank()[address]),
-            0x1000..=0x1fff => {
-                let address = address - 0x1000;
-                Ok(self.storage[address])
-            }
-            _ => Err(Error::bus_error(addr.into())),
-        }
+        let offset_addr = self.offset_addr(address);
+        Ok(self.storage[offset_addr])
     }
 }
 
@@ -62,7 +69,7 @@ where
 {
     fn write(&mut self, value: u8, addr: A) -> Result<(), Error> {
         if self.enable_cgb_feature {
-            self.storage.set_bank_index((value & 0x7).min(1) as usize);
+            self.bank = (value & 0x7).min(1);
             Ok(())
         } else {
             Err(Error::new_segfault(addr.into()))
@@ -71,7 +78,7 @@ where
 
     fn read(&self, addr: A) -> Result<u8, Error> {
         if self.enable_cgb_feature {
-            Ok(self.storage.current_bank_index as u8)
+            Ok(self.bank)
         } else {
             Err(Error::new_segfault(addr.into()))
         }
