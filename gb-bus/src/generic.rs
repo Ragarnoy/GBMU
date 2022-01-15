1e04116ece2f6c65a43d02555371b2e5438f60dd
firelight.flagboy <firelight.flagboy@gmail.com>
2022-01-15 10:12:44 +0100

    remove *bankable

--- gb-bus/src/generic.rs
+++ gb-bus/src/generic.rs
@@ -1,12 +1,8 @@
-mod bankable;
-mod bankable_dyn;
 mod chardev;
 mod panic;
 mod random;
 mod simple;
 
-pub use bankable::BankableStorage;
-pub use bankable_dyn::DynBankableStorage;
 pub use chardev::CharDevice;
 pub use panic::PanicDevice;
 pub use random::RandomDevice;
