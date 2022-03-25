df376b81d68314b56708b3cdde203808f97df3e1
Pierre Lamusse <plamusse42@gmail.com>
2022-03-25 14:14:24 +0100

    Write to registers

--- gb-dma/src/hdma.rs
+++ gb-dma/src/hdma.rs
@@ -1,6 +1,7 @@
 use gb_bus::{Address, Bus, Error, FileOperation, IORegArea};
 use gb_clock::{Tick, Ticker};
 
+#[derive(PartialEq)]
 enum Mode {
     Gdma,
     Hdma,
@@ -32,12 +33,45 @@ where
             IORegArea::Hdma2 => Ok(self.src as u8),
             IORegArea::Hdma3 => Ok((self.dest >> 8) as u8),
             IORegArea::Hdma4 => Ok(self.dest as u8),
-            IORegArea::Hdma5 => Ok(if self.active { self.len } else { 0xFF }),
+            IORegArea::Hdma5 => Ok(self.len | if self.active { 0x00 } else { 0x80 }),
             _ => Err(Error::SegmentationFault(addr.into())),
         }
     }
     fn write(&mut self, v: u8, addr: A) -> Result<(), gb_bus::Error> {
-        Ok(())
+        match addr.area_type() {
+            IORegArea::Hdma1 => {
+                self.src = ((v as u16) << 8) | (self.src & 0x00FF);
+                Ok(())
+            }
+            IORegArea::Hdma2 => {
+                self.src = (self.src & 0xFF00) | ((v & 0xF0) as u16);
+                Ok(())
+            }
+            IORegArea::Hdma3 => {
+                self.dest = 0x8000 | (((v & 0x1f) as u16) << 8) | (self.dest & 0xFF);
+                Ok(())
+            }
+            IORegArea::Hdma4 => {
+                self.dest = (self.dest & 0xFF00) | ((v & 0xF0) as u16);
+                Ok(())
+            }
+            IORegArea::Hdma5 => {
+                if self.active && self.mode == Some(Mode::Hdma) {
+                    if v & 0x80 == 0 {
+                        self.active = false;
+                    };
+                    return Ok(());
+                }
+                self.active = true;
+                self.len = v & 0x7F;
+                self.mode = match v & 0x80 {
+                    0 => Some(Mode::Gdma),
+                    _ => Some(Mode::Hdma),
+                };
+                Ok(())
+            }
+            _ => Err(Error::SegmentationFault(addr.into())),
+        }
     }
 }
 
