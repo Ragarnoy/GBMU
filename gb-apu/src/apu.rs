89c4cfdd82a46d97815c1922385f4ce60a8ee99b
Pierre Lamusse <plamusse42@gmail.com>
2022-03-29 18:48:36 +0200

    RW channel 2 (Nr21..Nr24)

--- gb-apu/src/apu.rs
+++ gb-apu/src/apu.rs
@@ -1,8 +1,7 @@
-use std::{cell::RefCell, rc::Rc};
-
-use gb_bus::Bus;
+use gb_bus::{Address, Bus, Error, FileOperation, IORegArea};
 use gb_clock::{Tick, Ticker};
 use sdl2::audio::AudioQueue;
+use std::{cell::RefCell, rc::Rc};
 
 #[derive(PartialEq)]
 enum ChannelType {
@@ -17,28 +16,34 @@ struct Sweep {
 }
 
 impl Sweep {
-    fn sweep_period() {}
-    fn negate() {}
-    fn shift() {}
+    fn sweep_period(&self) {}
+    fn negate(&self) {}
+    fn shift(&self) {}
 }
 
+#[derive(PartialEq)]
 enum Direction {
     Inc,
     Dec,
 }
+#[derive(Default)]
 struct VolumeEnvelope {
     bits: u8,
 }
 
 impl VolumeEnvelope {
-    fn initial_volume() -> u8 {
-        0
+    fn initial_volume(&self) -> u8 {
+        self.bits >> 4
     }
-    fn envelope_direction() -> Direction {
-        Direction::Inc
+    fn envelope_direction(&self) -> Direction {
+        if self.bits & 0x8 == 1 {
+            Direction::Inc
+        } else {
+            Direction::Dec
+        }
     }
-    fn envelope_sweep_nb() -> u8 {
-        0
+    fn envelope_sweep_nb(&self) -> u8 {
+        self.bits & 0x7
     }
 }
 
@@ -46,8 +51,9 @@ struct SoundChannel {
     buffer: Vec<i16>,
     channel_type: ChannelType,
     sweep: Option<Sweep>,
-    duty_pattern: Option<u8>,
-    length_load: u16,
+    duty_pattern_index: Option<u8>,
+    length_load: u8,
+    volume_envelope: Option<VolumeEnvelope>,
     trigger: bool,
     length_enabled: bool,
     frequency: Option<u16>,
@@ -62,12 +68,19 @@ impl SoundChannel {
             } else {
                 None
             },
-            duty_pattern: if channel_type == ChannelType::SquareWave {
+            duty_pattern_index: if channel_type == ChannelType::SquareWave {
                 Some(0)
             } else {
                 None
             },
             length_load: 0,
+            volume_envelope: if channel_type == ChannelType::SquareWave
+                || channel_type == ChannelType::Noise
+            {
+                Some(VolumeEnvelope::default())
+            } else {
+                None
+            },
             trigger: false,
             length_enabled: false,
             frequency: if channel_type == ChannelType::SquareWave
@@ -79,6 +92,95 @@ impl SoundChannel {
             },
             channel_type,
         }
+    }
+}
+
+impl<A> FileOperation<A, IORegArea> for SoundChannel
+where
+    A: Address<IORegArea>,
+    u16: From<A>,
+{
+    fn read(&self, addr: A) -> Result<u8, Error> {
+        use IORegArea::{Nr21, Nr22, Nr23, Nr24};
+        match addr.area_type() {
+            Nr21 => {
+                let mut res = 0;
+                if let Some(index) = self.duty_pattern_index {
+                    res = index << 6;
+                }
+                res |= self.length_load;
+                Ok(res)
+            }
+            Nr22 => {
+                if let Some(volume_envelope) = &self.volume_envelope {
+                    Ok(volume_envelope.bits)
+                } else {
+                    Ok(0)
+                }
+            }
+            Nr23 => {
+                if let Some(frequency) = self.frequency {
+                    Ok(frequency as u8)
+                } else {
+                    Ok(0)
+                }
+            }
+            Nr24 => {
+                let mut res = 0;
+                res |= if self.trigger { 0x80 } else { 0 };
+                res |= if self.length_enabled { 0x40 } else { 0 };
+                if let Some(frequency) = self.frequency {
+                    res |= ((frequency >> 8) & 0x07) as u8;
+                }
+
+                Ok(res)
+            }
+
+            _ => Err(Error::SegmentationFault(addr.into())),
+        }
+    }
+    fn write(&mut self, v: u8, addr: A) -> Result<(), Error> {
+        use IORegArea::{Nr21, Nr22, Nr23, Nr24};
+        match addr.area_type() {
+            Nr21 => {
+                if self.channel_type == ChannelType::SquareWave {
+                    self.duty_pattern_index = Some(v >> 6);
+                    self.length_load = v & 0x3F;
+                } else if self.channel_type == ChannelType::Noise {
+                    self.length_load = v & 0x3F;
+                } else {
+                    self.length_load = v;
+                }
+            }
+            Nr22 => {
+                if let Some(ve) = &mut self.volume_envelope {
+                    (*ve).bits = v;
+                }
+            }
+            Nr23 => {
+                if self.channel_type == ChannelType::SquareWave
+                    || self.channel_type == ChannelType::WaveForm
+                {
+                    if let Some(frequency) = &mut self.frequency {
+                        *frequency &= v as u16;
+                    }
+                }
+            }
+            Nr24 => {
+                self.trigger = v & 0x80 != 0;
+                self.length_enabled = v & 0x40 != 0;
+                if self.channel_type == ChannelType::SquareWave
+                    || self.channel_type == ChannelType::WaveForm
+                {
+                    if let Some(frequency) = &mut self.frequency {
+                        *frequency &= ((v & 0x07) as u16) << 8;
+                    }
+                }
+            }
+
+            _ => return Err(Error::SegmentationFault(addr.into())),
+        };
+        Ok(())
     }
 }
 
