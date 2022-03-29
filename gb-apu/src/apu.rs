7cc8fe84b3f9aaeb78d2a55c2f3df92e79062940
Pierre Lamusse <plamusse42@gmail.com>
2022-03-29 14:12:03 +0200

    Setup channel 2 (simple square channel)

--- gb-apu/src/apu.rs
+++ gb-apu/src/apu.rs
@@ -4,6 +4,7 @@ use gb_bus::Bus;
 use gb_clock::{Tick, Ticker};
 use sdl2::audio::AudioQueue;
 
+#[derive(PartialEq)]
 enum ChannelType {
     SquareWave,
     WaveForm,
@@ -21,18 +22,62 @@ impl Sweep {
     fn shift() {}
 }
 
+enum Direction {
+    Inc,
+    Dec,
+}
+struct VolumeEnvelope {
+    bits: u8,
+}
+
+impl VolumeEnvelope {
+    fn initial_volume() -> u8 {
+        0
+    }
+    fn envelope_direction() -> Direction {
+        Direction::Inc
+    }
+    fn envelope_sweep_nb() -> u8 {
+        0
+    }
+}
+
 struct SoundChannel {
     buffer: Vec<i16>,
     channel_type: ChannelType,
     sweep: Option<Sweep>,
+    duty_pattern: Option<u8>,
+    length_load: u16,
+    trigger: bool,
+    length_enabled: bool,
+    frequency: Option<u16>,
 }
 
 impl SoundChannel {
-    pub fn new(channel_type: ChannelType, sweep: Option<Sweep>) -> Self {
+    pub fn new(channel_type: ChannelType, handles_sweep: bool) -> Self {
         SoundChannel {
             buffer: Vec::new(),
+            sweep: if handles_sweep && channel_type == ChannelType::SquareWave {
+                Some(Sweep::default())
+            } else {
+                None
+            },
+            duty_pattern: if channel_type == ChannelType::SquareWave {
+                Some(0)
+            } else {
+                None
+            },
+            length_load: 0,
+            trigger: false,
+            length_enabled: false,
+            frequency: if channel_type == ChannelType::SquareWave
+                || channel_type == ChannelType::WaveForm
+            {
+                Some(0)
+            } else {
+                None
+            },
             channel_type,
-            sweep,
         }
     }
 }
@@ -46,10 +91,10 @@ pub struct Apu {
 impl Apu {
     pub fn new(audio_queue: Rc<RefCell<AudioQueue<i16>>>) -> Apu {
         let sound_channels = vec![
-            SoundChannel::new(ChannelType::SquareWave, Some(Sweep::default())),
-            SoundChannel::new(ChannelType::SquareWave, None),
-            SoundChannel::new(ChannelType::WaveForm, None),
-            SoundChannel::new(ChannelType::Noise, None),
+            // SoundChannel::new(ChannelType::SquareWave, Some(Sweep::default())),
+            SoundChannel::new(ChannelType::SquareWave, false),
+            // SoundChannel::new(ChannelType::WaveForm, None),
+            // SoundChannel::new(ChannelType::Noise, None),
         ];
         Self {
             audio_queue,
