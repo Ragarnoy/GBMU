9ea992611e66ba68e8ebc1d58c225b55ab8f84cb
Pierre Lamusse <plamusse42@gmail.com>
2022-04-01 11:23:36 +0200

    Control registers + length counter

--- gb-apu/src/apu.rs
+++ gb-apu/src/apu.rs
@@ -3,8 +3,8 @@ use gb_clock::{Tick, Ticker};
 use sdl2::audio::AudioQueue;
 use std::{cell::RefCell, rc::Rc};
 
-#[derive(PartialEq)]
-enum ChannelType {
+#[derive(PartialEq, Clone, Copy)]
+pub enum ChannelType {
     SquareWave,
     WaveForm,
     Noise,
@@ -19,6 +19,67 @@ impl Sweep {
     fn sweep_period(&self) {}
     fn negate(&self) {}
     fn shift(&self) {}
+}
+
+const DUTY_CYCLES: [[bool; 8]; 4] = [
+    [false, false, false, false, false, false, false, true],
+    [true, false, false, false, false, false, false, true],
+    [true, false, false, false, false, true, true, true],
+    [false, true, true, true, true, true, true, false],
+];
+
+#[derive(Default)]
+struct Duty {
+    pattern_index: u8,
+    step: u8,
+}
+
+impl Duty {
+    pub fn reset(&mut self) {
+        self.step = 0;
+    }
+    pub fn next(&mut self) -> bool {
+        self.step += 1;
+        self.step %= 8;
+        DUTY_CYCLES[self.pattern_index as usize][self.step as usize]
+    }
+}
+
+pub struct LengthCounter {
+    channel_type: ChannelType,
+    enabled: bool,
+    length_load: u8,
+    counter: u16,
+}
+
+impl LengthCounter {
+    pub fn new(channel_type: ChannelType) -> Self {
+        Self {
+            channel_type,
+            length_load: 0,
+            counter: 0,
+            enabled: false,
+        }
+    }
+
+    pub fn step(&mut self) -> bool {
+        if self.counter > 0 && self.enabled {
+            self.counter -= 1;
+
+            if self.counter == 0 {
+                return true;
+            }
+        }
+        false
+    }
+
+    pub fn reload(&mut self) {
+        self.counter = if self.channel_type == ChannelType::WaveForm {
+            0x100
+        } else {
+            0x40
+        }
+    }
 }
 
 #[derive(PartialEq)]
@@ -51,10 +112,10 @@ struct SoundChannel {
     buffer: Vec<i16>,
     channel_type: ChannelType,
     sweep: Option<Sweep>,
-    duty_pattern_index: Option<u8>,
-    length_load: u8,
+    duty: Option<Duty>,
+    length_counter: LengthCounter,
     volume_envelope: Option<VolumeEnvelope>,
-    trigger: bool,
+    enabled: bool,
     length_enabled: bool,
     frequency: Option<u16>,
 }
@@ -68,12 +129,12 @@ impl SoundChannel {
             } else {
                 None
             },
-            duty_pattern_index: if channel_type == ChannelType::SquareWave {
-                Some(0)
+            duty: if channel_type == ChannelType::SquareWave {
+                Some(Duty::default())
             } else {
                 None
             },
-            length_load: 0,
+            length_counter: LengthCounter::new(channel_type.clone()),
             volume_envelope: if channel_type == ChannelType::SquareWave
                 || channel_type == ChannelType::Noise
             {
@@ -81,7 +142,7 @@ impl SoundChannel {
             } else {
                 None
             },
-            trigger: false,
+            enabled: false,
             length_enabled: false,
             frequency: if channel_type == ChannelType::SquareWave
                 || channel_type == ChannelType::WaveForm
@@ -92,6 +153,13 @@ impl SoundChannel {
             },
             channel_type,
         }
+    }
+    pub fn disable(&mut self) {
+        self.enabled = false;
+    }
+
+    pub fn length_counter_step(&mut self) {
+        self.enabled = self.length_counter.step();
     }
 }
 
@@ -105,10 +173,10 @@ where
         match addr.area_type() {
             Nr21 => {
                 let mut res = 0;
-                if let Some(index) = self.duty_pattern_index {
-                    res = index << 6;
+                if let Some(duty) = &self.duty {
+                    res = duty.pattern_index << 6;
                 }
-                res |= self.length_load;
+                res |= self.length_counter.length_load;
                 Ok(res)
             }
             Nr22 => {
@@ -127,7 +195,7 @@ where
             }
             Nr24 => {
                 let mut res = 0;
-                res |= if self.trigger { 0x80 } else { 0 };
+                res |= if self.enabled { 0x80 } else { 0 };
                 res |= if self.length_enabled { 0x40 } else { 0 };
                 if let Some(frequency) = self.frequency {
                     res |= ((frequency >> 8) & 0x07) as u8;
@@ -144,12 +212,19 @@ where
         match addr.area_type() {
             Nr21 => {
                 if self.channel_type == ChannelType::SquareWave {
-                    self.duty_pattern_index = Some(v >> 6);
-                    self.length_load = v & 0x3F;
-                } else if self.channel_type == ChannelType::Noise {
-                    self.length_load = v & 0x3F;
+                    if let Some(duty) = &mut self.duty {
+                        (*duty).pattern_index = v >> 6;
+                        (*duty).step = 0;
+                    }
+                }
+                if self.channel_type == ChannelType::SquareWave
+                    || self.channel_type == ChannelType::Noise
+                {
+                    self.length_counter.length_load = v & 0x3F;
+                    self.length_counter.counter = 0x40 - (v & 0x3F) as u16;
                 } else {
-                    self.length_load = v;
+                    self.length_counter.length_load = v;
+                    self.length_counter.counter = 0x100 - v as u16;
                 }
             }
             Nr22 => {
@@ -167,7 +242,7 @@ where
                 }
             }
             Nr24 => {
-                self.trigger = v & 0x80 != 0;
+                self.enabled = v & 0x80 != 0;
                 self.length_enabled = v & 0x40 != 0;
                 if self.channel_type == ChannelType::SquareWave
                     || self.channel_type == ChannelType::WaveForm
@@ -184,25 +259,52 @@ where
     }
 }
 
+#[derive(Default)]
+struct FrameSequencer {
+    step: u8,
+}
+
+impl FrameSequencer {
+    fn next(&mut self) -> u8 {
+        self.step += 1;
+        self.step %= 8;
+        self.step
+    }
+}
+
 pub struct Apu {
+    cycle_counter: u16,
+    enabled: bool,
     audio_queue: Rc<RefCell<AudioQueue<i16>>>,
     mix_buffer: Vec<i16>,
     sound_channels: Vec<SoundChannel>,
+    frame_sequencer: FrameSequencer,
 }
 
 impl Apu {
     pub fn new(audio_queue: Rc<RefCell<AudioQueue<i16>>>) -> Apu {
+        // Channels order in vector is important !
         let sound_channels = vec![
-            // SoundChannel::new(ChannelType::SquareWave, Some(Sweep::default())),
+            SoundChannel::new(ChannelType::SquareWave, true),
             SoundChannel::new(ChannelType::SquareWave, false),
-            // SoundChannel::new(ChannelType::WaveForm, None),
-            // SoundChannel::new(ChannelType::Noise, None),
+            SoundChannel::new(ChannelType::WaveForm, false),
+            SoundChannel::new(ChannelType::Noise, false),
         ];
         Self {
+            cycle_counter: 0,
+            enabled: false,
             audio_queue,
             mix_buffer: Vec::new(),
             sound_channels,
+            frame_sequencer: FrameSequencer::default(),
         }
+    }
+
+    fn queue_audio(&self) {
+        self.audio_queue
+            .borrow()
+            .queue_audio(&self.mix_buffer)
+            .expect("failed to queue audio");
     }
 }
 
@@ -212,9 +314,59 @@ impl Ticker for Apu {
     }
 
     fn tick(&mut self, _addr_bus: &mut dyn Bus<u8>) {
-        self.audio_queue
-            .borrow()
-            .queue_audio(&self.mix_buffer)
-            .expect("failed to queue audio");
+        if !self.enabled || self.cycle_counter < 0x2000 {
+            self.cycle_counter += 1;
+            return;
+        }
+        self.cycle_counter = 0;
+
+        let step = self.frame_sequencer.next();
+        if step == 0 || step == 2 || step == 4 || step == 6 {
+            for i in 0..self.sound_channels.len() {
+                self.sound_channels[i].length_counter_step();
+            }
+        }
+    }
+}
+
+impl<A> FileOperation<A, IORegArea> for Apu
+where
+    A: Address<IORegArea>,
+    u16: From<A>,
+{
+    fn read(&self, addr: A) -> Result<u8, Error> {
+        use IORegArea::{Nr50, Nr51, Nr52};
+        match addr.area_type() {
+            Nr52 => Ok(if self.enabled { 0x80 } else { 0 }
+                | if self.sound_channels[3].enabled {
+                    0x8
+                } else {
+                    0
+                }
+                | if self.sound_channels[2].enabled {
+                    0x4
+                } else {
+                    0
+                }
+                | if self.sound_channels[1].enabled {
+                    0x2
+                } else {
+                    0
+                }
+                | if self.sound_channels[0].enabled {
+                    0x1
+                } else {
+                    0
+                }),
+            _ => Err(Error::SegmentationFault(addr.into())),
+        }
+    }
+    fn write(&mut self, v: u8, addr: A) -> Result<(), Error> {
+        use IORegArea::{Nr50, Nr51, Nr52};
+        match addr.area_type() {
+            Nr52 => self.enabled = v & 0x80 != 0x00,
+            _ => return Err(Error::SegmentationFault(addr.into())),
+        };
+        Ok(())
     }
 }
