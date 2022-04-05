fbddbc0691d0148c680484ded15cc7796d8f9869
firelight.flagboy <firelight.flagboy@gmail.com>
2022-04-05 19:40:55 +0200

    add constant for timing + GB screen size

--- src/constant.rs
+++ src/constant.rs
@@ -12,3 +12,7 @@ pub const PREFERED_ROM_EXTS: [&str; 3] = ["rom", "gb", "gbc"];
 #[cfg(feature = "save_state")]
 /// List of prefered extensions for `save state` file
 pub const PREFERED_SAVE_STATE_EXT: [&str; 1] = [SAVE_STATE_EXT];
+// const TARGET_FPS_X10: u64 = 597;    // the true value
+pub const TARGET_FPS_X10: u64 = 600;
+pub const GB_SCREEN_WIDTH: u32 = 160;
+pub const GB_SCREEN_HEIGHT: u32 = 144;
