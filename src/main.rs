771aa46a74641bd63e13b44879f8c79260bab5cd
firelight.flagboy <firelight.flagboy@gmail.com>
2022-04-05 20:48:48 +0200

    event base

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod config;
 mod constant;
+mod custom_event;
 mod logger;
 #[cfg(any(feature = "time_frame", feature = "debug_fps"))]
 mod time_frame;
@@ -7,11 +8,13 @@ mod time_frame;
 use clap::StructOpt;
 use config::Config;
 use constant::{GB_SCREEN_HEIGHT, GB_SCREEN_WIDTH, TARGET_FPS_X10};
+use custom_event::CustomEvent;
 use logger::init_logger;
 use pixels::{Error, Pixels, SurfaceTexture};
 use std::time::Duration;
 use winit::{
     dpi::LogicalSize,
+    event::Event,
     event_loop::EventLoop,
     window::{Window, WindowBuilder},
 };
@@ -28,17 +31,17 @@ fn main() -> Result<(), Error> {
     let mut render_time_frame = time_frame::TimeStat::default();
     let frame_duration_target = Duration::from_nanos(10_000_000_000 / TARGET_FPS_X10);
     init_logger(opts.log_level);
-    let input = WinitInputHelper::new();
+    let mut input = WinitInputHelper::new();
 
     let (event_loop, main_window, pixels) = init::<GB_SCREEN_WIDTH, GB_SCREEN_HEIGHT>(&opts)?;
 
-    Ok(())
+    event_loop.run(move |event, event_loop, control_flow| handle_input_event(&mut input, &event))
 }
 
 fn init<const WIDTH: u32, const HEIGHT: u32>(
     config: &Config,
-) -> Result<(EventLoop<()>, Window, Pixels), Error> {
-    let event_loop = EventLoop::new();
+) -> Result<(EventLoop<CustomEvent>, Window, Pixels), Error> {
+    let event_loop = EventLoop::with_user_event();
     let main_window = {
         let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
         WindowBuilder::new()
@@ -60,4 +63,8 @@ fn init<const WIDTH: u32, const HEIGHT: u32>(
     };
 
     Ok((event_loop, main_window, pixels))
+}
+
+fn handle_input_event(input: &mut WinitInputHelper, event: &Event<CustomEvent>) {
+    if input.update(event) {}
 }
