b3ed2a9c302fc0780327c5b2f033c868b33e5f18
firelight.flagboy <firelight.flagboy@gmail.com>
2022-04-09 13:53:51 +0200

    wip `file` menu

--- src/constant.rs
+++ src/constant.rs
@@ -8,7 +8,7 @@ pub const APP_NAME: &str = "gbmu";
 /// File extension for a file that should contain a `game save` save
 // pub const GAME_SAVE_EXT: &str = "gamepack";
 /// List of prefered extensions for ROM file
-// pub const PREFERED_ROM_EXTS: [&str; 3] = ["rom", "gb", "gbc"];
+pub const PREFERED_ROM_EXTS: [&str; 3] = ["rom", "gb", "gbc"];
 // #[cfg(feature = "save_state")]
 /// List of prefered extensions for `save state` file
 // pub const PREFERED_SAVE_STATE_EXT: [&str; 1] = [SAVE_STATE_EXT];
