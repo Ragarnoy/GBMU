995c64719ab8aeedff0c16dec8e8c4d2e6cf999e
firelight.flagboy <firelight.flagboy@gmail.com>
2022-04-10 11:17:58 +0200

    factorize keyentry

--- gb-joypad/src/config.rs
+++ gb-joypad/src/config.rs
@@ -1,11 +1,27 @@
 use crate::InputType;
 use std::collections::HashMap;
-use winit::event::ScanCode;
+use winit::event::{KeyboardInput, ScanCode, VirtualKeyCode};
 
 /// Store a joypad configuration.
 ///
 /// Since it implement Serialise and Deserialize, it can be used to quickly save/load a joypad configuration into/from a file.
 #[derive(serde::Serialize, serde::Deserialize)]
 pub struct Config {
-    pub mapping: HashMap<ScanCode, InputType>,
+    pub mapping: HashMap<KeyEntry, InputType>,
+}
+
+#[derive(Hash, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone, Copy)]
+pub enum KeyEntry {
+    ScanCode(ScanCode),
+    VirtualKeyCode(VirtualKeyCode),
+}
+
+impl From<KeyboardInput> for KeyEntry {
+    fn from(ipt: KeyboardInput) -> Self {
+        if let Some(virtual_keycode) = ipt.virtual_keycode {
+            Self::VirtualKeyCode(virtual_keycode)
+        } else {
+            Self::ScanCode(ipt.scancode)
+        }
+    }
 }
