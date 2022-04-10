70512918742b4e660acef42a8c49c8fc759b25ab
firelight.flagboy <firelight.flagboy@gmail.com>
2022-04-10 15:44:26 +0200

    add ui for settings

---
+++ src/ui/settings.rs
@@ -0,0 +1,46 @@
+use egui::Ui;
+use winit::event_loop::EventLoopProxy;
+
+#[cfg(feature = "cgb")]
+use crate::config::{Config, Mode};
+use crate::{custom_event::CustomEvent, windows::WindowType};
+
+pub(crate) fn draw_ui(
+    ui: &mut Ui,
+    event_proxy: &EventLoopProxy<CustomEvent>,
+    #[cfg(feature = "cgb")] config: &mut Config,
+) {
+    ui.menu_button("Settings", |ui| {
+        ui.style_mut().override_text_style = None;
+        if ui.button("Input").clicked() {
+            event_proxy
+                .send_event(CustomEvent::OpenWindow(WindowType::Keybindings))
+                .expect("cannot send open keybindings window event");
+        }
+        #[cfg(feature = "cgb")]
+        {
+            ui.separator();
+            if ui.radio_value(&mut config.mode, None, "auto").clicked() {
+                event_proxy
+                    .send_event(CustomEvent::ChangedMode(None))
+                    .unwrap();
+            }
+            if ui
+                .radio_value(&mut config.mode, Some(Mode::Classic), "dmg")
+                .clicked()
+            {
+                event_proxy
+                    .send_event(CustomEvent::ChangedMode(Some(Mode::Classic)))
+                    .unwrap();
+            }
+            if ui
+                .radio_value(&mut config.mode, Some(Mode::Color), "cgb")
+                .clicked()
+            {
+                event_proxy
+                    .send_event(CustomEvent::ChangedMode(Some(Mode::Color)))
+                    .unwrap();
+            }
+        }
+    });
+}
