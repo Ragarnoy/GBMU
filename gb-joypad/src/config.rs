1233dfca4979d40777b1be361de68880eb66f7bd
firelight.flagboy <firelight.flagboy@gmail.com>
2022-04-10 11:21:09 +0200

    add default key constant

--- gb-joypad/src/config.rs
+++ gb-joypad/src/config.rs
@@ -10,7 +10,7 @@ pub struct Config {
     pub mapping: HashMap<KeyEntry, InputType>,
 }
 
-#[derive(Hash, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone, Copy)]
+#[derive(Hash, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone, Copy, Debug)]
 pub enum KeyEntry {
     ScanCode(ScanCode),
     VirtualKeyCode(VirtualKeyCode),
@@ -24,4 +24,15 @@ impl From<KeyboardInput> for KeyEntry {
             Self::ScanCode(ipt.scancode)
         }
     }
+}
+
+impl KeyEntry {
+    pub const A: KeyEntry = KeyEntry::VirtualKeyCode(VirtualKeyCode::A);
+    pub const B: KeyEntry = KeyEntry::VirtualKeyCode(VirtualKeyCode::B);
+    pub const Up: KeyEntry = KeyEntry::VirtualKeyCode(VirtualKeyCode::Up);
+    pub const Down: KeyEntry = KeyEntry::VirtualKeyCode(VirtualKeyCode::Down);
+    pub const Left: KeyEntry = KeyEntry::VirtualKeyCode(VirtualKeyCode::Left);
+    pub const Right: KeyEntry = KeyEntry::VirtualKeyCode(VirtualKeyCode::Right);
+    pub const Return: KeyEntry = KeyEntry::VirtualKeyCode(VirtualKeyCode::Return);
+    pub const RShift: KeyEntry = KeyEntry::VirtualKeyCode(VirtualKeyCode::RShift);
 }
