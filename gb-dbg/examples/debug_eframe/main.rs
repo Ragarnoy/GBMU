781906438ba204bcb5a43b5ff83ef67712b8f6f4
tlernoul <tlernoul@student.42.fr>
2022-05-12 00:24:38 +0200

    update/fix because why not

--- gb-dbg/examples/debug_eframe/main.rs
+++ gb-dbg/examples/debug_eframe/main.rs
@@ -1,6 +1,6 @@
 mod game;
 
-use eframe::egui::CtxRef;
+use eframe::egui::Context;
 use eframe::epi::*;
 use egui::Vec2;
 use game::Game;
@@ -10,19 +10,19 @@ use gb_dbg::{DEBUGGER_HEIGHT, DEBUGGER_WIDTH};
 use std::ops::ControlFlow;
 
 pub struct DebuggerApp {
-    pub debugger: Debugger<Game>,
+    pub debugger: Debugger,
     pub memory: Game,
 }
 
 impl App for DebuggerApp {
-    fn update(&mut self, ctx: &CtxRef, _frame: &Frame) {
+    fn update(&mut self, ctx: &Context, _frame: &Frame) {
         self.debugger.draw(ctx, &mut self.memory, None);
 
         if let Some(flow) = self.debugger.flow_status() {
             match flow {
                 ControlFlow::Break(x) => match x {
                     Until::Cycle(n) => self.memory.pc += (n << 1) as u16,
-                    Until::Step(_n) => self.memory.pc += 1 as u16,
+                    Until::Step(_n) => self.memory.pc += 1_u16,
                     Until::Frame(n) => self.memory.pc += (n << 2) as u16,
                     Until::Second(n) => self.memory.pc += (n << 3) as u16,
                     Until::Null => self.memory.pc = 0,
@@ -38,14 +38,14 @@ impl App for DebuggerApp {
 }
 
 fn main() {
-    let dbg: Debugger<Game> = DebuggerBuilder::new().build();
+    let dbg: Debugger = DebuggerBuilder::new().build();
     let dgb_app = DebuggerApp {
         debugger: dbg,
         memory: Default::default(),
     };
     let options = NativeOptions {
         resizable: false,
-        initial_window_size: Some(Vec2::from((DEBUGGER_WIDTH, DEBUGGER_HEIGHT))),
+        initial_window_size: Some(Vec2::from((DEBUGGER_WIDTH as f32, DEBUGGER_HEIGHT as f32))),
         ..Default::default()
     };
     eframe::run_native(Box::new(dgb_app), options)
