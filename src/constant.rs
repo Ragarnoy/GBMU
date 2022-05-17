7726c831aa6f7c54d39e6e68ca5e3acfe08d57a5
42plamusse <plamusse42@gmail.com>
2022-05-17 15:45:50 +0200

    Feature/audio sync (#686)
    * Disable v-sync in pixels init
    
    * Sync with audio
    
    * double buffer size.
    
    * Remove audio feature.
    
    * Fix/audio sync (#695)
    
    * increase main window init size
    
    * add spelling
    
    * add log crate to apu
    
    * add log + sanity check to apu
    
    * Squashed commit of the following:
    
    commit 76b223e04b00629b03d80ad58df91d6988fa5199
    Author: firelight.flagboy <firelight.flagboy@gmail.com>
    Date:   Sat May 14 15:16:08 2022 +0200
    
        add comment for feature related to FPS
    
    commit 4b933192d9c5d7549cbc5a57f0fa24ceb7121651
    Author: firelight.flagboy <firelight.flagboy@gmail.com>
    Date:   Sat May 14 15:13:46 2022 +0200
    
        add feature fps_stat & fps_expert
    
    commit 1ae80511d4c46eaeb2cb98995b05b326ba2a42c4
    Author: firelight.flagboy <firelight.flagboy@gmail.com>
    Date:   Sat May 14 14:53:58 2022 +0200
    
        correct typo
    
    commit 25ad14afdb789482ba944b89dfa342f2cee41d39
    Author: firelight.flagboy <firelight.flagboy@gmail.com>
    Date:   Sat May 14 14:52:55 2022 +0200
    
        rename feature debug_fps => fps
    
    commit 330633cc93ca78c419d956045a223e89e5c1ca38
    Author: firelight.flagboy <firelight.flagboy@gmail.com>
    Date:   Sat May 14 14:51:49 2022 +0200
    
        remove time_stat_samples
    
    commit c4ba66e7ed659aba3ae68653381d206c1425b4b8
    Author: firelight.flagboy <firelight.flagboy@gmail.com>
    Date:   Sat May 14 14:50:41 2022 +0200
    
        remove feature time_frame
    
    commit 52cceaf2dee5f8f36c923da2b8ae4bfc4648e8dd
    Author: firelight.flagboy <firelight.flagboy@gmail.com>
    Date:   Sat May 14 14:43:16 2022 +0200
    
        rework time stat
    
    commit 330590f32397d410c02231904ea6c94eb7f68755
    Author: firelight.flagboy <firelight.flagboy@gmail.com>
    Date:   Sat May 14 12:35:56 2022 +0200
    
        spelling + format
    
    commit 8053242abe1611b9805ca4af78f9628cc19cd409
    Author: firelight.flagboy <firelight.flagboy@gmail.com>
    Date:   Sat May 14 12:35:41 2022 +0200
    
        rework fps
    
    commit badff9b1fab28c6d14ff4105b8b54bfcee73b2e3
    Author: firelight.flagboy <firelight.flagboy@gmail.com>
    Date:   Sat May 14 12:09:12 2022 +0200
    
        correct clokc example
    
    commit 6e49642c691a8ebd6507fe3514b86df85e061cf0
    Author: firelight.flagboy <firelight.flagboy@gmail.com>
    Date:   Sat May 14 12:09:02 2022 +0200
    
        add spelling file
    
    commit c031f1b7de19f250326f549594f34c3892b012da
    Author: guilhemSmith <guilhemSmith@users.noreply.github.com>
    Date:   Sat May 14 10:37:24 2022 +0200
    
        Line 153 count as LY 0. (#688)
    
        * Line 153 count as LY 0.
    
        * Code format.
    
    commit 3987ac2ebd876d546cb3f7e6d9df41ad689da422
    Author: FirelightFlagboy <30697622+FirelightFlagboy@users.noreply.github.com>
    Date:   Sat May 14 09:43:59 2022 +0200
    
        add example to list config (#687)
    
    * add word to cspell
    
    * pad fps value
    
    * add debug message when audio buffer is full
    
    * correct typo
    
    * refactor build stream output
    
    * move next_value callback to build_output_stream
    
    * rework how we retrieve a valid device
    
    * update conditionnal for selected viable stream config
    
    * correct clippy
    
    Co-authored-by: guilhem smith <smith.guilhem@gmail.com>
    Co-authored-by: FirelightFlagboy <30697622+FirelightFlagboy@users.noreply.github.com>
    Co-authored-by: firelight.flagboy <firelight.flagboy@gmail.com>

--- src/constant.rs
+++ src/constant.rs
@@ -1,5 +1,3 @@
-use std::time::Duration;
-
 /// Name of the application
 pub const APP_NAME: &str = "gbmu";
 /// Name of the organization
@@ -10,13 +8,11 @@ pub const APP_NAME: &str = "gbmu";
 /// File extension for a file that should contain a `game save` save
 pub const GAME_SAVE_EXT: &str = "gamepack";
 /// List of preferred extensions for ROM file
-pub const PREFERED_ROM_EXTS: [&str; 3] = ["rom", "gb", "gbc"];
+pub const PREFERRED_ROM_EXTS: [&str; 3] = ["rom", "gb", "gbc"];
 // #[cfg(feature = "save_state")]
-// /// List of prefered extensions for `save state` file
-// pub const PREFERED_SAVE_STATE_EXT: [&str; 1] = [SAVE_STATE_EXT];
+// /// List of preferred extensions for `save state` file
+// pub const PREFERRED_SAVE_STATE_EXT: [&str; 1] = [SAVE_STATE_EXT];
 
 pub const MENU_BAR_SIZE: f32 = 30.;
-// /// FPS we want to have
-// // pub const TARGET_FPS_X10: u64 = 597;    // the true value
-pub const TARGET_FPS_X10: u64 = 600;
-pub const TARGET_FRAME_DURATION: Duration = Duration::from_nanos(10_000_000_000 / TARGET_FPS_X10);
+
+pub const AUDIO_BUFFER_SIZE: usize = 2048;
