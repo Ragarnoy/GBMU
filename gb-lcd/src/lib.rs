168d00110112ed3a5d4dfcfb8e340ee8d844630d
Tiago Lernould <tiagolernould@gmail.com>
2022-05-18 18:44:43 +0200

    removing dead code

--- gb-lcd/src/lib.rs
+++ gb-lcd/src/lib.rs
@@ -17,22 +17,6 @@ use winit::{
     window::{Window, WindowId},
 };
 
-// use error::Error;
-// use sdl2::{video::GLProfile, EventPump, Sdl, VideoSubsystem};
-
-// pub fn init() -> Result<(Sdl, VideoSubsystem, EventPump), Error> {
-//     let sdl_context = sdl2::init().map_err(Error::MainSys)?;
-//     let video_subsystem = sdl_context.video().map_err(Error::MainSys)?;
-
-//     let event_pump = sdl_context.event_pump().map_err(Error::MainSys)?;
-
-//     let gl_attr = video_subsystem.gl_attr();
-//     gl_attr.set_context_profile(GLProfile::Core);
-//     // OpenGL 3.3 is the minimum that we will support.
-//     gl_attr.set_context_version(3, 3);
-
-//     Ok((sdl_context, video_subsystem, event_pump))
-// }
 
 pub trait PseudoWindow {
     /// Returns the scale factor that can be used to map logical pixels to physical pixels, and vice versa.
